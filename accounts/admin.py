a5f2ced2c0b15a362b378477bc6f97fb2bdca7ab
Eric Moss <eric.moss08@gmail.com>
2022-10-30 02:08:25 -0400

    Finalized user admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -18,9 +18,23 @@ You should have received a copy of the GNU General Public License
 along with Monta.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-from typing import Type
+from typing import Any, Type
 
-from django.contrib import admin
+from django.conf import settings
+from django.contrib import admin, messages
+from django.contrib.admin.options import IS_POPUP_VAR, csrf_protect_m
+from django.contrib.admin.utils import unquote
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.admin import sensitive_post_parameters_m
+from django.contrib.auth.forms import (AdminPasswordChangeForm, UserChangeForm,
+                                       UserCreationForm)
+from django.core.exceptions import PermissionDenied
+from django.db import router, transaction
+from django.http import Http404, HttpRequest, HttpResponse, HttpResponseRedirect
+from django.template.response import TemplateResponse
+from django.urls import URLPattern, path, reverse
+from django.utils.html import escape
+from django.utils.translation import gettext, gettext_lazy as _
 
 from accounts import models
 
@@ -45,14 +59,214 @@ class ProfileInline(admin.StackedInline):
 class UserAdmin(admin.ModelAdmin):
     """
     User Admin
+
+    Notes:
+        This code was taken from Django Admin defaults and modified to allow for
+        password changes.
     """
 
-    inlines: tuple[Type[ProfileInline]] = (ProfileInline,)
-    list_display: tuple[str, ...] = (
-        "username",
-        "email",
+    change_user_password_template = None
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        (
+            "Permissions",
+            {"fields": ("is_staff", "is_superuser", "groups", "user_permissions")},
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
     )
-    search_fields = ("username", "email")
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2"),
+            },
+        ),
+    )
+    form: Type[UserChangeForm] = UserChangeForm
+    add_form: Type[UserCreationForm] = UserCreationForm
+    change_password_form: Type[AdminPasswordChangeForm] = AdminPasswordChangeForm
+    list_display = ("username", "email", "is_staff")
+    list_filter = ("is_staff", "is_superuser", "groups")
+    search_fields = ("username", "first_name", "last_name", "email")
+    ordering = ("username",)
+    filter_horizontal = (
+        "groups",
+        "user_permissions",
+    )
+    inlines: tuple[Type[ProfileInline]] = (ProfileInline,)
+
+    def get_fieldsets(self, request: HttpRequest, obj=None):
+        """Return fieldsets for add/change view
+
+        Args:
+            request (HttpRequest): request
+            obj (): object
+
+        Returns:
+            fieldsets
+
+        """
+        if not obj:
+            return self.add_fieldsets
+        return super().get_fieldsets(request, obj)
+
+    def get_form(self, request: HttpRequest, obj=None, **kwargs: Any) -> UserChangeForm:
+        """Get form for user admin
+
+        Args:
+            request (HttpRequest): request
+            obj: object
+            **kwargs (Any): kwargs
+
+        Returns:
+
+        """
+        defaults = {}
+        if obj is None:
+            defaults["form"] = self.add_form
+        defaults.update(kwargs)
+        return super().get_form(request, obj, **defaults)
+
+    def get_urls(self) -> list[URLPattern]:
+        """Get urls for user admin
+
+        Returns:
+            list[URLPattern]: urls for user admin
+        """
+        return [
+                   path(
+                       "<id>/password/",
+                       self.admin_site.admin_view(self.user_change_password),
+                       name="auth_user_password_change",
+                   ),
+               ] + super().get_urls()
+
+    def lookup_allowed(self, lookup: str, value: str) -> bool:
+        """Allow lookup for username
+
+        Args:
+            lookup (str): lookup
+            value (str): value
+
+        Returns:
+            bool: True if lookup is allowed
+        """
+        # Don't allow lookups involving passwords.
+        return not lookup.startswith("password") and super().lookup_allowed(
+            lookup, value
+        )
+
+    @sensitive_post_parameters_m
+    @csrf_protect_m
+    def add_view(self, request: HttpRequest, form_url: str = "", extra_context: Any = None) -> HttpResponse:
+        """The 'add' admin view for this model.
+
+        Args:
+            request (HttpRequest): request
+            form_url (str): form url
+            extra_context (Any): extra context
+
+        Returns:
+            HttpResponse: response
+        """
+        with transaction.atomic(using=router.db_for_write(self.model)):
+            return self._add_view(request, form_url, extra_context)
+
+    def _add_view(self, request, form_url="", extra_context=None) -> HttpResponse:
+        # It's an error for a user to have add permission but NOT change
+        # permission for users. If we allowed such users to add users, they
+        # could create superusers, which would mean they would essentially have
+        # the permission to change users. To avoid the problem entirely, we
+        # disallow users from adding users if they don't have change
+        # permission.
+        if not self.has_change_permission(request):
+            if self.has_add_permission(request) and settings.DEBUG:
+                # Raise Http404 in debug mode so that the user gets a helpful
+                # error message.
+                raise Http404(
+                    'Your user does not have the "Change user" permission. In '
+                    "order to add users, Django requires that your user "
+                    'account have both the "Add user" and "Change user" '
+                    "permissions set."
+                )
+            raise PermissionDenied
+        if extra_context is None:
+            extra_context = {}
+        username_field = self.model._meta.get_field(self.model.USERNAME_FIELD)
+        defaults = {
+            "auto_populated_fields": (),
+            "username_help_text": username_field.help_text,
+        }
+        extra_context.update(defaults)
+        return super().add_view(request, form_url, extra_context)
+
+    @sensitive_post_parameters_m
+    def user_change_password(self, request, id, form_url="") -> HttpResponseRedirect | TemplateResponse:
+        user = self.get_object(request, unquote(id))
+        if not self.has_change_permission(request, user):
+            raise PermissionDenied
+        if user is None:
+            raise Http404(
+                _("%(name)s object with primary key %(key)r does not exist.")
+                % {
+                    "name": self.model._meta.verbose_name,
+                    "key": escape(id),
+                }
+            )
+        if request.method == "POST":
+            form: AdminPasswordChangeForm = self.change_password_form(user, request.POST)
+            if form.is_valid():
+                form.save()
+                change_message = self.construct_change_message(request, form, None)
+                self.log_change(request, user, change_message)
+                msg: str = gettext("Password changed successfully.")
+                messages.success(request, msg)
+                update_session_auth_hash(request, form.user)
+                return HttpResponseRedirect(
+                    reverse(
+                        "%s:%s_%s_change"
+                        % (
+                            self.admin_site.name,
+                            user._meta.app_label,
+                            user._meta.model_name,
+                        ),
+                        args=(user.pk,),
+                    )
+                )
+        else:
+            form: AdminPasswordChangeForm = self.change_password_form(user)
+
+        fieldsets = [(None, {"fields": list(form.base_fields)})]
+        adminForm = admin.helpers.AdminForm(form, fieldsets, {})
+
+        context = {
+            "title": _("Change password: %s") % escape(user.get_username()),
+            "adminForm": adminForm,
+            "form_url": form_url,
+            "form": form,
+            "is_popup": (IS_POPUP_VAR in request.POST or IS_POPUP_VAR in request.GET),
+            "is_popup_var": IS_POPUP_VAR,
+            "add": True,
+            "change": False,
+            "has_delete_permission": False,
+            "has_change_permission": True,
+            "has_absolute_url": False,
+            "opts": self.model._meta,
+            "original": user,
+            "save_as": False,
+            "show_save": True,
+            **self.admin_site.each_context(request),
+        }
+
+        request.current_app = self.admin_site.name
+
+        return TemplateResponse(
+            request,
+            self.change_user_password_template
+            or "admin/auth/user/change_password.html",
+            context,
+        )
 
 
 @admin.register(models.JobTitle)
--- accounts/signals.py
+++ accounts/signals.py
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with Monta.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-from typing import Any, Optional
+from typing import Any
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
