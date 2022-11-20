22aa91d1f730926c4adfd7a53f8f1567f9d3e821
Eric Moss <yeswolfprod@gmail.com>
2022-11-20 18:54:05 -0500

    Add new exception for Sequencing.

--- backend/urls.py
+++ backend/urls.py
@@ -29,4 +29,4 @@ urlpatterns = [
     path("api/", include("core.urls")),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # type: ignore
