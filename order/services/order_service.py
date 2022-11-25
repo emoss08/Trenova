6cfd04081c47b25a5988d4bab87c6b1d118066ea
Eric Moss <yeswolfprod@gmail.com>
2022-11-24 23:59:50 -0500

    Add new Order Control fields

--- order/services/order_service.py
+++ order/services/order_service.py
@@ -16,6 +16,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Monta.  If not, see <https://www.gnu.org/licenses/>.
 """
+
 from django.db.models import Sum
 
 from order.models import Order
