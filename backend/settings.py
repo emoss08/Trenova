e2b9cd480f60d65411c4d0d3602de325e4bd6c04
Eric Moss <yeswolfprod@gmail.com>
2022-12-01 02:55:12 -0500

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -101,7 +101,7 @@ ROOT_URLCONF = "backend.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        'DIRS': [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
