a52ec649ce47030ef8daba0403fcdb5ed57ab6aa
Eric Moss <yeswolfprod@gmail.com>
2022-12-01 04:24:46 -0500

    Move django app to server file

--- server/backend/urls.py
+++ server/backend/urls.py
@@ -17,11 +17,15 @@ You should have received a copy of the GNU General Public License
 along with Monta.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 
-from core import views
-
-app_name = "core"
 urlpatterns = [
-    path("", views.HomeView.as_view(), name="home"),
+    path("__debug__/", include("debug_toolbar.urls")),
+    path("admin/doc/", include("django.contrib.admindocs.urls")),
+    path("admin/", admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # type: ignore
