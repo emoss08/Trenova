839dbdb2e30dbd9a45720534e61337697a64e50c
Wolfred <119015874+wolfredstep@users.noreply.github.com>
2022-12-01 04:26:33 -0500

    Verion 0 0 3 (#9)
    * add and change tests
    
    * worker tests
    
    * accounting tests
    
    * Create views.py
    
    * Factory Changes
    
    * Add organization middleware
    
    * Small user changes
    
    * Generic Classes implementation
    
    At some point these will be re-written to include re-usable code.
    
    * Update organization_middleware.py
    
    Fix mypy issue with organization
    
    * Update organization_middleware.py
    
    change to throw forbidden if user is not apart of an organization
    
    * Login View and changes to organization middleware
    
    * Update settings.py
    
    * Update .gitignore
    
    * Organization Changes
    
    * Update .gitignore
    
    * Move django app to server file
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: Eric Moss <yeswolfprod@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ server/accounts/backend.py
@@ -0,0 +1,52 @@
+"""
+COPYRIGHT 2022 MONTA
+
+This file is part of Monta.
+
+Monta is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Monta is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Monta.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from typing import Optional
+
+from django.contrib.auth.backends import ModelBackend
+
+from .models import User
+
+UserModel: type[User] = User
+
+
+class UserBackend(ModelBackend):
+    """User Authentication backend
+
+    This class is used to authenticate users using their user id.
+     Returns the user object if the user is authenticated.
+    Along with related profile, title and organization objects.
+    """
+
+    def get_user(self, user_id: int) -> Optional[User]:
+        """Get the user object.
+
+        Args:
+            user_id (int): parameter to get the primary key of the user.
+
+        Returns:
+            Optional[User]: Returns a user object if the user is authenticated.
+        """
+        try:
+            user = UserModel._default_manager.select_related(
+                "profile", "profile__title", "profile__organization", "organization"
+            ).get(pk__exact=user_id)
+        except UserModel.DoesNotExist:
+            return None
+        return user if self.user_can_authenticate(user) else None
---
+++ server/utils/views.py
@@ -0,0 +1,72 @@
+"""
+COPYRIGHT 2022 MONTA
+
+This file is part of Monta.
+
+Monta is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Monta is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Monta.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from braces import views
+from django.contrib.auth import mixins
+from django.views import generic
+
+
+class GenericTemplateView(
+    mixins.LoginRequiredMixin, views.PermissionRequiredMixin, generic.TemplateView
+):
+    """
+    Generic Template view with LoginRequiredMixin and PermissionRequiredMixin
+    """
+
+    pass
+
+
+class GenericListView(
+    mixins.LoginRequiredMixin, views.PermissionRequiredMixin, generic.ListView
+):
+    """
+    Generic List view with LoginRequiredMixin and PermissionRequiredMixin
+    """
+
+    pass
+
+
+class GenericView(
+    mixins.LoginRequiredMixin, views.PermissionRequiredMixin, generic.View
+):
+    """
+    Generic View with LoginRequiredMixin and PermissionRequiredMixin
+    """
+
+    pass
+
+
+class GenericCreateView(
+    mixins.LoginRequiredMixin, views.PermissionRequiredMixin, generic.CreateView
+):
+    """
+    Generic Create view with LoginRequiredMixin and PermissionRequiredMixin
+    """
+
+    pass
+
+
+class GenericUpdateView(
+    mixins.LoginRequiredMixin, views.PermissionRequiredMixin, generic.UpdateView
+):
+    """
+    Generic Update view with LoginRequiredMixin and PermissionRequiredMixin
+    """
+
+    pass
