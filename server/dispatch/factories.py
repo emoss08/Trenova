d81738af013f0dba25325a31bcdc098ca015c2a9
Wolfred <119015874+wolfredstep@users.noreply.github.com>
2022-12-01 04:26:33 -0500

    Verion 0 0 3 (#9)
    * add and change tests
    
    * worker tests
    
    * accounting tests
    
    * Create views.py
    
    * Factory Changes
    
    * Add organization middleware
    
    * Small user changes
    
    * Generic Classes implementation
    
    At some point these will be re-written to include re-usable code.
    
    * Update organization_middleware.py
    
    Fix mypy issue with organization
    
    * Update organization_middleware.py
    
    change to throw forbidden if user is not apart of an organization
    
    * Login View and changes to organization middleware
    
    * Update settings.py
    
    * Update .gitignore
    
    * Organization Changes
    
    * Update .gitignore
    
    * Move django app to server file
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: Eric Moss <yeswolfprod@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ server/dispatch/factories.py
@@ -0,0 +1,84 @@
+"""
+COPYRIGHT 2022 MONTA
+
+This file is part of Monta.
+
+Monta is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Monta is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Monta.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import factory
+
+
+class DispatchControlFactory(factory.django.DjangoModelFactory):
+    """
+    Dispatch control factory
+    """
+
+    organization = factory.SubFactory("organization.factories.OrganizationFactory")
+    regulatory_check = True
+
+    class Meta:
+        """
+        Metaclass for DispatchControlFactory
+        """
+
+        model = "dispatch.DispatchControl"
+
+
+class DelayCodeFactory(factory.django.DjangoModelFactory):
+    """
+    Delay code factory
+    """
+
+    organization = factory.SubFactory("organization.factories.OrganizationFactory")
+    code = factory.Faker("pystr", max_chars=4)
+
+    class Meta:
+        """
+        Metaclass for DelayCodeFactory
+        """
+
+        model = "dispatch.DelayCode"
+
+
+class FleetCodeFactory(factory.django.DjangoModelFactory):
+    """
+    Fleet code factory
+    """
+
+    organization = factory.SubFactory("organization.factories.OrganizationFactory")
+    code = factory.Faker("pystr", max_chars=4)
+
+    class Meta:
+        """
+        Metaclass for FleetCodeFactory
+        """
+
+        model = "dispatch.FleetCode"
+
+
+class CommentTypeFactory(factory.django.DjangoModelFactory):
+    """
+    Comment type factory
+    """
+
+    organization = factory.SubFactory("organization.factories.OrganizationFactory")
+    name = factory.Faker("word", locale="en_US")
+
+    class Meta:
+        """
+        Metaclass for CommentTypeFactory
+        """
+
+        model = "dispatch.CommentType"
