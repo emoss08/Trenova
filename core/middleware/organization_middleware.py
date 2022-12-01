783a34fac9d1a119f4255c823c1fe28306444f93
Eric Moss <yeswolfprod@gmail.com>
2022-12-01 02:12:49 -0500

    Add organization middleware

---
+++ core/middleware/organization_middleware.py
@@ -0,0 +1,38 @@
+"""
+COPYRIGHT 2022 MONTA
+
+This file is part of Monta.
+
+Monta is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Monta is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Monta.  If not, see <https://www.gnu.org/licenses/>.
+"""
+from django.http import HttpRequest, HttpResponse
+
+
+class OrganizationMiddleware:
+    """
+    Append organization to request
+    """
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request: HttpRequest) -> HttpResponse:
+
+        if request.user.is_authenticated:
+            request.organization = request.user.organization  # type: ignore
+        else:
+            request.organization = None
+
+        response = self.get_response(request)
+        return response
