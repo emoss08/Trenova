eeb95199a059bf1e818b36026975cf5b6c4b86c8
Eric Moss <yeswolfprod@gmail.com>
2022-12-02 16:57:36 -0500

    Authentication logic for Token Auth

--- backend/settings.py
+++ backend/settings.py
@@ -239,9 +239,10 @@ CACHEOPS = {
 # Rest Framework Configurations
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.BasicAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
     ]
 }
 
--- core/urls.py
+++ core/urls.py
@@ -17,18 +17,15 @@ You should have received a copy of the GNU General Public License
 along with Monta.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainSlidingView, TokenRefreshSlidingView
+from django.urls import include, path
+from rest_framework_simplejwt.views import TokenRefreshSlidingView
 
-from accounts.views import GenericTokenVerifyView
+from accounts.views import GenericTokenVerifyView, TokenObtainView
 
 urlpatterns = [
-    path("token/", TokenObtainSlidingView.as_view(), name="token_obtain"),
+    path("user/", include("accounts.urls")),
+    path("auth/", include('djoser.urls')),
+    path("token/", TokenObtainView.as_view(), name="token_obtain"),
     path("token/refresh/", TokenRefreshSlidingView.as_view(), name="token_refresh"),
     path("token/verify/", GenericTokenVerifyView.as_view(), name="token_verify"),
 ]
-
-# urlpatterns = [
-#     path("auth/", include("djoser.urls")),
-#     path("auth/", include("djoser.urls.jwt")),
-# ]
