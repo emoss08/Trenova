7844b7b44a26fd6e1edf318d3f7a5c4af426f4bb
Eric Moss <yeswolfprod@gmail.com>
2022-12-06 18:42:10 -0500

    Api And Local Storage Service

--- frontend/src/app/modules/auth/core/_requests.ts
+++ frontend/src/app/modules/auth/core/_requests.ts
@@ -1,22 +1,19 @@
-import axios from 'axios'
-import {AuthModel, UserModel} from './_models'
+import axios from "axios";
+import { AuthModel, UserModel } from "./_models";
+import ApiService from "../../../../_monta/helpers/ApiService";
 
-const API_URL = process.env.REACT_APP_API_URL
+const API_URL = process.env.REACT_APP_API_URL;
 
-export const GET_USER_BY_ACCESSTOKEN_URL = `${API_URL}/users/token/verify/`
-export const LOGIN_URL = `${API_URL}/users/token/provision/`
-export const REGISTER_URL = `${API_URL}/register`
-export const REQUEST_PASSWORD_URL = `${API_URL}/forgot_password`
+export const GET_USER_BY_ACCESSTOKEN_URL: string = `${API_URL}/token/verify/`;
+export const LOGIN_URL: string = `${API_URL}/token/provision/`;
+export const REGISTER_URL: string = `${API_URL}/register`;
+export const REQUEST_PASSWORD_URL: string = `${API_URL}/forgot_password`;
 
-// Server should return AuthModel
 export function login(username: string, password: string) {
-  return axios.post<AuthModel>(LOGIN_URL, {
-    username,
-    password,
-  })
+
+  return ApiService.post<AuthModel>(LOGIN_URL, { username, password });
 }
 
-// Server should return AuthModel
 export function register(
   email: string,
   firstname: string,
@@ -24,24 +21,24 @@ export function register(
   password: string,
   password_confirmation: string
 ) {
-  return axios.post(REGISTER_URL, {
+  return ApiService.post(REGISTER_URL, {
     email,
     first_name: firstname,
     last_name: lastname,
     password,
-    password_confirmation,
-  })
+    password_confirmation
+  });
+
 }
 
-// Server should return object => { result: boolean } (Is Email in DB)
 export function requestPassword(email: string) {
-  return axios.post<{result: boolean}>(REQUEST_PASSWORD_URL, {
-    email,
-  })
+  return axios.post<{ result: boolean }>(REQUEST_PASSWORD_URL, {
+    email
+  });
 }
 
 export function getUserByToken(token: string) {
-  return axios.post<UserModel>(GET_USER_BY_ACCESSTOKEN_URL, {
-    token: token,
-  })
+  return ApiService.post<UserModel>(GET_USER_BY_ACCESSTOKEN_URL, {
+    token: token
+  });
 }
