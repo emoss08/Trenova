3b547047f94e4eea21c00b3fba743e83dbc692ff
Eric Moss <yeswolfprod@gmail.com>
2022-12-06 20:54:21 -0500

    remove frontend

--- frontend/src/_monta/layout/core/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './_LayoutConfig'
-export * from './_LayoutProvider'
-export * from './_Models'
-export * from './_LayoutSetup'
-export * from './PageData'
-export * from './MetronicSplashScreen'
