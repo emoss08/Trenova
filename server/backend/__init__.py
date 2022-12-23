094389549b44a0e3c1b5c7ade04db438e3cff91c
Eric Moss <yeswolfprod@gmail.com>
2022-12-22 23:50:11 -0500

    re-add settings

---
+++ server/backend/__init__.py
@@ -0,0 +1,22 @@
+"""
+COPYRIGHT 2022 MONTA
+
+This file is part of Monta.
+
+Monta is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Monta is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Monta.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from .celery import app as celery_app
+
+__all__ = ("celery_app",)
