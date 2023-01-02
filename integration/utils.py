a84b12edb1b9e0bd0f5d3481f8c7c1c40527350f
Eric Moss <yeswolfprod@gmail.com>
2023-01-02 04:19:35 -0500

    Integration Base Utils

---
+++ integration/utils.py
@@ -0,0 +1,67 @@
+"""
+COPYRIGHT 2022 MONTA
+
+This file is part of Monta.
+
+Monta is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Monta is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Monta.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from typing import Dict
+
+from django.db.models.base import ModelBase
+
+from integration import models
+
+
+class IntegrationBase:
+    """
+    Blank for now.
+    """
+
+    model = None
+    headers: Dict = None
+
+    def _check(self):
+        """Checks to make sure that the type of the global variables are correct.
+
+        Raises:
+            TypeError: If the model is not a subclass of django.db.models.base.ModelBase.
+            TypeError: If the headers is not a dictionary.
+        """
+        if self.model and not isinstance(self.model, ModelBase):
+            raise TypeError(
+                f"{self.__class__.__name__}.model must be a subclass of ModelBase, not {type(self.model)}"
+            )
+
+        if self.headers and not isinstance(self.headers, Dict):
+            raise TypeError(
+                f"{self.__class__.__name__}.headers must be a dictionary, not {type(self.headers)}"
+            )
+
+    def get_fields(self):
+        """
+
+        Returns:
+
+        """
+        # Run the check to make sure that the model
+        self._check()
+
+        # Reverse = False, then relations point this model are not returned.
+        model_fields = self.model._meta._get_fields(reverse=False)
+
+
+class Test(IntegrationBase):
+    model = models.Integration
+    headers = {"test": "test"}
