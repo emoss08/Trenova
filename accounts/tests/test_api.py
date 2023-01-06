37ea078dcfcae5b99c598847075b595cb06acdb3
Eric Moss <yeswolfprod@gmail.com>
2023-01-06 12:45:26 -0500

    update accounts tests

--- accounts/tests/test_api.py
+++ accounts/tests/test_api.py
@@ -20,69 +20,105 @@ along with Monta.  If not, see <https://www.gnu.org/licenses/>.
 import pytest
 from rest_framework.test import APIClient
 
-from accounts.factories import TokenFactory, UserFactory
+from accounts.tests.factories import TokenFactory, UserFactory
 from organization.factories import OrganizationFactory
 
 client = APIClient()
 
-
-@pytest.fixture()
-def user():
-    """
-    User fixture
-    """
-    return UserFactory()
+pytestmark = pytest.mark.django_db
 
 
-@pytest.fixture()
-def token(user):
-    """
-    Token fixture
-    """
-    return TokenFactory(user=user)
+class TestAccountsAPI:
+    @pytest.fixture()
+    def user(self):
+        """
+        User fixture
+        """
+        return UserFactory()
 
+    @pytest.fixture()
+    def token(self, user):
+        """
+        Token fixture
+        """
+        return TokenFactory(user=user)
 
-@pytest.fixture()
-def organization(user):
-    """
-    Organization fixture
-    """
-    return OrganizationFactory()
+    @pytest.fixture()
+    def organization(self, user):
+        """
+        Organization fixture
+        """
+        return OrganizationFactory()
 
+    def test_get(self, token):
+        """
+        Test get user
+        """
+        client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
+        response = client.get("/api/users/")
+        assert response.status_code == 200
 
-@pytest.mark.django_db
-def test_get_user(token):
-    """
-    Test get user
-    """
-    client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
-    response = client.get("/api/users/")
-    assert response.status_code == 200
+    def test_get_with_id(self, user, token):
+        """
+        Test get user by ID
+        """
+        client.credentials(HTTP_AUTHORIZATION="TOKEN " + token.key)
+        response = client.get(f"/api/users/{user.id}/")
+        assert response.status_code == 200
 
+    def test_post(self, token, organization):
+        """
+        Test create user
+        """
+        client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
+        response = client.post(
+            "/api/users/",
+            {
+                "organization": organization.id,
+                "username": "test",
+                "email": "test@test.com",
+            },
+            format="json",
+        )
+        assert response.status_code == 201
+        assert response.data["username"] == "test"
+        assert response.data["email"] == "test@test.com"
 
-@pytest.mark.django_db
-def test_create_user(token, organization):
-    """
-    Test create user
-    """
-    client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
-    response = client.post(
-        "/api/users/",
-        {
-            "organization": organization.id,
-            "username": "test",
-            "password": "test12345",
-            "email": "test@test.com",
-        },
-    )
-    assert response.status_code == 201
+    def test_put(self, token, user):
+        """
+        Test Put request
+        """
+        client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
+        response = client.put(
+            f"/api/users/{user.id}/",
+            {
+                "username": "test",
+                "email": "test@test.com",
+                "profile": {
+                    "first_name": "test",
+                    "last_name": "user",
+                    "address_line_1": "test",
+                    "city": "test",
+                    "state": "NC",
+                    "zip_code": "12345",
+                },
+            },
+            format="json",
+        )
+        assert response.status_code == 200
+        assert response.data["username"] == "test"
+        assert response.data["email"] == "test@test.com"
+        assert response.data["profile"]["first_name"] == "test"
+        assert response.data["profile"]["last_name"] == "user"
+        assert response.data["profile"]["address_line_1"] == "test"
+        assert response.data["profile"]["city"] == "test"
+        assert response.data["profile"]["state"] == "NC"
+        assert response.data["profile"]["zip_code"] == "12345"
 
-
-@pytest.mark.django_db
-def test_delete_user(user, token):
-    """
-    Test delete user
-    """
-    client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
-    response = client.delete(f"/api/users/{str(user.id)}/")
-    assert response.status_code == 204
+    def test_delete(self, user, token):
+        """
+        Test delete user
+        """
+        client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
+        response = client.delete(f"/api/users/{user.id}/")
+        assert response.status_code == 204
