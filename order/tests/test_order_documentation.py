5dd83129f097bb6c64f1ac003b9c33ca8caff90f
Eric Moss <yeswolfprod@gmail.com>
2023-01-14 16:01:14 -0500

    Update test_order_documentation.py

--- order/tests/test_order_documentation.py
+++ order/tests/test_order_documentation.py
@@ -18,6 +18,10 @@ along with Monta.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import os
+import shutil
+from pathlib import Path
+from time import sleep
+
 import pytest
 
 from billing.tests.factories import DocumentClassificationFactory
@@ -145,6 +149,7 @@ class TestOrderDocumentationApi(ApiTest):
         """
         Test get Order Documentation by ID
         """
+
         response = api_client.get(
             f"/api/order_documents/{order_documentation.data['id']}/"
         )
@@ -158,3 +163,82 @@ class TestOrderDocumentationApi(ApiTest):
         if os.path.exists("testfile.txt"):
             # Remove file once it is generated
             return os.remove("testfile.txt")
+
+    def test_put(self, api_client, order, order_documentation, document_classification):
+        """
+        Test put Order Documentation by ID
+        """
+
+        fpath = "putfile.txt"
+        test_file = open(fpath, "w")
+        test_file.write("Hello World")
+        test_file.close()
+        test_file = open(fpath, "r")
+
+        response = api_client.put(
+            f"/api/order_documents/{order_documentation.data['id']}/",
+            {
+                "order": f"{order.id}",
+                "document": test_file,
+                "document_class": f"{document_classification.id}"
+            }
+        )
+
+        assert response.data is not None
+        assert response.status_code == 200
+        assert response.data['order'] == order.id
+        assert response.data['document'] is not None
+        assert response.data['document_class'] == document_classification.id
+
+        if os.path.exists(fpath):
+            # Remove file once it is generated
+            return os.remove(fpath)
+
+    def test_patch(self, api_client, order, order_documentation, document_classification):
+        """
+        Test patch Order Documentation by ID
+        """
+
+        fpath = "patchfile.txt"
+        test_file = open(fpath, "w")
+        test_file.write("Hello World")
+        test_file.close()
+        test_file = open(fpath, "r")
+
+        response = api_client.put(
+            f"/api/order_documents/{order_documentation.data['id']}/",
+            {
+                "order": f"{order.id}",
+                "document": test_file,
+                "document_class": f"{document_classification.id}"
+            }
+        )
+
+        assert response.data is not None
+        assert response.status_code == 200
+        assert response.data['order'] == order.id
+        assert response.data['document'] is not None
+        assert response.data['document_class'] == document_classification.id
+
+        if os.path.exists(fpath):
+            # Remove file once it is generated
+            return os.remove(fpath)
+
+    def test_delete(self, api_client, order_documentation):
+        """
+        Test Delete by ID
+        """
+
+        response = api_client.delete(f"/api/order_documents/{order_documentation.data['id']}/")
+
+        assert response.status_code == 204
+        assert response.data is None
+
+        if os.path.exists("testfile.txt"):
+            return os.remove("testfile.txt")
+
+    def test_tear_down(self):
+        """
+        Tear down tests
+        """
+        self.remove_media_directory("order_documentation")
