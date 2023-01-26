a1244bfd5e0f10cc456ade784fc5066023998048
Eric Moss <yeswolfprod@gmail.com>
2023-01-25 23:22:49 -0500

    ADD: Text to startup & Update Requirements
    - this is just for fun.

--- backend/asgi.py
+++ backend/asgi.py
@@ -6,16 +6,19 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
-
 import os
-
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
+from art import *
+from rich.console import Console
+
+console = Console()
+logo = text2art("MONTA", font="Larry 3D")
+console.print(logo, style="bold purple")
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 application = get_asgi_application()
-
 application = ProtocolTypeRouter(
     {
         "http": application,
--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from django.core.management import call_command
 
 
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "core"
+
