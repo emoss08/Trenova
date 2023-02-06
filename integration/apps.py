d24a20c3d377ab11c9c13f88eceeec0b9b3622ab
Eric Moss <yeswolfprod@gmail.com>
2023-02-05 23:00:59 -0500

    ADD: Integration Hook

--- integration/apps.py
+++ integration/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class IntegrationConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "integration"
-
-    def ready(self) -> None:
-        from integration import signals
