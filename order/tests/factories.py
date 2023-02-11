7d2d85147a6267eba2cc3feb8e84f87b5d6ee947
Eric Moss <yeswolfprod@gmail.com>
2023-02-10 21:27:59 -0500

    CHANGE: Order Model Fields to Money Field

--- order/tests/factories.py
+++ order/tests/factories.py
@@ -171,6 +171,6 @@ class AdditionalChargeFactory(factory.django.DjangoModelFactory):
     organization = factory.SubFactory("organization.factories.OrganizationFactory")
     order = factory.SubFactory(OrderFactory)
     charge = factory.SubFactory("billing.tests.factories.AccessorialChargeFactory")
-    charge_amount = FuzzyDecimal(low=10.00, high=100000.00, precision=2)
-    sub_total = FuzzyDecimal(low=10.00, high=100000.00, precision=2)
+    charge_amount = FuzzyDecimal(low=10.00, high=100000.00, precision=4)
+    sub_total = FuzzyDecimal(low=10.00, high=100000.00, precision=4)
     entered_by = factory.SubFactory("accounts.tests.factories.UserFactory")
