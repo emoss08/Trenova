496a0689bc642821b508468a7b839082772db173
Eric Moss <yeswolfprod@gmail.com>
2023-03-03 16:17:35 -0500

    Fix routing by adding suspense.

--- frontend_app/src/routing/PrivateRoutes.tsx
+++ frontend_app/src/routing/PrivateRoutes.tsx
@@ -17,15 +17,23 @@
  * along with Monta.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import React, {lazy} from 'react'
+import React, {lazy, Suspense} from 'react'
 import {Navigate, Route, Routes} from 'react-router-dom'
+// import SystemHealthPage from '../pages/SystemHealthPage'
 
 const PrivateRoutes = () => {
   const SystemHealthPage = lazy(() => import('../pages/SystemHealthPage'))
 
   return (
     <Routes>
-      <Route path='system_health' element={<SystemHealthPage />}></Route>
+      <Route
+        path='/system_health'
+        element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <SystemHealthPage />
+          </Suspense>
+        }
+      ></Route>
       {/* Page Not Found */}
       <Route path='*' element={<Navigate to='/error/404' />} />
     </Routes>
