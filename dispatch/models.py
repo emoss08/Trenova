9896223e9394d0a1dfe79eb2c6da8a0a535f20f4
Eric Moss <yeswolfprod@gmail.com>
2023-03-03 00:45:24 -0500

    CHANGE: Copyright Notice

--- dispatch/models.py
+++ dispatch/models.py
@@ -1,21 +1,21 @@
-"""
-COPYRIGHT 2022 MONTA
-
-This file is part of Monta.
-
-Monta is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Monta is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Monta.  If not, see <https://www.gnu.org/licenses/>.
-"""
+# --------------------------------------------------------------------------------------------------
+#  COPYRIGHT(c) 2023 MONTA                                                                         -
+#                                                                                                  -
+#  This file is part of Monta.                                                                     -
+#                                                                                                  -
+#  Monta is free software: you can redistribute it and/or modify                                   -
+#  it under the terms of the GNU General Public License as published by                            -
+#  the Free Software Foundation, either version 3 of the License, or                               -
+#  (at your option) any later version.                                                             -
+#                                                                                                  -
+#  Monta is distributed in the hope that it will be useful,                                        -
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of                                  -
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                   -
+#  GNU General Public License for more details.                                                    -
+#                                                                                                  -
+#  You should have received a copy of the GNU General Public License                               -
+#  along with Monta.  If not, see <https://www.gnu.org/licenses/>.                                 -
+# --------------------------------------------------------------------------------------------------
 
 import textwrap
 import uuid
@@ -35,7 +35,7 @@ from integration.models import IntegrationChoices
 from organization.models import Organization
 from utils.models import ChoiceField, GenericModel, RatingMethodChoices
 
-User = settings.AUTH_USER_MODEL
+User: str = settings.AUTH_USER_MODEL
 
 
 class DispatchControl(GenericModel):
