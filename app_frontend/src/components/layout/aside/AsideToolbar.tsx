70dbd1fcca14ee7c54961aea55da6b490073f488
Eric Moss <yeswolfprod@gmail.com>
2023-03-08 20:21:15 -0500

    Implement JobStore

--- app_frontend/src/components/layout/aside/AsideToolbar.tsx
+++ app_frontend/src/components/layout/aside/AsideToolbar.tsx
@@ -24,48 +24,34 @@ import avatar3001 from "../../../../public/media/avatars/300-1.jpg";
 import cod001 from "../../../../public/media/icons/duotune/coding/cod001.svg";
 import { MTSVG } from "@/components/elements/MTSVG";
 import SvgCod001 from "@/components/svgs/SvgCod001";
+import { jobStore } from "@/utils/stores";
 
 /* eslint-disable jsx-a11y/anchor-is-valid */
 const AsideToolbar = () => {
   const [user] = authStore.use("user");
-
+  const [job] = jobStore.use("job");
   return (
     <>
-      {/*begin::User*/}
       <div className="aside-user d-flex align-items-sm-center justify-content-center py-5">
-        {/*begin::Symbol*/}
         <div className="symbol symbol-50px">
           <Image src={avatar3001} alt="" />
         </div>
-        {/*end::Symbol*/}
 
-        {/*begin::Wrapper*/}
         <div className="aside-user-info flex-row-fluid flex-wrap ms-5">
-          {/*begin::Section*/}
           <div className="d-flex">
-            {/*begin::Info*/}
             <div className="flex-grow-1 me-2">
-              {/*begin::Username*/}
               <a href="#" className="text-white text-hover-primary fs-6 fw-bold">
                 {user?.first_name} {user?.last_name}
               </a>
-              {/*end::Username*/}
 
-              {/*begin::Description*/}
-              <span className="text-gray-600 fw-bold d-block fs-8 mb-1">Python dev</span>
-              {/*end::Description*/}
+              <span className="text-gray-600 fw-bold d-block fs-8 mb-1">{job?.name}</span>
 
-              {/*begin::Label*/}
               <div className="d-flex align-items-center text-success fs-9">
                 <span className="bullet bullet-dot bg-success me-1"></span>online
               </div>
-              {/*end::Label*/}
             </div>
-            {/*end::Info*/}
 
-            {/*begin::User menu*/}
             <div className="me-n2">
-              {/*begin::Action*/}
               <a
                 href="#"
                 className="btn btn-icon btn-sm btn-active-color-primary mt-n2"
@@ -77,21 +63,14 @@ const AsideToolbar = () => {
               </a>
 
               <HeaderUserMenu />
-              {/*end::Action*/}
             </div>
-            {/*end::User menu*/}
           </div>
-          {/*end::Section*/}
         </div>
-        {/*end::Wrapper*/}
       </div>
-      {/*end::User*/}
 
-      {/*begin::Aside search*/}
       <div className="aside-search py-5">
         <Search />
       </div>
-      {/*end::Aside search*/}
     </>
   );
 };
