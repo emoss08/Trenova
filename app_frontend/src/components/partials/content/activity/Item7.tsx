eb035947ad9d5b093849870c014d1cf41c04757c
Eric Moss <yeswolfprod@gmail.com>
2023-03-11 13:50:35 -0500

    CHANGE: Copyright Header in files

--- app_frontend/src/components/partials/content/activity/Item7.tsx
+++ app_frontend/src/components/partials/content/activity/Item7.tsx
@@ -3,23 +3,21 @@
  *
  * This file is part of Monta.
  *
- * Monta is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Monta is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Monta.  If not, see <https://www.gnu.org/licenses/>.
+ * The Monta software is licensed under the Business Source License 1.1. You are granted the right
+ * to copy, modify, and redistribute the software, but only for non-production use or with a total
+ * of less than three server instances. Starting from the Change Date (November 16, 2026), the
+ * software will be made available under version 2 or later of the GNU General Public License.
+ * If you use the software in violation of this license, your rights under the license will be
+ * terminated automatically. The software is provided "as is," and the Licensor disclaims all
+ * warranties and conditions. If you use this license's text or the "Business Source License" name
+ * and trademark, you must comply with the Licensor's covenants, which include specifying the
+ * Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use
+ * Grant, and not modifying the license in any other way.
  */
 
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import {FC} from 'react'
-import { toAbsoluteUrl } from "@/utils/AssetHelpers";
+import { toAbsoluteUrl } from "@/utils/helpers/AssetHelpers";
 
 const Item7: FC = () => {
   return (
