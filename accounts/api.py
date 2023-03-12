c145c5b5a115e8b122e98b7c651baeb87ab79996
Eric Moss <yeswolfprod@gmail.com>
2023-03-11 22:36:11 -0500

    Add custom permissions class
    Add custom permissions class to prevent users without proper model permissions from viewing, editing, and deleting data.

--- accounts/api.py
+++ accounts/api.py
@@ -3,18 +3,16 @@
 #                                                                                                  -
 #  This file is part of Monta.                                                                     -
 #                                                                                                  -
-#  Monta is free software: you can redistribute it and/or modify                                   -
-#  it under the terms of the GNU General Public License as published by                            -
-#  the Free Software Foundation, either version 3 of the License, or                               -
-#  (at your option) any later version.                                                             -
-#                                                                                                  -
-#  Monta is distributed in the hope that it will be useful,                                        -
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of                                  -
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                   -
-#  GNU General Public License for more details.                                                    -
-#                                                                                                  -
-#  You should have received a copy of the GNU General Public License                               -
-#  along with Monta.  If not, see <https://www.gnu.org/licenses/>.                                 -
+#  The Monta software is licensed under the Business Source License 1.1. You are granted the right -
+#  to copy, modify, and redistribute the software, but only for non-production use or with a total -
+#  of less than three server instances. Starting from the Change Date (November 16, 2026), the     -
+#  software will be made available under version 2 or later of the GNU General Public License.     -
+#  If you use the software in violation of this license, your rights under the license will be     -
+#  terminated automatically. The software is provided "as is," and the Licensor disclaims all      -
+#  warranties and conditions. If you use this license's text or the "Business Source License" name -
+#  and trademark, you must comply with the Licensor's covenants, which include specifying the      -
+#  Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use     -
+#  Grant, and not modifying the license in any other way.                                          -
 # --------------------------------------------------------------------------------------------------
 
 from typing import Any
@@ -25,6 +23,7 @@ from rest_framework import generics, request, response, views
 from accounts import models, serializers
 from utils.exceptions import InvalidTokenException
 from utils.views import OrganizationMixin
+from utils.permissions import MontaModelPermissions
 
 
 class UserViewSet(OrganizationMixin):
@@ -37,6 +36,7 @@ class UserViewSet(OrganizationMixin):
     search_fields = ["username", "email", "profiles__first_name", "profiles__last_name"]
     filterset_fields = ["department__name", "is_staff", "username"]
     ordering_fields = "__all__"
+    permission_classes = [MontaModelPermissions]
 
     def get_queryset(self) -> QuerySet[models.User]:  # type: ignore
         """Filter the queryset to only include the current user
@@ -46,7 +46,7 @@ class UserViewSet(OrganizationMixin):
         """
 
         return self.queryset.filter(
-            organization_id=self.request.user.organization_id  # type: ignore
+            organization=self.request.user.organization  # type: ignore
         ).select_related(
             "organization",
             "profiles",
