41b628f4c4d4bc215a948ac670985541228431cb
Eric Moss <yeswolfprod@gmail.com>
2023-03-15 11:23:04 -0400

    Finalize Navigation bar, and WIP keepaliveconnection
    Keep Alive connection will provide a websocket connection to the backend application that can be used to send messages to users, and additionally, it will be used to ensure that the NextJS application is able to maintain it's connection to the Django API. If the Django API is down, the goal is to put the frontend application in a mode so that users are unable to execute actions until it has been restored.

--- app_frontend/src/components/layout/aside/AsideToolbar.tsx
+++ app_frontend/src/components/layout/aside/AsideToolbar.tsx
@@ -3,36 +3,32 @@
  *
  * This file is part of Monta.
  *
- * Monta is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Monta is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Monta.  If not, see <https://www.gnu.org/licenses/>.
+ * The Monta software is licensed under the Business Source License 1.1. You are granted the right
+ * to copy, modify, and redistribute the software, but only for non-production use or with a total
+ * of less than three server instances. Starting from the Change Date (November 16, 2026), the
+ * software will be made available under version 2 or later of the GNU General Public License.
+ * If you use the software in violation of this license, your rights under the license will be
+ * terminated automatically. The software is provided "as is," and the Licensor disclaims all
+ * warranties and conditions. If you use this license's text or the "Business Source License" name 
+ * and trademark, you must comply with the Licensor's covenants, which include specifying the
+ * Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use
+ * Grant, and not modifying the license in any other way.
  */
 
 import { authStore } from "@/utils/providers/AuthGuard";
 import { HeaderUserMenu, Search } from "@/components/partials";
-import Image from "next/image";
-import avatar3001 from "../../../../public/media/avatars/300-1.jpg";
 import { MTSVG } from "@/components/elements/MTSVG";
 import SvgCod001 from "@/components/svgs/SvgCod001";
+import { MontaSymbol } from "@/components/partials/MontaSymbol";
 
 /* eslint-disable jsx-a11y/anchor-is-valid */
 const AsideToolbar = () => {
   const [user] = authStore.use("user");
+  
   return (
     <>
       <div className="aside-user d-flex align-items-sm-center justify-content-center py-5">
-        <div className="symbol symbol-50px">
-          <Image src={avatar3001} alt="" />
-        </div>
+        <MontaSymbol colorClass={'bg-primary text-inverse-primary'} text={user?.first_name[0]} />
 
         <div className="aside-user-info flex-row-fluid flex-wrap ms-5">
           <div className="d-flex">
