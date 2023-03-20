7d716d0b47eb24e449310e8796320d1c831c1ee5
Eric Moss <yeswolfprod@gmail.com>
2023-03-19 22:39:03 -0400

    Change shared_task to app.task

--- core/tasks.py
+++ core/tasks.py
@@ -1,29 +1,27 @@
-"""
-COPYRIGHT 2023 MONTA
-
-This file is part of Monta.
-
-Monta is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Monta is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Monta.  If not, see <https://www.gnu.org/licenses/>.
-"""
-
+# --------------------------------------------------------------------------------------------------
+#  COPYRIGHT(c) 2023 MONTA                                                                         -
+#                                                                                                  -
+#  This file is part of Monta.                                                                     -
+#                                                                                                  -
+#  The Monta software is licensed under the Business Source License 1.1. You are granted the right -
+#  to copy, modify, and redistribute the software, but only for non-production use or with a total -
+#  of less than three server instances. Starting from the Change Date (November 16, 2026), the     -
+#  software will be made available under version 2 or later of the GNU General Public License.     -
+#  If you use the software in violation of this license, your rights under the license will be     -
+#  terminated automatically. The software is provided "as is," and the Licensor disclaims all      -
+#  warranties and conditions. If you use this license's text or the "Business Source License" name -
+#  and trademark, you must comply with the Licensor's covenants, which include specifying the      -
+#  Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use     -
+#  Grant, and not modifying the license in any other way.                                          -
+# --------------------------------------------------------------------------------------------------
 
 import datetime
 
-from celery import shared_task
 from django.core.management import call_command
 from django.utils import timezone
 from kombu.exceptions import OperationalError
+
+from backend.celery import app
 
 
 def get_cutoff_date() -> datetime.datetime:
@@ -36,7 +34,7 @@ def get_cutoff_date() -> datetime.datetime:
     return timezone.now() - datetime.timedelta(days=30)
 
 
-@shared_task(bind=True)
+@app.task(name='delete_audit_log_records', bind=True, max_retries=3, default_retry_delay=60)
 def delete_audit_log_records(self) -> str:
     """Delete audit log records older than 30 days.
 
