bca828f57ee57dca36c763662e356f855db50dac
Eric Moss <yeswolfprod@gmail.com>
2023-03-19 22:33:34 -0400

    Change createtestusers command
    Change command to include progress bar.

--- accounts/management/commands/createtestusers.py
+++ accounts/management/commands/createtestusers.py
@@ -3,18 +3,16 @@
 #                                                                                                  -
 #  This file is part of Monta.                                                                     -
 #                                                                                                  -
-#  Monta is free software: you can redistribute it and/or modify                                   -
-#  it under the terms of the GNU General Public License as published by                            -
-#  the Free Software Foundation, either version 3 of the License, or                               -
-#  (at your option) any later version.                                                             -
-#                                                                                                  -
-#  Monta is distributed in the hope that it will be useful,                                        -
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of                                  -
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                   -
-#  GNU General Public License for more details.                                                    -
-#                                                                                                  -
-#  You should have received a copy of the GNU General Public License                               -
-#  along with Monta.  If not, see <https://www.gnu.org/licenses/>.                                 -
+#  The Monta software is licensed under the Business Source License 1.1. You are granted the right -
+#  to copy, modify, and redistribute the software, but only for non-production use or with a total -
+#  of less than three server instances. Starting from the Change Date (November 16, 2026), the     -
+#  software will be made available under version 2 or later of the GNU General Public License.     -
+#  If you use the software in violation of this license, your rights under the license will be     -
+#  terminated automatically. The software is provided "as is," and the Licensor disclaims all      -
+#  warranties and conditions. If you use this license's text or the "Business Source License" name -
+#  and trademark, you must comply with the Licensor's covenants, which include specifying the      -
+#  Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use     -
+#  Grant, and not modifying the license in any other way.                                          -
 # --------------------------------------------------------------------------------------------------
 from typing import Any
 
@@ -25,11 +23,10 @@ from accounts.models import JobTitle, UserProfile
 from organization.models import Organization
 import random
 import string
-
+from rich.progress import Progress
 
 class Command(BaseCommand):
-    """
-        A Django management command to create test users for a given organization.
+    """A Django management command to create test users for a given organization.
 
     Prompts the user for the name of the organization and the number of users to create.
     If the organization does not exist, raises a CommandError.
@@ -74,13 +71,13 @@ class Command(BaseCommand):
 
         try:
             system_org = Organization.objects.get(name=system_org_answer)
-        except Organization.DoesNotExist:
-            raise CommandError(f"Organization {system_org_answer} does not exist.")
+        except Organization.DoesNotExist as e:
+            raise CommandError(f"Organization {system_org_answer} does not exist.") from e
 
         try:
             number_of_users = int(number_of_users_answer)
-        except ValueError:
-            raise CommandError(f"{number_of_users_answer} is not a valid number.")
+        except ValueError as e:
+            raise CommandError(f"{number_of_users_answer} is not a valid number.") from e
 
         User = get_user_model()
 
@@ -96,49 +93,54 @@ class Command(BaseCommand):
         )
 
         new_users = []
-        for i in range(number_of_users):
-            if usernames[i] in existing_users:
-                self.stdout.write(
-                    self.style.WARNING(
-                        f"User {usernames[i]} already exists. Skipping..."
-                    )
+        with Progress() as progress:
+            task = progress.add_task("[cyan]Creating test users...", total=number_of_users)
+
+            for i in range(number_of_users):
+                if usernames[i] in existing_users:
+                    progress.update(task, advance=1)
+                    continue
+
+                email = f"testuser-{i}@monta.io"
+                password = "testuser".join(
+                    random.choices(string.ascii_uppercase + string.digits, k=8)
                 )
-                continue
 
-            email = f"testuser-{i}@monta.io"
-            password = "testuser".join(
-                random.choices(string.ascii_uppercase + string.digits, k=8)
-            )
-
-            new_user = User(
-                username=usernames[i],
-                email=email,
-                password=password,
-                organization=system_org,
-            )
-            new_users.append(new_user)
+                new_user = User(
+                    username=usernames[i],
+                    email=email,
+                    password=password,
+                    organization=system_org,
+                )
+                new_users.append(new_user)
+                progress.update(task, advance=1)
 
         User.objects.bulk_create(new_users)
 
         new_profiles = []
-        for i in range(number_of_users):
-            if usernames[i] in existing_users:
-                continue
+        with Progress() as progress:
+            task = progress.add_task("[cyan]Creating user profiles...", total=number_of_users)
 
-            user = User.objects.get(username=usernames[i])
-            new_profile = UserProfile(
-                user=user,
-                organization=system_org,
-                title=job_title,
-                first_name="Test",
-                last_name=f"User-{i}",
-                address_line_1="123 Test Street",
-                city="Test City",
-                state="CA",
-                zip_code="12345",
-                phone_number="123-456-7890",
-            )
-            new_profiles.append(new_profile)
+            for i in range(number_of_users):
+                if usernames[i] in existing_users:
+                    progress.update(task, advance=1)
+                    continue
+
+                user = User.objects.get(username=usernames[i])
+                new_profile = UserProfile(
+                    user=user,
+                    organization=system_org,
+                    title=job_title,
+                    first_name="Test",
+                    last_name=f"User-{i}",
+                    address_line_1="123 Test Street",
+                    city="Test City",
+                    state="CA",
+                    zip_code="12345",
+                    phone_number="123-456-7890",
+                )
+                new_profiles.append(new_profile)
+                progress.update(task, advance=1)
 
         UserProfile.objects.bulk_create(new_profiles)
 
