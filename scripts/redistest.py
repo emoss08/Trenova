b769c242e0fb7b425f19b59220bed5c56d7ee7bb
Eric Moss <yeswolfprod@gmail.com>
2023-03-28 12:10:21 -0400

    Update copyright

--- scripts/redistest.py
+++ scripts/redistest.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python3
+
+# --------------------------------------------------------------------------------------------------
+#  COPYRIGHT(c) 2023 MONTA                                                                         -
+#                                                                                                  -
+#  This file is part of Monta.                                                                     -
+#                                                                                                  -
+#  The Monta software is licensed under the Business Source License 1.1. You are granted the right -
+#  to copy, modify, and redistribute the software, but only for non-production use or with a total -
+#  of less than three server instances. Starting from the Change Date (November 16, 2026), the     -
+#  software will be made available under version 2 or later of the GNU General Public License.     -
+#  If you use the software in violation of this license, your rights under the license will be     -
+#  terminated automatically. The software is provided "as is," and the Licensor disclaims all      -
+#  warranties and conditions. If you use this license's text or the "Business Source License" name -
+#  and trademark, you must comply with the Licensor's covenants, which include specifying the      -
+#  Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use     -
+#  Grant, and not modifying the license in any other way.                                          -
+# --------------------------------------------------------------------------------------------------
+
 import time
+from typing import Dict
 
 import redis
 
 redis_client = redis.Redis(host="localhost", port=6379, db=0, decode_responses=True)
 
 
-def redis_set(data: dict) -> int:
+def redis_set(data: Dict) -> int:
     """
     Sets key-value pairs in Redis.
 
@@ -21,7 +40,7 @@ def redis_set(data: dict) -> int:
     return len(data)
 
 
-def redis_get(data) -> int:
+def redis_get(data: Dict) -> int:
     """
     Retrieves values for given keys from Redis.
 
