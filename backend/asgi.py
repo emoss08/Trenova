af11ad0bd3ff3e10cfbec5644946c2eaf8804a6c
Eric Moss <yeswolfprod@gmail.com>
2023-03-29 22:20:11 -0400

    Update asgi.py

--- backend/asgi.py
+++ backend/asgi.py
@@ -16,15 +16,16 @@
 # --------------------------------------------------------------------------------------------------
 
 import os
-
+import sys
 from art import *
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from rich.console import Console
 from organization.routing import websocket_urlpatterns
-import warnings
 
-warnings.warn("Running on PyPy is not fully supported. Be aware some features may not work as expected.", RuntimeWarning)
+if sys.implementation.name == "pypy":
+    import warnings
+    warnings.warn("Running on PyPy is not fully supported. Be aware some features may not work as expected.", RuntimeWarning)
 
 console = Console()
 logo = text2art("MONTA", font="Larry 3D")  # type: ignore
