94a24d3de43e603b5740af196c42dc83c649bd0a
Eric Moss <yeswolfprod@gmail.com>
2023-04-06 00:23:58 -0400

    Add average wait_time to locations api
    Add field to show the average wait_time of a location.

--- location/api.py
+++ location/api.py
@@ -1,23 +1,26 @@
-"""
-COPYRIGHT 2022 MONTA
+# --------------------------------------------------------------------------------------------------
+#  COPYRIGHT(c) 2023 MONTA                                                                         -
+#                                                                                                  -
+#  This file is part of Monta.                                                                     -
+#                                                                                                  -
+#  The Monta software is licensed under the Business Source License 1.1. You are granted the right -
+#  to copy, modify, and redistribute the software, but only for non-production use or with a total -
+#  of less than three server instances. Starting from the Change Date (November 16, 2026), the     -
+#  software will be made available under version 2 or later of the GNU General Public License.     -
+#  If you use the software in violation of this license, your rights under the license will be     -
+#  terminated automatically. The software is provided "as is," and the Licensor disclaims all      -
+#  warranties and conditions. If you use this license's text or the "Business Source License" name -
+#  and trademark, you must comply with the Licensor's covenants, which include specifying the      -
+#  Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use     -
+#  Grant, and not modifying the license in any other way.                                          -
+# --------------------------------------------------------------------------------------------------
 
-This file is part of Monta.
+from typing import Any
 
-Monta is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+from requests import Response
+from rest_framework.request import Request
 
-Monta is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Monta.  If not, see <https://www.gnu.org/licenses/>.
-"""
-
-from location import models, serializers
+from location import models, serializers, selectors
 from utils.views import OrganizationMixin
 
 
@@ -53,6 +56,17 @@ class LocationViewSet(OrganizationMixin):
     serializer_class = serializers.LocationSerializer
     filterset_fields = ("location_category__name", "depot__name", "is_geocoded")
 
+    def list(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        response = super().list(request, *args, **kwargs)
+
+        locations = response.data["results"]
+
+        for location in locations:
+            location_obj = models.Location.objects.get(id=location["id"])
+            wait_time_avg = selectors.get_avg_wait_time(location=location_obj)
+            location["wait_time_avg"] = wait_time_avg
+
+        return response
 
 class LocationContactViewSet(OrganizationMixin):
     """A viewset for viewing and editing Location Contact information in the system.
