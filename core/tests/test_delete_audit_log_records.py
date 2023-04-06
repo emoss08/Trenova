e884558434725d1a14afc795b18b9dce6bb2f932
Eric Moss <yeswolfprod@gmail.com>
2023-04-06 00:23:24 -0400

    Update test_delete_audit_log_records.py

--- core/tests/test_delete_audit_log_records.py
+++ core/tests/test_delete_audit_log_records.py
@@ -1,37 +1,26 @@
-"""
-COPYRIGHT 2022 MONTA
+# --------------------------------------------------------------------------------------------------
+#  COPYRIGHT(c) 2023 MONTA                                                                         -
+#                                                                                                  -
+#  This file is part of Monta.                                                                     -
+#                                                                                                  -
+#  The Monta software is licensed under the Business Source License 1.1. You are granted the right -
+#  to copy, modify, and redistribute the software, but only for non-production use or with a total -
+#  of less than three server instances. Starting from the Change Date (November 16, 2026), the     -
+#  software will be made available under version 2 or later of the GNU General Public License.     -
+#  If you use the software in violation of this license, your rights under the license will be     -
+#  terminated automatically. The software is provided "as is," and the Licensor disclaims all      -
+#  warranties and conditions. If you use this license's text or the "Business Source License" name -
+#  and trademark, you must comply with the Licensor's covenants, which include specifying the      -
+#  Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use     -
+#  Grant, and not modifying the license in any other way.                                          -
+# --------------------------------------------------------------------------------------------------
 
-This file is part of Monta.
-
-Monta is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Monta is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Monta.  If not, see <https://www.gnu.org/licenses/>.
-"""
-
-import datetime
 from unittest.mock import patch
 
 import pytest
 from celery.exceptions import Retry
 
-from core.tasks import delete_audit_log_records, get_cutoff_date
-
-
-def test_get_cutoff_date():
-    """
-    Test the get_cutoff_date function.
-    """
-    cutoff_date: datetime.datetime = get_cutoff_date()
-    assert isinstance(cutoff_date, datetime.datetime)
+from core.tasks import delete_audit_log_records
 
 
 @patch("core.tasks.call_command")
@@ -39,14 +28,11 @@ def test_delete_audit_log_records(patched_call_command):
     """
     Test the delete_audit_log_records function.
     """
-    cutoff_date: datetime.datetime = get_cutoff_date()
-    formatted_date: str = cutoff_date.strftime("%Y-%m-%d")
     delete_audit_log_records()
 
     assert patched_call_command.call_count == 1
     assert patched_call_command.call_args_list[0][0][0] == "auditlogflush"
     assert patched_call_command.call_args_list[0][0][1] == "-b"
-    assert patched_call_command.call_args_list[0][0][2] == formatted_date
     assert patched_call_command.call_args_list[0][0][3] == "-y"
     assert patched_call_command.call_args_list[0][1] == {}
 
@@ -61,4 +47,3 @@ def test_delete_audit_log_records_retry(patched_call_command):
         delete_audit_log_records()
 
 
-# Path: core\tests\test_delete_audit_log_records.py
