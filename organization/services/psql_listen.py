9cb2401c6218b7b1689b1a868c3dcfe9baaf6488
Eric Moss <yeswolfprod@gmail.com>
2023-04-29 23:48:52 -0400

    Fix bug report - #93

--- organization/services/psql_listen.py
+++ organization/services/psql_listen.py
@@ -29,33 +29,8 @@ environ.Env.read_env(os.path.join(ENV_DIR, ".env"))
 
 
 class PSQLListener:
-    """
-    Listens for table change notifications from a PostgreSQL database and prints them to the console.
-
-    This class provides a `listen()` method that retrieves a list of active table change alerts from the database
-    using the `get_active_table_alerts()` function. It then sets up a PostgreSQL connection using the `connect()`
-    method, and registers a PostgreSQL listener for each alert. When a notification is received on a channel, the
-    `listen()` method prints the notification details to the console.
-
-    The `connect()` method is a class method that creates a connection to the PostgreSQL database using the
-    `psycopg2.connect()` function, and sets the `autocommit` attribute to `True`.
-
-    Example usage:
-    >>>    listener = PSQLListener()
-    >>>    listener.listen()
-    """
-
     @classmethod
     def connect(cls):
-        """
-        Creates a connection to a PostgreSQL database.
-
-        Returns:
-            psycopg2.connection: A connection to the PostgreSQL database.
-
-        Raises:
-            None.
-        """
         conn = psycopg2.connect(
             host="localhost",
             database=env("DB_NAME"),
@@ -68,20 +43,6 @@ class PSQLListener:
 
     @classmethod
     def listen(cls) -> None:
-        """
-        Listens for table change notifications and prints them to the console.
-
-        This method retrieves a list of active table change alerts from the database using the
-        `get_active_table_alerts()` function. It then sets up a PostgreSQL connection using the `connect()`
-        method, and registers a PostgreSQL listener for each alert. When a notification is received on a channel,
-        the method prints the notification details to the console.
-
-        Returns:
-            None.
-
-        Raises:
-            None.
-        """
         conn = cls.connect()
         table_changes = get_active_table_alerts()
 
