c48b1b405d2f117ceabf0197f54f8bb4da2cab75
Eric Moss <yeswolfprod@gmail.com>
2023-06-18 23:25:11 -0400

    fix(billing): fix N+1 queries

--- billing/api.py
+++ billing/api.py
@@ -154,39 +154,35 @@ class BillingHistoryViewSet(viewsets.ModelViewSet):
     http_method_names = ["get", "head", "options"]
 
     def get_queryset(self) -> QuerySet[models.BillingHistory]:
-        queryset = (
-            self.queryset.filter(
-                organization_id=self.request.user.organization_id  # type: ignore
-            )
-            .select_related("organization")
-            .only(
-                "id",
-                "freight_charge_amount_currency",
-                "commodity__id",
-                "bol_number",
-                "total_amount_currency",
-                "other_charge_total_currency",
-                "commodity_descr",
-                "consignee_ref_number",
-                "mileage",
-                "total_amount",
-                "pieces",
-                "user__id",
-                "freight_charge_amount",
-                "invoice_number",
-                "customer__id",
-                "bill_date",
-                "weight",
-                "is_cancelled",
-                "worker__id",
-                "other_charge_total",
-                "order_type__id",
-                "organization__id",
-                "order__id",
-                "revenue_code__id",
-                "is_summary",
-                "bill_type",
-            )
+        queryset = self.queryset.filter(
+            organization_id=self.request.user.organization_id  # type: ignore
+        ).only(
+            "id",
+            "freight_charge_amount_currency",
+            "commodity_id",
+            "bol_number",
+            "total_amount_currency",
+            "other_charge_total_currency",
+            "commodity_descr",
+            "consignee_ref_number",
+            "mileage",
+            "total_amount",
+            "pieces",
+            "user_id",
+            "freight_charge_amount",
+            "invoice_number",
+            "customer_id",
+            "bill_date",
+            "weight",
+            "is_cancelled",
+            "worker_id",
+            "other_charge_total",
+            "order_type_id",
+            "organization_id",
+            "order_id",
+            "revenue_code_id",
+            "is_summary",
+            "bill_type",
         )
         return queryset
 
@@ -229,17 +225,13 @@ class ChargeTypeViewSet(viewsets.ModelViewSet):
     filterset_fields = ("name",)
 
     def get_queryset(self) -> QuerySet[models.ChargeType]:
-        queryset = (
-            self.queryset.filter(
-                organization_id=self.request.user.organization_id  # type: ignore
-            )
-            .select_related("organization")
-            .only(
-                "id",
-                "organization__id",
-                "name",
-                "description",
-            )
+        queryset = self.queryset.filter(
+            organization_id=self.request.user.organization_id  # type: ignore
+        ).only(
+            "id",
+            "organization_id",
+            "name",
+            "description",
         )
         return queryset
 
@@ -263,21 +255,17 @@ class AccessorialChargeViewSet(viewsets.ModelViewSet):
     filterset_fields = ("code", "is_detention", "method")
 
     def get_queryset(self) -> QuerySet[models.AccessorialCharge]:
-        queryset = (
-            self.queryset.filter(
-                organization_id=self.request.user.organization_id  # type: ignore
-            )
-            .select_related("organization")
-            .only(
-                "id",
-                "is_detention",
-                "charge_amount",
-                "code",
-                "description",
-                "method",
-                "organization__id",
-                "charge_amount_currency",
-            )
+        queryset = self.queryset.filter(
+            organization_id=self.request.user.organization_id  # type: ignore
+        ).only(
+            "id",
+            "is_detention",
+            "charge_amount",
+            "code",
+            "description",
+            "method",
+            "organization_id",
+            "charge_amount_currency",
         )
 
         return queryset
@@ -302,17 +290,13 @@ class DocumentClassificationViewSet(viewsets.ModelViewSet):
     filterset_fields = ("name",)
 
     def get_queryset(self) -> QuerySet[models.DocumentClassification]:
-        queryset = (
-            self.queryset.filter(
-                organization_id=self.request.user.organization_id  # type: ignore
-            )
-            .select_related("organization")
-            .only(
-                "id",
-                "name",
-                "organization__id",
-                "description",
-            )
+        queryset = self.queryset.filter(
+            organization_id=self.request.user.organization_id  # type: ignore
+        ).only(
+            "id",
+            "name",
+            "organization_id",
+            "description",
         )
         return queryset
 
