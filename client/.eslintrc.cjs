f82849d3f9e0404cc38fc53c6d65b0295d3f1a52
Eric Moss <yeswolfprod@gmail.com>
2023-06-20 12:34:28 -0400

    change(client): update eslint

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,13 +1,24 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ["plugin:react/recommended", "standard-with-typescript"],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: "latest",
-  },
-  plugins: ["react"],
-  rules: {},
-};
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+}
