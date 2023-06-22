136263cdae0925a2a49312351130fb6e77d82a0a
Eric Moss <yeswolfprod@gmail.com>
2023-06-21 22:52:43 -0400

    change(backend): add docstrings

--- reports/consumers.py
+++ reports/consumers.py
@@ -14,6 +14,9 @@
 #  Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use     -
 #  Grant, and not modifying the license in any other way.                                          -
 # --------------------------------------------------------------------------------------------------
+
+from http.cookies import SimpleCookie
+
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import JsonWebsocketConsumer
 from channels.layers import get_channel_layer
@@ -24,11 +27,51 @@ channel_layer = get_channel_layer()
 
 
 class NotificationConsumer(JsonWebsocketConsumer):
-    def connect(self):
+    """This class inherits from JsonWebsocketConsumer and serves as a consumer for notifications.
+
+    The NotificationConsumer class consumes notifications, authenticates the user from a provided token,
+    adds the user to a communication group, and sends notifications to the user.
+
+    Attributes:
+        scope: A dictionary-like object that contains metadata about the connection.
+        channel_name: A unique channel name automatically set on the base AsyncConsumer when a new
+        connection is accepted.
+
+    Methods:
+        connect: Handles the connection process for a new consumer. It authenticates the user,
+        adds the user to a group, and accepts the connection.
+        disconnect: Handles the disconnection process for the consumer. It removes the user from the group.
+        send_notification: Sends the notification data to the user/client as JSON.
+    """
+
+    def connect(self) -> None:
+        """This method is called when the websocket is handshaking as part of the connection process.
+
+        The method authenticates the user using a bearer token obtained from cookies, adds the user to
+        a group (named by the user's username), and accepts the incoming socket connection.
+
+        If the token is not provided or authentication fails, the method returns without doing anything.
+
+        Returns:
+            None: This function does not return anything.
+
+        Raises:
+            HTTPError: If the token_authenticator.authenticate() fails to authenticate.
+        """
+
         token_authenticator = BearerTokenAuthentication()
 
-        # Get the token from the URL
-        token = self.scope["query_string"].decode("utf-8").split("=")[1]
+        # Parse cookies from headers
+        headers = dict(self.scope["headers"])
+        cookies = SimpleCookie()
+        cookies.load(headers.get(b"cookie", b"").decode())
+
+        # Get the token from the cookie
+        token = cookies.get("auth_token")
+        token = token.value if token else None
+
+        if token is None:
+            return
 
         # Mocking a request to verify the token
         mock_request = type("", (), {})()  # Create a blank class
@@ -41,13 +84,35 @@ class NotificationConsumer(JsonWebsocketConsumer):
         self.scope["user"] = user_token[0]
 
         self.room_group_name = self.scope["user"].username
+
         async_to_sync(channel_layer.group_add)(self.room_group_name, self.channel_name)
         self.accept()
 
-    def disconnect(self, close_code):
+    def disconnect(self, close_code: int) -> None:
+        """This method is called when the WebSocket closes for any reason.
+
+        The method removes the user from the group.
+
+        Args:
+            close_code (int): An integer that provides more detail on why the connection was closed.
+
+        Returns:
+            None: This function does not return anything.
+        """
         async_to_sync(channel_layer.group_discard)(
             self.room_group_name, self.channel_name
         )
 
-    def send_notification(self, event):
+    def send_notification(self, event: dict):
+        """Sends the notification data to the client as JSON.
+
+        The event dict should follow the format {type: x, value: y}, where 'type' is the type of
+        event and 'value' is the data related to the event.
+
+        Args:
+            event (dict): A dictionary containing the event data to be sent.
+
+        Returns:
+            None: This function does not return anything.
+        """
         self.send_json(event)
