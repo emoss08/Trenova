9caca066f3d66edc953c3892ca632e695480b152
Eric Moss <yeswolfprod@gmail.com>
2023-07-14 01:26:04 -0400

    Add types for BillingClient

--- utils/types.py
+++ utils/types.py
@@ -14,8 +14,11 @@
 #  Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use     -
 #  Grant, and not modifying the license in any other way.                                          -
 # --------------------------------------------------------------------------------------------------
+from __future__ import annotations
 
-from typing import Any, TypeAlias, Union
+import uuid
+from enum import Enum
+from typing import Any, TypeAlias, Union, TypedDict
 from uuid import UUID
 
 from django.db.models import UUIDField
@@ -29,7 +32,65 @@ HealthStatusAndTime: TypeAlias = Union[
     dict[str, Union[str, int, int, int, float, float]]
 ]
 DiskUsage: TypeAlias = tuple[int, int, int]
+BilledOrders: TypeAlias = tuple[list[Any | list[dict[str, str | list[str]]]], list[Any]]
+
+
+class BillingClientActions(Enum):
+    """
+    The different actions that the billing client can take.
+    """
+
+    GET_STARTED = "GET_STARTED"
+    ORDERS_READY = "ORDERS_READY"
+    BILLING_QUEUE = "BILLING_QUEUE"
+    BILL_ORDERS = "BILL_ORDERS"
+    BILLING_COMPLETE = "BILLING_COMPLETE"
+
+
+class BillingClientStatuses(Enum):
+    """
+    The different statuses that the billing client can have.
+    """
+
+    SUCCESS = "SUCCESS"
+    ERROR = "ERROR"
+    WARNING = "WARNING"
+    INFO = "INFO"
+
+
+class BillingClientSessionResponse(TypedDict):
+    """
+    A response from the billing client session.
+    """
+
+    user_id: uuid.UUID
+    # The current action that the client is taking.
+    current_action: str
+    # The previous action that was taken by the client.
+    previous_action: str | None
+    # Last response sent from the server to the client.
+    last_response: Any
+    # Last message sent from the client to the server.
+    last_message: Any
 
 
 class AuthenticatedRequest(Request):
+    """
+    A request that has been authenticated by the authentication middleware.
+
+    Attributes:
+        user: The user that made the request.
+    """
+
     user: User
+
+
+class BillingClientResponse(TypedDict):
+    """
+    A response from the billing client.
+    """
+
+    action: str
+    status: BillingClientStatuses
+    step: int
+    message: Any
