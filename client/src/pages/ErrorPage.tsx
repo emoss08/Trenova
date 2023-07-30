5e59da0787b6a012a82c6d6616b5f2c24f3f8a05
Eric Moss <yeswolfprod@gmail.com>
2023-07-29 23:58:24 -0400

    change(client): finalized error page

--- client/src/pages/ErrorPage.tsx
+++ client/src/pages/ErrorPage.tsx
@@ -16,38 +16,105 @@
  */
 
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  createStyles,
+  Image,
+  Container,
+  Title,
+  Text,
+  Button,
+  SimpleGrid,
+  rem,
+  Flex,
+  Card,
+} from "@mantine/core";
+
+import image from "../assets/images/404.svg";
+
+const useStyles = createStyles((theme) => ({
+  card: {
+    width: "100%",
+    // maxWidth: "100%",
+    // height: "auto",
+    "@media (max-width: 576px)": {
+      height: "auto",
+      maxHeight: "none",
+    },
+  },
+  root: {
+    paddingTop: rem(80),
+    paddingBottom: rem(80),
+  },
+
+  title: {
+    fontWeight: 900,
+    fontSize: rem(34),
+    marginBottom: theme.spacing.md,
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+
+    [theme.fn.smallerThan("sm")]: {
+      fontSize: rem(32),
+    },
+  },
+
+  control: {
+    [theme.fn.smallerThan("sm")]: {
+      width: "100%",
+    },
+  },
+
+  mobileImage: {
+    [theme.fn.largerThan("sm")]: {
+      display: "none",
+    },
+  },
+
+  desktopImage: {
+    [theme.fn.smallerThan("sm")]: {
+      display: "none",
+    },
+  },
+}));
 
 const ErrorPage: React.FC = () => {
+  const { classes } = useStyles();
+  const navigate = useNavigate();
+
   return (
-    <main className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
-      <div className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
-        <div className="text-center">
-          <p className="text-base font-semibold text-indigo-600">404</p>
-          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-5xl">
-            Page not found
-          </h1>
-          <p className="mt-6 text-base leading-7 text-gray-600">
-            Sorry, we couldn’t find the page you’re looking for.
-          </p>
-          <div className="mt-10 flex items-center justify-center gap-x-6">
-            <Link
-              to="/"
-              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline
-             focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    <Flex
+      style={{
+        paddingTop: "20vh",
+      }}
+    >
+      <Container className={classes.root}>
+        <SimpleGrid
+          spacing={80}
+          cols={2}
+          breakpoints={[{ maxWidth: "sm", cols: 1, spacing: 40 }]}
+        >
+          <Image src={image} className={classes.mobileImage} />
+          <div>
+            <Title className={classes.title}>Something is not right...</Title>
+            <Text color="dimmed" size="lg">
+              Page you are trying to open does not exist. You may have mistyped
+              the address, or the page has been moved to another URL. If you
+              think this is an error contact support.
+            </Text>
+            <Button
+              // variant="outline"
+              size="md"
+              mt="xl"
+              className={classes.control}
+              onClick={() => navigate("/")}
             >
-              Go back home
-            </Link>
-            <a
-              href="#"
-              className="text-sm font-semibold text-gray-900 dark:text-white"
-            >
-              Contact support <span aria-hidden="true">&rarr;</span>
-            </a>
+              Get back to home page
+            </Button>
           </div>
-        </div>
-      </div>
-    </main>
+          <Image src={image} className={classes.desktopImage} />
+        </SimpleGrid>
+      </Container>
+    </Flex>
   );
 };
 export default ErrorPage;
