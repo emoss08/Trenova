6a40410147cee406a6ebe4cd07fcdad7ba782653
Eric Moss <yeswolfprod@gmail.com>
2023-08-06 19:53:14 -0400

    remodel application

--- client/src/utils/zustand.ts
+++ client/src/utils/zustand.ts
@@ -1,26 +1,10 @@
-/*
- * COPYRIGHT(c) 2023 MONTA
- *
- * This file is part of Monta.
- *
- * The Monta software is licensed under the Business Source License 1.1. You are granted the right
- * to copy, modify, and redistribute the software, but only for non-production use or with a total
- * of less than three server instances. Starting from the Change Date (November 16, 2026), the
- * software will be made available under version 2 or later of the GNU General Public License.
- * If you use the software in violation of this license, your rights under the license will be
- * terminated automatically. The software is provided "as is," and the Licensor disclaims all
- * warranties and conditions. If you use this license's text or the "Business Source License" name
- * and trademark, you must comply with the Licensor's covenants, which include specifying the
- * Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use
- * Grant, and not modifying the license in any other way.
- */
-
-// Credits: Acorn1010 - https://gist.github.com/acorn1010/9f4621d3dfc33052ffd84f6c2a06d4d6.
-
 import { SetStateAction, useCallback } from 'react';
 import { create } from 'zustand';
 
+// Credits: Acorn1010 - https://gist.github.com/acorn1010/9f4621d3dfc33052ffd84f6c2a06d4d6.
+
 export type EqualityFn<T> = (left: T | null | undefined, right: T | null | undefined) => boolean;
+
 // eslint-disable-next-line @typescript-eslint/ban-types
 const isFunction = (fn: unknown): fn is Function => typeof fn === 'function';
 
@@ -40,7 +24,7 @@ const isFunction = (fn: unknown): fn is Function => typeof fn === 'function';
  * const store = createStore({ count: 0 });
  *
  * const Component = () => {
- *   const [count, setCount] = store.use("count");
+ *   const [count, setCount] = store.use('count');
  *   ...
  * };
  */
@@ -49,12 +33,9 @@ export const createGlobalStore = <State extends object>(initialState: State) => 
   const store = create<State>(() => deepClone(initialState));
 
   const setter = <T extends keyof State>(key: T, value: SetStateAction<State[T]>) => {
-    if (typeof value === 'function') {
+    if (isFunction(value)) {
       store.setState(
-        (prevValue) =>
-          ({
-            [key]: (value as Function)(prevValue[key])
-          }) as unknown as Partial<State>
+        (prevValue) => ({ [key]: value(prevValue[key]) }) as unknown as Partial<State>
       );
     } else {
       store.setState({ [key]: value } as unknown as Partial<State>);
@@ -160,16 +141,16 @@ function deepClone<T>(obj: T): T {
   return result;
 }
 
-// Define a custom persisting middleware
 function getRegExpFlags(regExp: RegExp): string {
   if ((typeof regExp.source as any).flags === 'string') {
     return (regExp.source as any).flags;
+  } else {
+    const flags = [];
+    regExp.global && flags.push('g');
+    regExp.ignoreCase && flags.push('i');
+    regExp.multiline && flags.push('m');
+    regExp.sticky && flags.push('y');
+    regExp.unicode && flags.push('u');
+    return flags.join('');
   }
-  const flags = [];
-  regExp.global && flags.push('g');
-  regExp.ignoreCase && flags.push('i');
-  regExp.multiline && flags.push('m');
-  regExp.sticky && flags.push('y');
-  regExp.unicode && flags.push('u');
-  return flags.join('');
 }
