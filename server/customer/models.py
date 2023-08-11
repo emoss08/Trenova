7e62663628444b52604945ce68d671f37e72ac2d
Eric Moss <yeswolfprod@gmail.com>
2023-08-11 13:56:34 -0400

    change(backend): change model fields

--- server/customer/models.py
+++ server/customer/models.py
@@ -1,19 +1,30 @@
-# --------------------------------------------------------------------------------------------------
-#  COPYRIGHT(c) 2023 MONTA                                                                         -
-#                                                                                                  -
-#  This file is part of Monta.                                                                     -
-#                                                                                                  -
-#  The Monta software is licensed under the Business Source License 1.1. You are granted the right -
-#  to copy, modify, and redistribute the software, but only for non-production use or with a total -
-#  of less than three server instances. Starting from the Change Date (November 16, 2026), the     -
-#  software will be made available under version 2 or later of the GNU General Public License.     -
-#  If you use the software in violation of this license, your rights under the license will be     -
-#  terminated automatically. The software is provided "as is," and the Licensor disclaims all      -
-#  warranties and conditions. If you use this license's text or the "Business Source License" name -
-#  and trademark, you must comply with the Licensor's covenants, which include specifying the      -
-#  Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use     -
-#  Grant, and not modifying the license in any other way.                                          -
-# --------------------------------------------------------------------------------------------------
+#  COPYRIGHT(c) 2023 MONTA
+#
+#  This file is part of Monta.
+#
+#  The Monta software is licensed under the Business Source License 1.1. You are granted the right
+#  to copy, modify, and redistribute the software, but only for non-production use or with a total
+#  of less than three server instances. Starting from the Change Date (November 16, 2026), the
+#  software will be made available under version 2 or later of the GNU General Public License.
+#  If you use the software in violation of this license, your rights under the license will be
+#  terminated automatically. The software is provided "as is," and the Licensor disclaims all
+#  warranties and conditions. If you use this license's text or the "Business Source License" name
+#  and trademark, you must comply with the Licensor's covenants, which include specifying the
+#  Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use
+#  Grant, and not modifying the license in any other way.
+#
+#  This file is part of Monta.
+#
+#  The Monta software is licensed under the Business Source License 1.1. You are granted the right
+#  to copy, modify, and redistribute the software, but only for non-production use or with a total
+#  of less than three server instances. Starting from the Change Date (November 16, 2026), the
+#  software will be made available under version 2 or later of the GNU General Public License.
+#  If you use the software in violation of this license, your rights under the license will be
+#  terminated automatically. The software is provided "as is," and the Licensor disclaims all
+#  warranties and conditions. If you use this license's text or the "Business Source License" name
+#  and trademark, you must comply with the Licensor's covenants, which include specifying the
+#  Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use
+#  Grant, and not modifying the license in any other way.
 
 import textwrap
 import uuid
@@ -70,6 +81,7 @@ class Customer(GenericModel):  # type: ignore
         _("Name"),
         max_length=150,
         help_text=_("Customer name"),
+        db_index=True,
     )
     address_line_1 = models.CharField(
         _("Address Line 1"),
@@ -99,21 +111,25 @@ class Customer(GenericModel):  # type: ignore
         help_text=_("Zip code"),
         blank=True,
     )
-    has_customer_portal = models.BooleanField(
+    has_customer_portal = models.CharField(
         _("Has Customer Portal?"),
-        default=False,
         help_text=_(
             "Designates whether this customer has the customer portal. "
             "active or not."
         ),
+        choices=[("Y", "Yes"), ("N", "No")],
+        max_length=1,
+        default="N",
     )
-    auto_mark_ready_to_bill = models.BooleanField(
+    auto_mark_ready_to_bill = models.CharField(
         _("Auto Mark Ready to Bill?"),
-        default=False,
         help_text=_(
             "Designates whether to automatically mark customer orders ready to bill. "
             "if the order passes customer billing requirements."
         ),
+        choices=[("Y", "Yes"), ("N", "No")],
+        max_length=1,
+        default="N",
     )
 
     class Meta:
@@ -479,6 +495,7 @@ class CustomerContact(GenericModel):
         _("Name"),
         max_length=150,
         help_text=_("Contact name"),
+        db_index=True,
     )
     email = models.EmailField(
         _("Email"),
@@ -506,6 +523,9 @@ class CustomerContact(GenericModel):
     )
 
     class Meta:
+        """
+        Metaclass for CustomerContact
+        """
         verbose_name = _("Customer Contact")
         verbose_name_plural = _("Customer Contacts")
         ordering = ["customer", "name"]
