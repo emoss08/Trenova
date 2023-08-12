04dcf7a376f1a0af1bd4188fe9e558ff4c400c06
Eric Moss <yeswolfprod@gmail.com>
2023-08-12 15:11:26 -0400

    add(backend): add path to search

--- server/core/api.py
+++ server/core/api.py
@@ -1,19 +1,18 @@
-# --------------------------------------------------------------------------------------------------
-#  COPYRIGHT(c) 2023 MONTA                                                                         -
-#                                                                                                  -
-#  This file is part of Monta.                                                                     -
-#                                                                                                  -
-#  The Monta software is licensed under the Business Source License 1.1. You are granted the right -
-#  to copy, modify, and redistribute the software, but only for non-production use or with a total -
-#  of less than three server instances. Starting from the Change Date (November 16, 2026), the     -
-#  software will be made available under version 2 or later of the GNU General Public License.     -
-#  If you use the software in violation of this license, your rights under the license will be     -
-#  terminated automatically. The software is provided "as is," and the Licensor disclaims all      -
-#  warranties and conditions. If you use this license's text or the "Business Source License" name -
-#  and trademark, you must comply with the Licensor's covenants, which include specifying the      -
-#  Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use     -
-#  Grant, and not modifying the license in any other way.                                          -
-# --------------------------------------------------------------------------------------------------
+#  COPYRIGHT(c) 2023 MONTA
+#
+#  This file is part of Monta.
+#
+#  The Monta software is licensed under the Business Source License 1.1. You are granted the right
+#  to copy, modify, and redistribute the software, but only for non-production use or with a total
+#  of less than three server instances. Starting from the Change Date (November 16, 2026), the
+#  software will be made available under version 2 or later of the GNU General Public License.
+#  If you use the software in violation of this license, your rights under the license will be
+#  terminated automatically. The software is provided "as is," and the Licensor disclaims all
+#  warranties and conditions. If you use this license's text or the "Business Source License" name
+#  and trademark, you must comply with the Licensor's covenants, which include specifying the
+#  Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use
+#  Grant, and not modifying the license in any other way.
+
 from typing import Any
 
 from django.apps import apps
@@ -40,7 +39,29 @@ class CustomPageNumberPagination(pagination.PageNumberPagination):
 
 
 class SearchView(APIView):
+    """Handles handling of search queries through specified format 'model_name:search_term'.
+
+    This class inherits from the APIView and redefines its get() method for performing
+    specific search operations.
+
+    Attributes:
+        get: A method that executes the search operation based on request's parameters.
+    """
+
     def get(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        """Handle GET request on the SearchView.
+
+        Args:
+            request (Request): The request from the client. Must include 'term'
+            in the query parameters.
+            *args (Any): Variable length argument list.
+            **kwargs (Any): Arbitrary keyword arguments.
+
+        Returns:
+            Response: The search results, paginated if applicable, or an error
+            message if the search term is not properly formatted or if the
+            model name is invalid.
+        """
         term = request.query_params.get("term", "")
         if not term or ":" not in term:
             return Response(
@@ -59,7 +80,7 @@ class SearchView(APIView):
                 {"error": "Invalid model name"}, status=status.HTTP_400_BAD_REQUEST
             )
 
-        app_name, serializer, search_fields, display = model_info.values()
+        app_name, serializer, search_fields, display, path = model_info.values()
         model = apps.get_model(app_name, model_name)
 
         # Create a SearchVector for each specified field
@@ -87,6 +108,7 @@ class SearchView(APIView):
             data = [
                 {
                     "display": display(result),
+                    "path": path(result),
                     "model_name": model_name,
                 }
                 for result in page
@@ -96,6 +118,7 @@ class SearchView(APIView):
         # Serialize the results
         data = [
             {
+                "path": path(result),
                 "display": display(result),
                 "model_name": model_name,
             }
