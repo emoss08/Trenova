d8fc5648e37ed8b627b0c453eaf20262a17dd1ee
Eric Moss <yeswolfprod@gmail.com>
2023-08-15 00:45:11 -0400

    feat: customer email profile api

--- server/customer/api.py
+++ server/customer/api.py
@@ -14,9 +14,14 @@
 #  Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use     -
 #  Grant, and not modifying the license in any other way.                                          -
 # --------------------------------------------------------------------------------------------------
+import typing
 
 from django.db.models import Prefetch, QuerySet
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.exceptions import ValidationError
+from rest_framework.request import Request
+from rest_framework.response import Response
 
 from core.permissions import CustomObjectPermissions
 from customer import models, serializers
@@ -82,16 +87,22 @@ class CustomerViewSet(viewsets.ModelViewSet):
 
 
 class CustomerBillingProfileViewSet(viewsets.ModelViewSet):
-    """A viewset for viewing and editing customer billing profile information in the system.
+    """A viewset for managing Customer Billing Profile records.
 
-    The viewset provides default operations for creating, updating, and deleting customer
-    billing profiles, as well as listing and retrieving customer billing profiles. It uses
-    the `CustomerBillingProfileSerializer` class to convert the customer billing profile
-    instances to and from JSON-formatted data.
+    This viewset handles HTTP requests to perform CRUD operations on the customer billing profile model. Authenticated
+    users can perform operations to create, update, view, list or delete the customer billing profiles. It also support
+    filtering on 'status', 'customer', and 'rule_profile' fields.
 
-    Only authenticated users are allowed to access the views provided by this viewset.
-    Filtering is also available, with the ability to filter by customer billing profile ID,
-    customer ID, and billing profile ID.
+    Attributes:
+        queryset (QuerySet): QuerySet of CustomerBillingProfile model instances.
+        serializer_class ('CustomerBillingProfileSerializer'): The serializer to handle the conversion to/from JSON for CustomerBillingProfile.
+        filterset_fields (tuple): Fields on which the queryset can be filtered.
+        permission_classes (list): The class-based views to be applied to the viewset.
+
+    Methods:
+        customer_billing_profile_details: Handle GET requests and return the customer billing details for a given customer ID.
+        get_queryset: Custom method to get the queryset.
+
     """
 
     queryset = models.CustomerBillingProfile.objects.all()
@@ -99,7 +110,44 @@ class CustomerBillingProfileViewSet(viewsets.ModelViewSet):
     filterset_fields = ("status", "customer", "rule_profile")
     permission_classes = [CustomObjectPermissions]
 
+    @action(detail=False, methods=["get"])
+    def customer_billing_profile_details(
+        self, request: Request, *args: typing.Any, **kwargs: typing.Any
+    ) -> Response:
+        """Handle GET requests and return the customer billing details for a given customer ID.
+
+        Args:
+            request (Request): Django Request object.
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            Response: The Django Rest Framework Response object, containing the serialized details
+                      of the customer billing profile or a validation error message.
+
+        Raises:
+            ValidationError: If 'customer_id' parameter is not found in the request.
+
+        """
+        customer_id = request.query_params.get("customer_id")
+
+        if not customer_id:
+            raise ValidationError("Query parameter 'customer_id' is required.")
+
+        queryset = models.CustomerBillingProfile.objects.get(
+            customer_id=customer_id, organization_id=self.request.user.organization_id  # type: ignore
+        )
+        serializer = serializers.CustomerBillingProfileSerializer(queryset)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
     def get_queryset(self) -> QuerySet[models.CustomerBillingProfile]:
+        """Custom method to get the queryset. Filters the queryset based on organization
+        that the authenticated user is part of and selects only selected fields.
+
+        Returns:
+            QuerySet: A QuerySet containing the customer billing profiles for the organization
+                      that authenticated user is part of.
+        """
         queryset = self.queryset.filter(
             organization_id=self.request.user.organization_id  # type: ignore
         ).only(
@@ -218,4 +266,43 @@ class CustomerRuleProfileViewSet(viewsets.ModelViewSet):
             )
         )
 
+        return queryset
+
+
+class CustomerEmailProfileViewSet(viewsets.ModelViewSet):
+    """Model View Set for CustomerEmailProfile.
+
+    This ViewSet provides complete CRUD operations for CustomerEmailProfile.
+    It uses Django queryset to fetch all objects of Customer Email Profile and also uses
+    a custom serializer 'CustomerEmailProfileSerializer' for the serialization of the data.
+    The users are able to filter the data based on the field "name".
+    In addition, for listening to appropriate actions, CustomObjectPermissions are maintained.
+
+    Attributes:
+        queryset (object): A Django Queryset that fetches all CustomerEmailProfile objects from the database.
+        serializer_class (class): The serializer class used for the serialization of this model's data.
+        filterset_fields (tuple): A tuple containing the fields on which users can filter.
+        permission_classes (list): Permissions that must be fulfilled to access or modify the data.
+    """
+
+    queryset = models.CustomerEmailProfile.objects.all()
+    serializer_class = serializers.CustomerEmailProfileSerializer
+    filterset_fields = ("name",)
+    permission_classes = [CustomObjectPermissions]
+
+    def get_queryset(self) -> QuerySet[models.CustomerEmailProfile]:
+        """
+        Get queryset specifically for the current user's organization.
+
+        Get and return queryset of CustomerEmailProfile objects which belong to the same organization
+        as the user making the request. The user's organization is determined using the `organization_id`
+        attribute from the authenticated user's request object.
+
+        Returns:
+            QuerySet[models.CustomerEmailProfile]: A QuerySet containing CustomerEmailProfile objects belonging to the
+            current user's organization.
+        """
+        queryset = self.queryset.filter(
+            organization_id=self.request.user.organization_id  # type: ignore
+        )
         return queryset
