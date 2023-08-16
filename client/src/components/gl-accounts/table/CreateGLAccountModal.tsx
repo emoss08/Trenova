201ac7fa768e81aea7a027502b31a57a88b81ab5
Eric Moss <yeswolfprod@gmail.com>
2023-08-16 13:49:47 -0400

    refactor: hazardous material page

--- client/src/components/gl-accounts/table/CreateGLAccountModal.tsx
+++ client/src/components/gl-accounts/table/CreateGLAccountModal.tsx
@@ -15,16 +15,193 @@
  * Grant, and not modifying the license in any other way.
  */
 
-import { Modal, Skeleton } from "@mantine/core";
+import { Box, Button, Group, Modal, SimpleGrid, Skeleton } from "@mantine/core";
 import React, { Suspense } from "react";
-import { generalLedgerTableStore } from "@/stores/AccountingStores";
-import { CreateGLAccountModalForm } from "./_Partials/CreateGLAccountModalForm";
+import { useMutation, useQueryClient } from "react-query";
+import { notifications } from "@mantine/notifications";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faXmark } from "@fortawesome/pro-solid-svg-icons";
+import { useForm, yupResolver } from "@mantine/form";
+import { generalLedgerTableStore as store } from "@/stores/AccountingStores";
+import { useFormStyles } from "@/styles/FormStyles";
+import { GLAccountFormValues } from "@/types/apps/accounting";
+import axios from "@/lib/AxiosConfig";
+import { APIError } from "@/types/server";
+import { glAccountSchema } from "@/utils/apps/accounting/schema";
+import { SelectInput } from "@/components/ui/fields/SelectInput";
+import { statusChoices } from "@/lib/utils";
+import { ValidatedTextInput } from "@/components/ui/fields/TextInput";
+import { ValidatedTextArea } from "@/components/ui/fields/TextArea";
+import {
+  accountClassificationChoices,
+  accountSubTypeChoices,
+  accountTypeChoices,
+  cashFlowTypeChoices,
+} from "@/utils/apps/accounting";
 
-export const CreateGLAccountModal: React.FC = () => {
-  const [showCreateModal, setShowCreateModal] =
-    generalLedgerTableStore.use("createModalOpen");
+function CreateGLAccountModalForm(): React.ReactElement {
+  const { classes } = useFormStyles();
+  const [loading, setLoading] = React.useState<boolean>(false);
+  const queryClient = useQueryClient();
 
-  if (!showCreateModal) return null;
+  const mutation = useMutation(
+    (values: GLAccountFormValues) => axios.post("/gl_accounts/", values),
+    {
+      onSuccess: () => {
+        queryClient
+          .invalidateQueries({
+            queryKey: ["gl-account-table-data"],
+          })
+          .then(() => {
+            notifications.show({
+              title: "Success",
+              message: "GL Account created successfully",
+              color: "green",
+              withCloseButton: true,
+              icon: <FontAwesomeIcon icon={faCheck} />,
+            });
+            store.set("createModalOpen", false);
+          });
+      },
+      onError: (error: any) => {
+        const { data } = error.response;
+        if (data.type === "validation_error") {
+          data.errors.forEach((e: APIError) => {
+            form.setFieldError(e.attr, e.detail);
+            if (e.attr === "non_field_errors") {
+              notifications.show({
+                title: "Error",
+                message: e.detail,
+                color: "red",
+                withCloseButton: true,
+                icon: <FontAwesomeIcon icon={faXmark} />,
+                autoClose: 10_000, // 10 seconds
+              });
+            }
+          });
+        }
+      },
+      onSettled: () => {
+        setLoading(false);
+      },
+    },
+  );
+
+  const form = useForm<GLAccountFormValues>({
+    validate: yupResolver(glAccountSchema),
+    initialValues: {
+      status: "A",
+      account_number: "0000-0000-0000-0000",
+      description: "",
+      account_type: "",
+      cash_flow_type: "",
+      account_sub_type: "",
+      account_classification: "",
+    },
+  });
+
+  const submitForm = (values: GLAccountFormValues) => {
+    setLoading(true);
+    mutation.mutate(values);
+  };
+
+  return (
+    <form onSubmit={form.onSubmit((values) => submitForm(values))}>
+      <Box className={classes.div}>
+        <Box>
+          <SimpleGrid cols={2} breakpoints={[{ maxWidth: "sm", cols: 1 }]}>
+            <SelectInput
+              form={form}
+              data={statusChoices}
+              className={classes.fields}
+              name="status"
+              label="Status"
+              placeholder="Status"
+              variant="filled"
+              withAsterisk
+            />
+            <ValidatedTextInput
+              form={form}
+              className={classes.fields}
+              name="account_number"
+              label="Account Number"
+              placeholder="Account Number"
+              variant="filled"
+              withAsterisk
+            />
+          </SimpleGrid>
+          <ValidatedTextArea
+            form={form}
+            className={classes.fields}
+            name="description"
+            label="Description"
+            placeholder="Description"
+            variant="filled"
+            withAsterisk
+          />
+          <SimpleGrid cols={2} breakpoints={[{ maxWidth: "sm", cols: 1 }]}>
+            <SelectInput
+              form={form}
+              data={accountTypeChoices}
+              className={classes.fields}
+              name="account_type"
+              label="Account Type"
+              placeholder="AP Account"
+              variant="filled"
+              withAsterisk
+              clearable
+            />
+            <SelectInput
+              form={form}
+              data={cashFlowTypeChoices}
+              className={classes.fields}
+              name="cash_flow_type"
+              label="Cash Flow Type"
+              placeholder="Cash Flow Type"
+              variant="filled"
+              clearable
+            />
+          </SimpleGrid>
+          <SimpleGrid cols={2} breakpoints={[{ maxWidth: "sm", cols: 1 }]}>
+            <SelectInput
+              form={form}
+              data={accountSubTypeChoices}
+              className={classes.fields}
+              name="account_sub_type"
+              label="Account Sub Type"
+              placeholder="Account Sub Type"
+              variant="filled"
+              clearable
+            />
+            <SelectInput
+              form={form}
+              data={accountClassificationChoices}
+              className={classes.fields}
+              name="account_classification"
+              label="Account Classification"
+              placeholder="Account Classification"
+              variant="filled"
+              clearable
+            />
+          </SimpleGrid>
+          <Group position="right" mt="md">
+            <Button
+              color="white"
+              type="submit"
+              className={classes.control}
+              loading={loading}
+            >
+              Submit
+            </Button>
+          </Group>
+        </Box>
+      </Box>
+    </form>
+  );
+}
+
+export function CreateGLAccountModal(): React.ReactElement {
+  const [showCreateModal, setShowCreateModal] = store.use("createModalOpen");
 
   return (
     <Modal.Root
@@ -45,4 +222,4 @@ export const CreateGLAccountModal: React.FC = () => {
       </Modal.Content>
     </Modal.Root>
   );
-};
+}
--- client/src/components/gl-accounts/table/EditGLAccountModal.tsx
+++ client/src/components/gl-accounts/table/EditGLAccountModal.tsx
@@ -15,17 +15,231 @@
  * Grant, and not modifying the license in any other way.
  */
 
-import { Modal, Skeleton } from "@mantine/core";
+import { Box, Button, Group, Modal, SimpleGrid, Skeleton } from "@mantine/core";
 import React, { Suspense } from "react";
-import { generalLedgerTableStore } from "@/stores/AccountingStores";
-import { EditGLAccountModalForm } from "@/components/gl-accounts/table/_Partials/EditGLAccountModalForm";
+import { useMutation, useQueryClient } from "react-query";
+import { notifications } from "@mantine/notifications";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faXmark } from "@fortawesome/pro-solid-svg-icons";
+import { useForm, yupResolver } from "@mantine/form";
+import { generalLedgerTableStore as store } from "@/stores/AccountingStores";
+import {
+  GeneralLedgerAccount,
+  GLAccountFormValues,
+} from "@/types/apps/accounting";
+import { useFormStyles } from "@/styles/FormStyles";
+import axios from "@/lib/AxiosConfig";
+import { APIError } from "@/types/server";
+import { glAccountSchema } from "@/utils/apps/accounting/schema";
+import { SelectInput } from "@/components/ui/fields/SelectInput";
+import { statusChoices } from "@/lib/utils";
+import { ValidatedTextInput } from "@/components/ui/fields/TextInput";
+import { ValidatedTextArea } from "@/components/ui/fields/TextArea";
+import {
+  accountClassificationChoices,
+  accountSubTypeChoices,
+  accountTypeChoices,
+  cashFlowTypeChoices,
+} from "@/utils/apps/accounting";
 
-export const EditGLAccountModal: React.FC = () => {
-  const [showEditModal, setShowEditModal] =
-    generalLedgerTableStore.use("editModalOpen");
-  const [glAccount] = generalLedgerTableStore.use("selectedRecord");
+type EditGLAccountModalFormProps = {
+  glAccount: GeneralLedgerAccount;
+};
 
-  if (!showEditModal) return null;
+export function EditGLAccountModalForm({
+  glAccount,
+}: EditGLAccountModalFormProps) {
+  const { classes } = useFormStyles();
+  const [loading, setLoading] = React.useState<boolean>(false);
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation(
+    (values: GLAccountFormValues) =>
+      axios.put(`/gl_accounts/${glAccount.id}/`, values),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: ["gl-account-table-data"],
+        });
+        queryClient
+          .invalidateQueries({
+            queryKey: ["glAccount", glAccount.id],
+          })
+          .then(() => {
+            notifications.show({
+              title: "Success",
+              message: "GL Account updated successfully",
+              color: "green",
+              withCloseButton: true,
+              icon: <FontAwesomeIcon icon={faCheck} />,
+            });
+            store.set("editModalOpen", false);
+          });
+      },
+      onError: (error: any) => {
+        const { data } = error.response;
+        if (data.type === "validation_error") {
+          data.errors.forEach((e: APIError) => {
+            form.setFieldError(e.attr, e.detail);
+            if (e.attr === "non_field_errors") {
+              notifications.show({
+                title: "Error",
+                message: e.detail,
+                color: "red",
+                withCloseButton: true,
+                icon: <FontAwesomeIcon icon={faXmark} />,
+                autoClose: 10_000, // 10 seconds
+              });
+            }
+          });
+        }
+      },
+      onSettled: () => {
+        setLoading(false);
+      },
+    },
+  );
+
+  const form = useForm<GLAccountFormValues>({
+    validate: yupResolver(glAccountSchema),
+    initialValues: {
+      status: glAccount.status,
+      account_number: glAccount.account_number,
+      description: glAccount.description,
+      account_type: glAccount.account_type,
+      cash_flow_type: glAccount?.cash_flow_type,
+      account_sub_type: glAccount?.account_sub_type,
+      account_classification: glAccount?.account_classification,
+    },
+  });
+
+  const submitForm = (values: GLAccountFormValues) => {
+    setLoading(true);
+    mutation.mutate(values);
+  };
+
+  return (
+    <form onSubmit={form.onSubmit((values) => submitForm(values))}>
+      <Box className={classes.div}>
+        <Box>
+          <SimpleGrid cols={2} breakpoints={[{ maxWidth: "sm", cols: 1 }]}>
+            <SelectInput
+              form={form}
+              data={statusChoices}
+              className={classes.fields}
+              name="status"
+              label="Status"
+              placeholder="Status"
+              variant="filled"
+              onMouseLeave={() => {
+                form.setFieldValue("status", form.values.status);
+              }}
+              withAsterisk
+            />
+            <ValidatedTextInput
+              form={form}
+              className={classes.fields}
+              name="account_number"
+              label="Account Number"
+              placeholder="Account Number"
+              variant="filled"
+              withAsterisk
+            />
+          </SimpleGrid>
+          <ValidatedTextArea
+            form={form}
+            className={classes.fields}
+            name="description"
+            label="Description"
+            placeholder="Description"
+            variant="filled"
+            withAsterisk
+          />
+          <SimpleGrid cols={2} breakpoints={[{ maxWidth: "sm", cols: 1 }]}>
+            <SelectInput
+              form={form}
+              data={accountTypeChoices}
+              className={classes.fields}
+              name="account_type"
+              label="Account Type"
+              placeholder="AP Account"
+              variant="filled"
+              onMouseLeave={() => {
+                form.setFieldValue("account_type", form.values.account_type);
+              }}
+              clearable
+              withAsterisk
+            />
+            <SelectInput
+              form={form}
+              data={cashFlowTypeChoices}
+              className={classes.fields}
+              name="cash_flow_type"
+              label="Cash Flow Type"
+              placeholder="Cash Flow Type"
+              variant="filled"
+              onMouseLeave={() => {
+                form.setFieldValue(
+                  "cash_flow_type",
+                  form.values.cash_flow_type,
+                );
+              }}
+              clearable
+            />
+          </SimpleGrid>
+          <SimpleGrid cols={2} breakpoints={[{ maxWidth: "sm", cols: 1 }]}>
+            <SelectInput
+              form={form}
+              data={accountSubTypeChoices}
+              className={classes.fields}
+              name="account_sub_type"
+              label="Account Sub Type"
+              placeholder="Account Sub Type"
+              variant="filled"
+              onMouseLeave={() => {
+                form.setFieldValue(
+                  "account_sub_type",
+                  form.values.account_sub_type,
+                );
+              }}
+              clearable
+            />
+            <SelectInput
+              form={form}
+              data={accountClassificationChoices}
+              className={classes.fields}
+              name="account_classification"
+              label="Account Classification"
+              placeholder="Account Classification"
+              variant="filled"
+              onMouseLeave={() => {
+                form.setFieldValue(
+                  "account_classification",
+                  form.values.account_classification,
+                );
+              }}
+              clearable
+            />
+          </SimpleGrid>
+          <Group position="right" mt="md">
+            <Button
+              color="white"
+              type="submit"
+              className={classes.control}
+              loading={loading}
+            >
+              Submit
+            </Button>
+          </Group>
+        </Box>
+      </Box>
+    </form>
+  );
+}
+
+export function EditGLAccountModal(): React.ReactElement {
+  const [showEditModal, setShowEditModal] = store.use("editModalOpen");
+  const [glAccount] = store.use("selectedRecord");
 
   return (
     <Modal.Root opened={showEditModal} onClose={() => setShowEditModal(false)}>
@@ -43,4 +257,4 @@ export const EditGLAccountModal: React.FC = () => {
       </Modal.Content>
     </Modal.Root>
   );
-};
+}
