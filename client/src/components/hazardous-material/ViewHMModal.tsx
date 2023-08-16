df536ed6b5768de62846c3b74a7ce42a680b94c1
Eric Moss <yeswolfprod@gmail.com>
2023-08-16 13:50:04 -0400

    refactor: hazardous material page

--- client/src/components/hazardous-material/ViewHMModal.tsx
+++ client/src/components/hazardous-material/ViewHMModal.tsx
@@ -16,16 +16,128 @@
  */
 
 import React, { Suspense } from "react";
-import { Modal, Skeleton } from "@mantine/core";
-import { hazardousMaterialTableStore } from "@/stores/CommodityStore";
-import { ViewHMModalForm } from "@/components/hazardous-material/_partials/ViewHMModalForm";
+import {
+  Box,
+  Button,
+  Group,
+  Modal,
+  Select,
+  SimpleGrid,
+  Skeleton,
+  Textarea,
+  TextInput,
+} from "@mantine/core";
+import { hazardousMaterialTableStore as store } from "@/stores/CommodityStore";
+import { HazardousMaterial } from "@/types/apps/commodities";
+import { useFormStyles } from "@/styles/FormStyles";
+import { statusChoices } from "@/lib/utils";
+import {
+  hazardousClassChoices,
+  packingGroupChoices,
+} from "@/utils/apps/commodities";
 
-export const ViewHMModal: React.FC = () => {
-  const [showViewModal, setShowViewModal] =
-    hazardousMaterialTableStore.use("viewModalOpen");
-  const [hazardousMaterial] = hazardousMaterialTableStore.use("selectedRecord");
+type ViewHMModalFormProps = {
+  hazardousMaterial: HazardousMaterial;
+};
 
-  if (!showViewModal) return null;
+export function ViewHMModalForm({ hazardousMaterial }: ViewHMModalFormProps) {
+  const { classes } = useFormStyles();
+
+  return (
+    <Box className={classes.div}>
+      <Box>
+        <SimpleGrid cols={2} breakpoints={[{ maxWidth: "sm", cols: 1 }]}>
+          <Select
+            data={statusChoices}
+            className={classes.fields}
+            value={hazardousMaterial.status}
+            name="status"
+            label="Status"
+            placeholder="Status"
+            variant="filled"
+            readOnly
+          />
+          <TextInput
+            className={classes.fields}
+            value={hazardousMaterial.name}
+            name="name"
+            label="Name"
+            placeholder="Name"
+            variant="filled"
+            readOnly
+          />
+        </SimpleGrid>
+        <Textarea
+          className={classes.fields}
+          value={hazardousMaterial.description || ""}
+          name="description"
+          label="Description"
+          placeholder="Description"
+          variant="filled"
+          readOnly
+        />
+        <SimpleGrid cols={2} breakpoints={[{ maxWidth: "sm", cols: 1 }]}>
+          <Select
+            data={hazardousClassChoices}
+            className={classes.fields}
+            value={hazardousMaterial.hazard_class}
+            name="hazard_class"
+            label="Hazard Class"
+            placeholder="Hazard Class"
+            variant="filled"
+            readOnly
+          />
+          <Select
+            data={packingGroupChoices}
+            className={classes.fields}
+            value={hazardousMaterial.packing_group || ""}
+            name="packing_group"
+            label="Packing Group"
+            placeholder="Packing Group"
+            variant="filled"
+            readOnly
+          />
+        </SimpleGrid>
+        <TextInput
+          className={classes.fields}
+          value={hazardousMaterial.erg_number || ""}
+          name="erg_number"
+          label="ERG Number"
+          placeholder="ERG Number"
+          variant="filled"
+          readOnly
+        />
+        <TextInput
+          className={classes.fields}
+          value={hazardousMaterial.proper_shipping_name || ""}
+          name="proper_shipping_name"
+          label="Proper Shipping Name"
+          placeholder="Proper Shipping Name"
+          variant="filled"
+          readOnly
+        />
+        <Group position="right" mt="md">
+          <Button
+            color="white"
+            type="submit"
+            onClick={() => {
+              store.set("selectedRecord", hazardousMaterial);
+              store.set("viewModalOpen", false);
+              store.set("editModalOpen", true);
+            }}
+            className={classes.control}
+          >
+            Edit Hazardous Material
+          </Button>
+        </Group>
+      </Box>
+    </Box>
+  );
+}
+
+export function ViewHMModal(): React.ReactElement {
+  const [showViewModal, setShowViewModal] = store.use("viewModalOpen");
+  const [hazardousMaterial] = store.use("selectedRecord");
 
   return (
     <Modal.Root opened={showViewModal} onClose={() => setShowViewModal(false)}>
@@ -45,4 +157,4 @@ export const ViewHMModal: React.FC = () => {
       </Modal.Content>
     </Modal.Root>
   );
-};
+}
