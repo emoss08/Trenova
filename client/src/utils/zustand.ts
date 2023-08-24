402bbe1dffcbd5db8c60e228cc650ba7cc3f92fb
Eric Moss <yeswolfprod@gmail.com>
2023-08-24 19:35:39 -0400

    add: new type for zustand store
    thanks acorn

--- client/src/utils/zustand.ts
+++ client/src/utils/zustand.ts
@@ -1,3 +1,19 @@
+/*
+ * COPYRIGHT(c) 2023 MONTA
+ *
+ * This file is part of Monta.
+ *
+ * The Monta software is licensed under the Business Source License 1.1. You are granted the right
+ * to copy, modify, and redistribute the software, but only for non-production use or with a total
+ * of less than three server instances. Starting from the Change Date (November 16, 2026), the
+ * software will be made available under version 2 or later of the GNU General Public License.
+ * If you use the software in violation of this license, your rights under the license will be
+ * terminated automatically. The software is provided "as is," and the Licensor disclaims all
+ * warranties and conditions. If you use this license's text or the "Business Source License" name
+ * and trademark, you must comply with the Licensor's covenants, which include specifying the
+ * Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use
+ * Grant, and not modifying the license in any other way.
+ */
 import { SetStateAction, useCallback } from "react";
 import { create } from "zustand";
 
@@ -7,6 +23,23 @@ export type EqualityFn<T> = (
   left: T | null | undefined,
   right: T | null | undefined,
 ) => boolean;
+
+export type Store<State> = {
+  get: <K extends keyof State>(key: K) => State[K];
+  getAll: () => State;
+  use: <K extends keyof State>(
+    key: K,
+    defaultValue?: State[K],
+    equalityFn?: EqualityFn<State[K]>,
+  ) => [State[K], (value: SetStateAction<State[K]>) => void];
+  useAll: () => State;
+  set: <K extends keyof State>(key: K, value: SetStateAction<State[K]>) => void;
+  setAll: (state: State) => void;
+  has: <K extends keyof State>(key: K) => boolean;
+  delete: <K extends keyof State>(key: K) => void;
+  update: (state: Partial<State>) => void;
+  reset: () => void;
+};
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 const isFunction = (fn: unknown): fn is Function => typeof fn === "function";
