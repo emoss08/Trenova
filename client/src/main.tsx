e1e758d70e4988eb55a5441ca24c5b83ed159307
Eric Moss <yeswolfprod@gmail.com>
2023-09-01 11:39:40 -0400

    chore: remove react toastify css from main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -18,10 +18,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   // <React.StrictMode>
-  <App />
+  <App />,
   // </React.StrictMode>
 );
