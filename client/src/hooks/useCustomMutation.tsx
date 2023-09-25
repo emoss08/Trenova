9ac6ed37b4876bd78ad97d8e8d135789ee53b4e0
Eric Moss <yeswolfprod@gmail.com>
2023-09-25 00:33:59 -0400

    refactor: useCustomMutation

--- client/src/hooks/useCustomMutation.tsx
+++ client/src/hooks/useCustomMutation.tsx
@@ -15,7 +15,7 @@
  * Grant, and not modifying the license in any other way.
  */
 
-import { useMutation, useQueryClient } from "react-query";
+import { QueryClient, useMutation, useQueryClient } from "react-query";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck, faXmark } from "@fortawesome/pro-solid-svg-icons";
 import React from "react";
@@ -23,154 +23,139 @@ import { NotificationsEvents } from "@mantine/notifications/lib/events";
 import { UseFormReturnType } from "@mantine/form";
 import axios from "@/helpers/AxiosConfig";
 import { APIError } from "@/types/server";
+import { TableStoreProps } from "@/types/tables";
+import { StoreType } from "@/helpers/useGlobalStore";
 
 type MutationOptions = {
   path: string;
   successMessage: string;
   errorMessage?: string;
-  notificationId: string;
   queryKeysToInvalidate?: Array<string>;
   closeModal?: boolean;
-  validationDetail: string;
-  validationFieldName: string;
+  method: "POST" | "PUT" | "PATCH" | "DELETE";
 };
 
-export function useCustomMutation<T extends Record<string, any>>(
+const DEFAULT_ERROR_MESSAGE = "An error occurred.";
+export function useCustomMutation<
+  T extends Record<string, any>,
+  K extends Omit<TableStoreProps<T>, "drawerOpen">,
+>(
   form: UseFormReturnType<T>,
-  store: any,
+  store: StoreType<K>,
   notifications: NotificationsEvents,
   options: MutationOptions,
   onMutationSettled?: () => void,
 ) {
   const queryClient = useQueryClient();
 
-  return useMutation((values: T) => axios.post(options.path, values), {
-    onSuccess: () => {
-      if (options.queryKeysToInvalidate) {
-        queryClient
-          .invalidateQueries(options.queryKeysToInvalidate)
-          .then(() => {
-            notifications.show({
-              title: "Success",
-              message: options.successMessage,
-              color: "green",
-              withCloseButton: true,
-              icon: <FontAwesomeIcon icon={faCheck} />,
-            });
-          });
-      }
-      if (options.closeModal) {
-        store.set("createModalOpen", false);
-      }
+  return useMutation(
+    (values: T) => executeMethod(options.method, options.path, values),
+    {
+      onSuccess: () =>
+        handleSuccess(options, notifications, queryClient, store),
+      onError: (error: any) => handleError(error, options, form, notifications),
+      onSettled: onMutationSettled,
     },
-    onError: (error: any) => {
-      const { data } = error.response;
-      if (data && data.type === "validationError") {
-        handleValidationErrors(
-          data.errors,
-          form,
-          notifications,
-          options.validationDetail,
-          options.validationFieldName,
-        );
-      } else {
-        // General error handling
-        notifications.show({
-          title: "Error",
-          message: options.errorMessage || "An error occurred.",
-          color: "red",
-          withCloseButton: true,
-          icon: <FontAwesomeIcon icon={faXmark} />,
-          autoClose: 10_000, // 10 seconds
-        });
-      }
-    },
-    onSettled: () => {
-      if (onMutationSettled) {
-        onMutationSettled();
-      }
-    },
+  );
+}
+
+function executeMethod(
+  method: MutationOptions["method"],
+  path: string,
+  values: any,
+): Promise<any> {
+  switch (method) {
+    case "POST":
+      return axios.post(path, values);
+    case "PUT":
+      return axios.put(path, values);
+    case "PATCH":
+      return axios.patch(path, values);
+    case "DELETE":
+      return axios.delete(path);
+    default:
+      throw new Error(`Unsupported method: ${method}`);
+  }
+}
+
+function handleSuccess<K>(
+  options: MutationOptions,
+  notifications: NotificationsEvents,
+  queryClient: QueryClient,
+  store: StoreType<K>,
+): void {
+  if (options.queryKeysToInvalidate) {
+    queryClient.invalidateQueries(options.queryKeysToInvalidate).then(() => {
+      showNotification(
+        notifications,
+        "Success",
+        options.successMessage,
+        "green",
+        faCheck,
+      );
+    });
+  }
+
+  const modalKey =
+    options.method === "POST" ? "createModalOpen" : "editModalOpen";
+  if (options.closeModal) {
+    store.set(modalKey as keyof K, false as any);
+  }
+}
+
+function handleError(
+  error: any,
+  options: MutationOptions,
+  form: UseFormReturnType<any>,
+  notifications: NotificationsEvents,
+): void {
+  const { data } = error.response;
+  if (data && data.type === "validationError") {
+    handleValidationErrors(data.errors, form, notifications);
+  } else {
+    showErrorNotification(notifications, options.errorMessage);
+  }
+}
+
+function showNotification(
+  notifications: NotificationsEvents,
+  title: string,
+  message: string,
+  color: string,
+  icon: any,
+): void {
+  notifications.show({
+    title,
+    message,
+    color,
+    withCloseButton: true,
+    icon: <FontAwesomeIcon icon={icon} />,
+    autoClose: 10_000,
   });
+}
+
+function showErrorNotification(
+  notifications: NotificationsEvents,
+  errorMessage?: string,
+): void {
+  showNotification(
+    notifications,
+    "Error",
+    errorMessage || DEFAULT_ERROR_MESSAGE,
+    "red",
+    faXmark,
+  );
 }
 
 function handleValidationErrors<T extends Record<string, any>>(
   errors: APIError[],
   form: UseFormReturnType<T>,
   notifications: NotificationsEvents,
-  notificationDetail: string,
-  validationFieldName: string,
-) {
+): void {
   errors.forEach((e: APIError) => {
     form.setFieldError(e.attr, e.detail);
     if (e.attr === "nonFieldErrors") {
-      notifications.show({
-        title: "Error",
-        message: e.detail,
-        color: "red",
-        withCloseButton: true,
-        icon: <FontAwesomeIcon icon={faXmark} />,
-        autoClose: 10_000, // 10 seconds
-      });
-    } else if (e.attr === "All" && e.detail === notificationDetail) {
-      form.setFieldError(validationFieldName, e.detail);
+      showErrorNotification(notifications, e.detail);
     }
-  });
-}
-
-export function usePutMutation<T extends Record<string, any>>(
-  form: UseFormReturnType<T>,
-  store: any,
-  notifications: NotificationsEvents,
-  options: MutationOptions,
-  onMutationSettled?: () => void,
-) {
-  const queryClient = useQueryClient();
-
-  return useMutation((values: T) => axios.put(options.path, values), {
-    onSuccess: () => {
-      if (options.queryKeysToInvalidate) {
-        queryClient
-          .invalidateQueries(options.queryKeysToInvalidate)
-          .then(() => {
-            notifications.show({
-              title: "Success",
-              message: options.successMessage,
-              color: "green",
-              withCloseButton: true,
-              icon: <FontAwesomeIcon icon={faCheck} />,
-            });
-          });
-      }
-      if (options.closeModal) {
-        store.set("editModalOpen", false);
-      }
-    },
-    onError: (error: any) => {
-      const { data } = error.response;
-      if (data && data.type === "validationError") {
-        handleValidationErrors(
-          data.errors,
-          form,
-          notifications,
-          options.validationDetail,
-          options.validationFieldName,
-        );
-      } else {
-        // General error handling
-        notifications.show({
-          title: "Error",
-          message: options.errorMessage || "An error occurred.",
-          color: "red",
-          withCloseButton: true,
-          icon: <FontAwesomeIcon icon={faXmark} />,
-          autoClose: 10_000, // 10 seconds
-        });
-      }
-    },
-    onSettled: () => {
-      if (onMutationSettled) {
-        onMutationSettled();
-      }
-    },
   });
 }
