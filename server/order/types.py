cd2d1fb087c71c58289a25c510457f015045d50f
Eric Moss <yeswolfprod@gmail.com>
2023-09-29 15:48:26 -0400

    add: type dict for formula variables

---
+++ server/order/types.py
@@ -0,0 +1,34 @@
+# --------------------------------------------------------------------------------------------------
+#  COPYRIGHT(c) 2023 MONTA                                                                         -
+#                                                                                                  -
+#  This file is part of Monta.                                                                     -
+#                                                                                                  -
+#  The Monta software is licensed under the Business Source License 1.1. You are granted the right -
+#  to copy, modify, and redistribute the software, but only for non-production use or with a total -
+#  of less than three server instances. Starting from the Change Date (November 16, 2026), the     -
+#  software will be made available under version 2 or later of the GNU General Public License.     -
+#  If you use the software in violation of this license, your rights under the license will be     -
+#  terminated automatically. The software is provided "as is," and the Licensor disclaims all      -
+#  warranties and conditions. If you use this license's text or the "Business Source License" name -
+#  and trademark, you must comply with the Licensor's covenants, which include specifying the      -
+#  Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use     -
+#  Grant, and not modifying the license in any other way.                                          -
+# --------------------------------------------------------------------------------------------------
+import decimal
+import typing
+
+
+class FormulaVariables(typing.TypedDict):
+    """
+    A dictionary of variables that can be used in a formula.
+    """
+
+    freight_charge: decimal.Decimal
+    other_charge: int
+    mileage: float
+    weight: decimal.Decimal | int
+    stops: int
+    rating_units: int
+    equipment_cost_per_mile: decimal.Decimal
+    hazmat_additional_cost: decimal.Decimal | int
+    temperature_differential: int
