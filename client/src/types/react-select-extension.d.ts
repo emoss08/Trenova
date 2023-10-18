b14b293badfd5d0bbfcbe37beab251eac81684ad
Eric Moss <yeswolfprod@gmail.com>
2023-10-18 00:16:52 -0400

    override: react select types

--- client/src/types/react-select-extension.d.ts
+++ client/src/types/react-select-extension.d.ts
@@ -8,6 +8,12 @@ declare module "react-select/dist/declarations/src/Select" {
   > {
     isFetchError?: boolean;
     formError?: string;
+    isLoading?: boolean;
+    maxOptions?: number;
+    isDisabled?: boolean;
+    isClearable?: boolean;
+    isMulti?: IsMulti;
+    placeholder?: string;
   }
   export interface CreatableProps<
     Option,
@@ -16,5 +22,15 @@ declare module "react-select/dist/declarations/src/Select" {
   > {
     isFetchError?: boolean;
     formError?: string;
+  }
+}
+
+// override the default Props on GroupBase
+
+declare module "react-select/dist/declarations/src/types" {
+  export interface GroupBase<Option> {
+    label: string;
+    options: Options<Option>;
+    value: string;
   }
 }
