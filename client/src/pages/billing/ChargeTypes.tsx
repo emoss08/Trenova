4b740d460f496d61d2fe3fa2c11b0e249365f294
Eric Moss <yeswolfprod@gmail.com>
2023-10-27 22:17:40 -0400

    add: ChargeType page

--- client/src/pages/billing/ChargeTypes.tsx
+++ client/src/pages/billing/ChargeTypes.tsx
@@ -15,21 +15,85 @@
  * Grant, and not modifying the license in any other way.
  */
 
-import React from "react";
-import { Card, Flex } from "@mantine/core";
-import { usePageStyles } from "@/assets/styles/PageStyles";
-import { ChargeTypeTable } from "@/components/charge-types/table/ChargeTypeTable";
+import { DataTable, StatusBadge } from "@/components/common/table/data-table";
+import { DataTableColumnHeader } from "@/components/common/table/data-table-column-header";
+import { Card, CardContent } from "@/components/ui/card";
+import { Checkbox } from "@/components/common/fields/checkbox";
+import { tableStatusChoices } from "@/lib/constants";
+import { FilterConfig } from "@/types/tables";
+import { ColumnDef } from "@tanstack/react-table";
+import { ChargeType } from "@/types/billing";
+import { ChargeTypeDialog } from "@/components/charge-types/charge-type-dialog";
+import { ChargeTypeEditSheet } from "@/components/charge-types/charge-type-edit-dialog";
 
-const ChargeTypes: React.FC = () => {
-  const { classes } = usePageStyles();
+const columns: ColumnDef<ChargeType>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={table.getIsAllPageRowsSelected()}
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+        className="translate-y-[2px]"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+        className="translate-y-[2px]"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "status",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Status" />
+    ),
+    cell: ({ row }) => <StatusBadge status={row.original.status} />,
+    filterFn: (row, id, value) => {
+      return value.includes(row.getValue(id));
+    },
+  },
+  {
+    accessorKey: "name",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Name" />
+    ),
+  },
+  {
+    accessorKey: "description",
+    header: "Description",
+  },
+];
 
+const filters: FilterConfig<ChargeType>[] = [
+  {
+    columnName: "status",
+    title: "Status",
+    options: tableStatusChoices,
+  },
+];
+
+export default function ChargeTypes() {
   return (
-    <Flex>
-      <Card className={classes.card}>
-        <ChargeTypeTable />
-      </Card>
-    </Flex>
+    <Card>
+      <CardContent>
+        <DataTable
+          queryKey="charge-type-table-data"
+          columns={columns}
+          link="/charge_types/"
+          name="Charge Type"
+          exportModelName="ChargeType"
+          filterColumn="name"
+          tableFacetedFilters={filters}
+          TableSheet={ChargeTypeDialog}
+          TableEditSheet={ChargeTypeEditSheet}
+        />
+      </CardContent>
+    </Card>
   );
-};
-
-export default ChargeTypes;
+}
