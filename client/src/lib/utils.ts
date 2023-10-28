38f16467f101d7b3ec6702e9e9a94a03efe38493
Eric Moss <yeswolfprod@gmail.com>
2023-10-27 22:15:58 -0400

    add: USDollarFormat helper function

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,4 +1,20 @@
-import { clsx, type ClassValue } from "clsx";
+/*
+ * COPYRIGHT(c) 2023 MONTA
+ *
+ * This file is part of Monta.
+ *
+ * The Monta software is licensed under the Business Source License 1.1. You are granted the right
+ * to copy, modify, and redistribute the software, but only for non-production use or with a total
+ * of less than three server instances. Starting from the Change Date (November 16, 2026), the
+ * software will be made available under version 2 or later of the GNU General Public License.
+ * If you use the software in violation of this license, your rights under the license will be
+ * terminated automatically. The software is provided "as is," and the Licensor disclaims all
+ * warranties and conditions. If you use this license's text or the "Business Source License" name
+ * and trademark, you must comply with the Licensor's covenants, which include specifying the
+ * Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use
+ * Grant, and not modifying the license in any other way.
+ */
+import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -7,4 +23,15 @@ export function cn(...inputs: ClassValue[]) {
 
 export function upperFirst(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export function USDollarFormat(num: number) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(num);
+}
+
+export function truncateText(str: string, length: number) {
+  return str.length > length ? str.substring(0, length) + "..." : str;
 }
