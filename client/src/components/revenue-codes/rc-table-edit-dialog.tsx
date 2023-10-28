e7b05d43f69ebaf25bb8c597e38d40d35b63b048
Eric Moss <yeswolfprod@gmail.com>
2023-10-27 22:14:38 -0400

    add: RC table dialog options

---
+++ client/src/components/revenue-codes/rc-table-edit-dialog.tsx
@@ -0,0 +1,128 @@
+/*
+ * COPYRIGHT(c) 2023 MONTA
+ *
+ * This file is part of Monta.
+ *
+ * The Monta software is licensed under the Business Source License 1.1. You are granted the right
+ * to copy, modify, and redistribute the software, but only for non-production use or with a total
+ * of less than three server instances. Starting from the Change Date (November 16, 2026), the
+ * software will be made available under version 2 or later of the GNU General Public License.
+ * If you use the software in violation of this license, your rights under the license will be
+ * terminated automatically. The software is provided "as is," and the Licensor disclaims all
+ * warranties and conditions. If you use this license's text or the "Business Source License" name
+ * and trademark, you must comply with the Licensor's covenants, which include specifying the
+ * Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use
+ * Grant, and not modifying the license in any other way.
+ */
+
+import { useTableStore } from "@/stores/TableStore";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import React from "react";
+import { formatDate } from "@/lib/date";
+import {
+  RevenueCode,
+  RevenueCodeFormValues as FormValues,
+} from "@/types/accounting";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { revenueCodeSchema } from "@/lib/validations/accounting";
+import { useForm } from "react-hook-form";
+import { RCForm } from "@/components/revenue-codes/rc-table-dialog";
+import { Button } from "@/components/ui/button";
+import { useCustomMutation } from "@/hooks/useCustomMutation";
+import { toast } from "@/components/ui/use-toast";
+import { useGLAccounts } from "@/hooks/useQueries";
+
+function RCEditForm({
+  revenueCode,
+  open,
+}: {
+  revenueCode: RevenueCode;
+  open: boolean;
+}) {
+  const [isSubmitting, setIsSubmitting] = React.useState<boolean>(false);
+  const { selectGLAccounts, isLoading, isError } = useGLAccounts(open);
+
+  const { handleSubmit, control, reset } = useForm<FormValues>({
+    resolver: yupResolver(revenueCodeSchema),
+    defaultValues: {
+      code: revenueCode.code,
+      description: revenueCode?.description || "",
+      expenseAccount: revenueCode?.expenseAccount || "",
+      revenueAccount: revenueCode?.revenueAccount || "",
+    },
+  });
+
+  const mutation = useCustomMutation<FormValues>(
+    control,
+    toast,
+    {
+      method: "PUT",
+      path: `/revenue_codes/${revenueCode.id}/`,
+      successMessage: "Revenue Code updated successfully.",
+      queryKeysToInvalidate: ["revenue-code-table-data"],
+      closeModal: true,
+      errorMessage: "Failed to update revenue code.",
+    },
+    () => setIsSubmitting(false),
+    reset,
+  );
+
+  const onSubmit = (values: FormValues) => {
+    setIsSubmitting(true);
+    mutation.mutate(values);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <RCForm
+        control={control}
+        glAccounts={selectGLAccounts}
+        isLoading={isLoading}
+        isError={isError}
+      />
+      <DialogFooter className="mt-6">
+        <Button
+          type="submit"
+          isLoading={isSubmitting}
+          loadingText="Saving Changes..."
+        >
+          Save
+        </Button>
+      </DialogFooter>
+    </form>
+  );
+}
+
+export function RCTableEditDialog({
+  open,
+  onOpenChange,
+}: {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}) {
+  const [revenueCode] = useTableStore.use("currentRecord");
+
+  if (!revenueCode) return null;
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{revenueCode.code}</DialogTitle>
+        </DialogHeader>
+        <DialogDescription>
+          Last updated on&nbsp;
+          {revenueCode && formatDate(revenueCode.modified)}
+        </DialogDescription>
+        {revenueCode && <RCEditForm revenueCode={revenueCode} open={open} />}
+      </DialogContent>
+    </Dialog>
+  );
+}
