a70a86330d44fa38cf2ee337636285be4687f066
Eric Moss <yeswolfprod@gmail.com>
2023-11-03 00:09:28 -0400

    change: tailwind config screen sizes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -30,7 +30,20 @@ module.exports = {
       center: true,
       padding: "2rem",
       screens: {
-        "2xl": "1400px",
+        sm: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        md: "768px",
+        // => @media (min-width: 768px) { ... }
+
+        lg: "1240px",
+        // => @media (min-width: 1024px) { ... }
+
+        xl: "1280px",
+        // => @media (min-width: 1280px) { ... }
+
+        "2xl": "1536px",
+        // => @media (min-width: 1536px) { ... }
       },
     },
     extend: {
