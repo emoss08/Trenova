cae16678fd7744fac836bf958e5a340af7ac07c5
Eric Moss <yeswolfprod@gmail.com>
2023-11-02 23:54:12 -0400

    chore: small project conf changes

---
+++ server/monkeytype_config.py
@@ -0,0 +1,36 @@
+# --------------------------------------------------------------------------------------------------
+#  COPYRIGHT(c) 2023 MONTA                                                                         -
+#                                                                                                  -
+#  This file is part of Monta.                                                                     -
+#                                                                                                  -
+#  The Monta software is licensed under the Business Source License 1.1. You are granted the right -
+#  to copy, modify, and redistribute the software, but only for non-production use or with a total -
+#  of less than three server instances. Starting from the Change Date (November 16, 2026), the     -
+#  software will be made available under version 2 or later of the GNU General Public License.     -
+#  If you use the software in violation of this license, your rights under the license will be     -
+#  terminated automatically. The software is provided "as is," and the Licensor disclaims all      -
+#  warranties and conditions. If you use this license's text or the "Business Source License" name -
+#  and trademark, you must comply with the Licensor's covenants, which include specifying the      -
+#  Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use     -
+#  Grant, and not modifying the license in any other way.                                          -
+# --------------------------------------------------------------------------------------------------
+import os
+from contextlib import contextmanager
+from typing import Iterator
+
+# 3rd-party
+from monkeytype.config import DefaultConfig
+
+
+# MonkeyType Configuration
+class MonkeyConfig(DefaultConfig):
+    @contextmanager
+    def cli_context(self, command: str) -> Iterator[None]:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_settings")
+        import django
+
+        django.setup()
+        yield
+
+
+CONFIG = MonkeyConfig()
