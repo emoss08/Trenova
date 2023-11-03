bf2f1b16a274e3effa7de97d9df4036a6838eb0a
Eric Moss <yeswolfprod@gmail.com>
2023-11-02 23:58:21 -0400

    add: signal for creating user profile thumbnail

--- server/accounts/apps.py
+++ server/accounts/apps.py
@@ -16,8 +16,18 @@
 # --------------------------------------------------------------------------------------------------
 
 from django.apps import AppConfig
+from django.db.models.signals import pre_save
 
 
 class AccountConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "accounts"
+
+    def ready(self) -> None:
+        from accounts import signals
+
+        pre_save.connect(
+            signals.create_thumbnail,
+            sender="accounts.UserProfile",
+            dispatch_uid="create_thumbnail",
+        )
