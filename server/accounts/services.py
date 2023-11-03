a361a60c00d13b50fe3b89af7b789af8bc214860
Eric Moss <yeswolfprod@gmail.com>
2023-11-02 23:56:39 -0400

    add: generate thumbnail service
    add service to optimize the user uploaded profile image into a smaller webp file.

--- server/accounts/services.py
+++ server/accounts/services.py
@@ -15,7 +15,17 @@
 #  Grant, and not modifying the license in any other way.                                          -
 # --------------------------------------------------------------------------------------------------
 
+import logging
 import secrets
+
+from django.core.exceptions import ValidationError
+from django.core.files.base import ContentFile
+from PIL import Image, UnidentifiedImageError
+
+from accounts import models
+from utils.helpers import optimize_image
+
+log = logging.getLogger(__name__)
 
 
 def generate_key() -> str:
@@ -25,3 +35,67 @@ def generate_key() -> str:
         str: A random key.
     """
     return secrets.token_hex(20)
+
+
+def generate_thumbnail(
+    *, user_profile: models.UserProfile, size: tuple[int, int]
+) -> None:
+    """Generates a thumbnail for a user profile picture.
+
+    This function opens the user's profile picture, resizes it into a thumbnail using
+    bicubic resampling, converts the colorspace to RGB, and saves it as a WebP format.
+
+    The thumbnail is saved in-memory before being added to the user's profile.
+    If the user doesn't have a profile picture, their thumbnail is set to `None`.
+
+    In case an error is encountered during opening or resizing of the image,
+    it is logged and the same is raised again. If the error is about the image being unidentified,
+    a validation error message is raised along with the original error.
+
+    Args:
+        user_profile (models.UserProfile): A UserProfile instance for which to generate
+        a thumbnail.
+        size (tuple[int, int]): A tuple containing the width and height of the thumbnail.
+
+    Returns:
+        None: This function does not return anything.
+
+    Raises:
+        UnidentifiedImageError: If the given image is unidentified.
+        ValidationError: If image uploading has an issue due to it being invalid.
+        Exception: If an unexpected error is raised during the thumbnail generation process.
+    """
+
+    # If the user doesn't have a profile picture, don't generate a thumbnail.
+    if not user_profile.profile_picture:
+        user_profile.thumbnail = None
+        return
+
+    try:
+        # Open the image.
+        img = Image.open(user_profile.profile_picture)
+
+        # Optimize the image.
+        optimized_img = optimize_image(img, size)
+
+        # Save the thumbnail to the user's profile.
+        user_profile.thumbnail = ContentFile(
+            optimized_img.getvalue(),
+            f"{user_profile.user.username}_thumbnail.webp",
+        )
+
+        # Close the opened resources.
+        img.close()
+        optimized_img.close()
+
+    except* UnidentifiedImageError as exc:
+        log.error(
+            f"Uploaded image for {user_profile.user.username} is invalid. Exception: {exc}"
+        )
+        raise ValidationError(
+            {"profile_picture": "The image is invalid. Please try again."},
+            code="invalid",
+        ) from exc
+    except* Exception as exc:
+        log.exception(f"Failed to generate thumbnail for {user_profile.user.username}.")
+        raise exc
