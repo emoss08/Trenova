460c6bcc7671ca650502536649a3bf4de8db7bb8
Eric Moss <yeswolfprod@gmail.com>
2023-11-14 22:11:49 -0500

    fix: button responsiveness

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -21,7 +21,7 @@ import { Loader2 } from "lucide-react";
 import * as React from "react";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 truncate",
   {
     variants: {
       variant: {
