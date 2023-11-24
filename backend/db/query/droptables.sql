4bd011df62577778214c7b02982123af4b9d1222
Eric Moss <yeswolfprod@gmail.com>
2023-11-24 15:13:34 -0500

    add: query to drop all tables
    Do not use this in production this is simply for development if you need to drop all the tables and create them.

---
+++ backend/db/query/droptables.sql
@@ -0,0 +1,23 @@
+drop table if exists user_profiles cascade;
+
+drop table if exists job_titles cascade;
+
+drop table if exists email_controls cascade;
+
+drop table if exists email_profiles cascade;
+
+drop table if exists user_roles cascade;
+
+drop table if exists users cascade;
+
+drop table if exists departments cascade;
+
+drop table if exists role_permissions cascade;
+
+drop table if exists roles cascade;
+
+drop table if exists permissions cascade;
+
+drop table if exists organizations cascade;
+
+drop table if exists business_units cascade;
