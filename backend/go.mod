1932f8345097df4214edf139eaae1aad225c849b
Eric Moss <yeswolfprod@gmail.com>
2023-11-24 01:25:38 -0500

    change: logging middleware

--- backend/go.mod
+++ backend/go.mod
@@ -8,6 +8,7 @@ require (
 )
 
 require (
+	github.com/fatih/color v1.16.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
@@ -15,6 +16,8 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.4.3 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/sagikazarmark/locafero v0.3.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
--- backend/middleware/logging.go
+++ backend/middleware/logging.go
@@ -4,12 +4,13 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/fatih/color"
 )
 
 // LoggingMiddleware logs the incoming HTTP request & its duration.
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Start timer
 		start := time.Now()
 
 		// Wrap the response writer to capture the status code
@@ -18,9 +19,56 @@ func LoggingMiddleware(next http.Handler) http.Handler {
 		// Process request
 		next.ServeHTTP(wrappedWriter, r)
 
-		// Log request details
-		log.Printf("%s %s %d %s", r.Method, r.URL.Path, wrappedWriter.statusCode, time.Since(start))
+		duration := time.Since(start)
+
+		statusColor := colorForStatus(wrappedWriter.statusCode)
+		methodColor := colorForMethod(r.Method)
+
+		// Using Sprintf to format colored strings
+		status := statusColor.Sprintf("%3d", wrappedWriter.statusCode)
+		method := methodColor.Sprint(r.Method)
+
+		log.Printf("[MONTA] %v | %s | %13v | %15s | %-7s %s",
+			start.Format("2006/01/02 - 15:04:05"),
+			status,
+			duration,
+			r.RemoteAddr,
+			method,
+			r.URL.Path,
+		)
 	})
+}
+
+func colorForStatus(code int) *color.Color {
+	switch {
+	case code >= 200 && code < 300:
+		return color.New(color.FgGreen)
+	case code >= 300 && code < 400:
+		return color.New(color.FgCyan)
+	case code >= 400 && code < 500:
+		return color.New(color.FgYellow)
+	default:
+		return color.New(color.FgRed)
+	}
+}
+
+func colorForMethod(method string) *color.Color {
+	switch method {
+	case "GET":
+		return color.New(color.FgBlue)
+	case "POST":
+		return color.New(color.FgCyan)
+	case "PUT":
+		return color.New(color.FgYellow)
+	case "DELETE":
+		return color.New(color.FgRed)
+	case "PATCH":
+		return color.New(color.FgGreen)
+	case "HEAD":
+		return color.New(color.FgMagenta)
+	default:
+		return color.New(color.FgWhite)
+	}
 }
 
 // responseWriter is a minimal wrapper for http.ResponseWriter that allows the status code to be captured
