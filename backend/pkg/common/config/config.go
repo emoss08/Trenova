27d59d818f2dfb081d1269e7a5d03623d2c7bcbf
Eric Moss <yeswolfprod@gmail.com>
2023-11-23 23:16:40 -0500

    change: config

--- backend/pkg/common/config/config.go
+++ backend/pkg/common/config/config.go
@@ -1,9 +1,10 @@
-package config
+package envs
 
 import "github.com/spf13/viper"
 
 type Config struct {
-	Port string `mapstructure:"PORT"`
+	Port   string `mapstructure:"PORT"`
+	DB_DSN string `mapstructure:"DB_DSN"`
 }
 
 func LoadConfig() (c Config, err error) {
