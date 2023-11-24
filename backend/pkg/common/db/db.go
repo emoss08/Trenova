9a9b1a6ea79d9500fe42d9ca4bd32a841634ea6c
Eric Moss <yeswolfprod@gmail.com>
2023-11-23 23:15:51 -0500

    change: db init

--- backend/pkg/common/db/db.go
+++ backend/pkg/common/db/db.go
@@ -1,21 +1,33 @@
 package db
 
 import (
+	"backend/pkg/common/models"
 	"log"
 
-	"github.com/YOUR_USERNAME/go-gin-api-medium/pkg/common/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
+var mods = []interface{}{
+	&models.Organization{},
+	&models.BusinessUnit{},
+	&models.JobTitle{},
+	&models.User{},
+	&models.UserProfile{},
+	&models.Department{},
+	&models.EmailControl{},
+	&models.EmailProfile{},
+}
+
 func Init(url string) *gorm.DB {
+
 	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
 
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	db.AutoMigrate(&models.Book{})
+	db.AutoMigrate(mods...)
 
 	return db
 }
