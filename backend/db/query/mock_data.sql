7c380e2c821afaa0c276a1c70bc916288d8adbb2
Eric Moss <yeswolfprod@gmail.com>
2023-11-25 00:32:27 -0500

    add: mock data helper sql and http request

---
+++ backend/db/query/mock_data.sql
@@ -0,0 +1,7 @@
+-- INSERT INTO business_units (name) VALUES ('Monta Transportation')
+
+-- SELECT * FROM business_units;
+
+-- INSERT INTO organizations (name, business_unit_id) VALUES ('Monta Transportation', 'e8c0d46c-7ff6-4894-92e8-c9613d22909a');
+
+SELECT * FROM organizations;
