4b97e274b3d551d880c5e252ad6822b457d53b0c
Eric Moss <yeswolfprod@gmail.com>
2023-11-25 01:15:26 -0500

    move: go into own repo

--- backend/Makefile
+++
@@ -1,2 +0,0 @@
-server:
-    go run cmd/main.go
--- backend/models/department.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Department struct {
-	BaseModel
-	Name        string `gorm:"size:255;" json:"name" validate:"required"`
-	Description string `gorm:"type:text;" json:"description" validate:"required"`
-}
