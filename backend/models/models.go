80dfc47092df4eafa14db3b8d573f44a9bccc0be
Eric Moss <yeswolfprod@gmail.com>
2023-11-25 00:32:12 -0500

    add: new api error function

--- backend/models/models.go
+++ backend/models/models.go
@@ -1,5 +1,7 @@
 package models
 
+import "encoding/json"
+
 type RequestData struct {
 	Name string `json:"name" validate:"required"`
 }
@@ -14,4 +16,23 @@ type APIErrorResponse struct {
 	Type    string       `json:"type"`
 	Message string       `json:"message"`
 	Errors  []FieldError `json:"errors"`
+}
+
+func NewAPIError(statusCode int, errorType, message string, fieldErrors []FieldError) error {
+	return &APIErrorResponse{
+		Type:    errorType,
+		Message: message,
+		Errors:  fieldErrors,
+	}
+}
+
+func (r *APIErrorResponse) Error() string {
+	// Convert the APIErrorResponse to a JSON string for the error message.
+	// Error messages are typically strings, so you might just return a simple message
+	// or format the struct as a JSON string.
+	b, err := json.Marshal(r)
+	if err != nil {
+		return "error marshalling APIErrorResponse"
+	}
+	return string(b)
 }
