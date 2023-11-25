74db5c9598d8f6b7909713e1c31f9bf25a2f3c8b
Eric Moss <yeswolfprod@gmail.com>
2023-11-25 01:15:26 -0500

    move: go into own repo

--- backend/db/query/mock_data.sql
+++
@@ -1,7 +0,0 @@
--- INSERT INTO business_units (name) VALUES ('Monta Transportation')
-
--- SELECT * FROM business_units;
-
--- INSERT INTO organizations (name, business_unit_id) VALUES ('Monta Transportation', 'e8c0d46c-7ff6-4894-92e8-c9613d22909a');
-
-SELECT * FROM organizations;
