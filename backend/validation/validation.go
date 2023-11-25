b5cdc0a5df55110a6239f7173a3f6bf2c4700f37
Eric Moss <yeswolfprod@gmail.com>
2023-11-25 00:30:43 -0500

    change: validation
    change validation to include dot notation on nested structs. Example: `userProfile.firstName` will be given in the response of invalid input.

--- backend/validation/validation.go
+++ backend/validation/validation.go
@@ -4,6 +4,7 @@ import (
 	"backend/models"
 	"reflect"
 	"regexp"
+	"strings"
 
 	"github.com/go-playground/locales/en"
 	ut "github.com/go-playground/universal-translator"
@@ -26,17 +27,53 @@ func init() {
 	validate.RegisterValidation("usazipcode", validateUSAZipCode)
 
 	validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
-		name := fld.Tag.Get("json")
+		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
 		if name == "-" {
 			return ""
 		}
-		return name
+
+		var path []string
+		if name != "" {
+			path = append(path, name)
+		}
+
+		// Traverse up the field's parent structs
+		for t := fld.Type; t.Kind() == reflect.Ptr || t.Kind() == reflect.Struct; {
+			if t.Kind() == reflect.Ptr {
+				t = t.Elem() // Get the type that the pointer points to
+			}
+
+			if t.Kind() != reflect.Struct {
+				break
+			}
+
+			// Traverse up to parent struct
+			if parentField, found := t.FieldByName(fld.Name); found && !parentField.Anonymous {
+				if parentJsonTag := parentField.Tag.Get("json"); parentJsonTag != "" {
+					parentName := strings.SplitN(parentJsonTag, ",", 2)[0]
+					if parentName != "" {
+						path = append([]string{parentName}, path...)
+					}
+				}
+			} else {
+				break
+			}
+		}
+
+		return strings.Join(path, ".")
 	})
 
 	validate.RegisterTranslation("required", trans, func(ut ut.Translator) error {
 		return ut.Add("required", "`{0}` is required and cannot be empty", true)
 	}, func(ut ut.Translator, fe validator.FieldError) string {
 		t, _ := ut.T("required", fe.Field())
+		return t
+	})
+
+	validate.RegisterTranslation("unique", trans, func(ut ut.Translator) error {
+		return ut.Add("unique", "This {0} already exists", true)
+	}, func(ut ut.Translator, fe validator.FieldError) string {
+		t, _ := ut.T("unique", fe.Field())
 		return t
 	})
 }
@@ -55,10 +92,17 @@ func ValidateStruct(data interface{}) ([]models.FieldError, error) {
 		if validationErrors, ok := err.(validator.ValidationErrors); ok {
 			var fieldErrors []models.FieldError
 			for _, ve := range validationErrors {
+				// Process the namespace to exclude 'CreateUserRequest'
+				namespace := ve.Namespace()
+				fields := strings.Split(namespace, ".")
+				if len(fields) > 1 {
+					namespace = strings.Join(fields[1:], ".")
+				}
+
 				fieldErrors = append(fieldErrors, models.FieldError{
 					Code:   "validationError",
 					Detail: ve.Translate(trans),
-					Attr:   ve.Field(),
+					Attr:   namespace,
 				})
 			}
 			return fieldErrors, nil
