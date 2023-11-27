4cac537a8045e5ef9dfd181a58db8001d4252604
Eric Moss <yeswolfprod@gmail.com>
2023-11-27 00:05:37 -0500

    fix: layout
    Issue where when dropping expanding a table row would cause two scrollbars

--- client/src/components/layout/layout.tsx
+++ client/src/components/layout/layout.tsx
@@ -15,18 +15,18 @@
  * Grant, and not modifying the license in any other way.
  */
 
-import { RainbowTopBar } from "@/components/layout/topbar";
-import { useUserStore } from "@/stores/AuthStore";
-import React from "react";
-import { NavMenu } from "@/components/layout/navbar";
-import { Skeleton } from "@/components/ui/skeleton";
-import { UserAvatarMenu } from "@/components/layout/user-avatar-menu";
 import { Breadcrumb } from "@/components/layout/breadcrumb";
-import { Toaster } from "@/components/ui/toaster";
-import { User } from "@/types/accounts";
-import { useUser } from "@/hooks/useQueries";
-import { Footer } from "@/components/layout/footer";
+import { NavMenu } from "@/components/layout/navbar";
 import { SiteSearch } from "@/components/layout/site-search";
+import { RainbowTopBar } from "@/components/layout/topbar";
+import { UserAvatarMenu } from "@/components/layout/user-avatar-menu";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Toaster } from "@/components/ui/toaster";
+import { useUser } from "@/hooks/useQueries";
+import { useUserStore } from "@/stores/AuthStore";
+import { User } from "@/types/accounts";
+import React from "react";
+import { Footer } from "./footer";
 
 /**
  * LayoutProps defines the props for the Layout components.
@@ -41,21 +41,19 @@ type LayoutProps = {
  */
 export function Layout({ children }: LayoutProps) {
   const { userId } = useUserStore.get("user");
-
+  //               (userData as User) && <UserAvatarMenu user={userData as User} />
   // Fetch user data based on userId
   const { data: userData, isLoading: isUserDataLoading } = useUser(userId);
 
   return (
-    <div className="relative flex flex-col h-screen">
+    // Use min-h-screen instead of h-screen to prevent overflow from causing double scrollbars
+    <div className="flex flex-col min-h-screen">
       <header className="bg-background sticky top-0 z-50 w-full border-b">
-        {/* Rainbow Header */}
         <RainbowTopBar />
         <div className="flex justify-between items-center h-14 w-full px-4">
-          {/* Navigation Menu with a little margin on the left */}
           <div className="ml-4">
             <NavMenu />
           </div>
-          {/* User Avatar Menu with a little margin on the right */}
           <div className="mr-4">
             {isUserDataLoading ? (
               <div className="flex items-center justify-end space-x-2">
@@ -67,22 +65,21 @@ export function Layout({ children }: LayoutProps) {
           </div>
         </div>
       </header>
-      {/* Main content and aside menu */}
-      <div className="flex flex-1 overflow-hidden">
-        {/* Main Content Area */}
-        <div className="flex-1 overflow-y-auto">
-          <div className="mx-auto px-6 sm:px-6 md:12 xl:px-48">
-            <Breadcrumb />
-            {/* Site Search Combobox Dialog */}
-            <SiteSearch />
-            {children}
-            <Toaster />
-          </div>
+
+      {/* Main content should allow for y-axis overflow only */}
+      <main className="flex-1 overflow-y-auto">
+        <div className="mx-auto px-6 sm:px-6 md:12 xl:px-48">
+          <Breadcrumb />
+          <SiteSearch />
+          {children}
+          <Toaster />
         </div>
-      </div>
+      </main>
 
       {/* Footer */}
-      <Footer />
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
