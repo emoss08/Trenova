a8335bb1855f504acf0050ad14fe9416facb69fa
Eric Moss <yeswolfprod@gmail.com>
2023-11-27 22:50:59 -0500

    change: improvements to models to type script

--- server/scripts/models_to_type.py
+++ server/scripts/models_to_type.py
@@ -14,12 +14,17 @@
 #  Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use     -
 #  Grant, and not modifying the license in any other way.                                          -
 # --------------------------------------------------------------------------------------------------
+
 import argparse
 import ast
 import os
+import typing
+from pathlib import Path
 
 import rich
 from rich.progress import Progress
+
+type ModelReturnType = dict[str, list[tuple[str, str]]]
 
 # Define the mapping from Django fields to TypeScript types
 TYPE_MAP = {
@@ -56,17 +61,50 @@ TYPE_MAP = {
 }
 
 
-# Function to convert snake_case to camelCase
 def snake_to_camel(snake_str: str) -> str:
+    """Converts a snake_case string to camelCase.
+
+    This function takes a string formatted in snake_case (with underscores between words)
+    and converts it to camelCase, where the first word is in lowercase and each subsequent
+    word starts with an uppercase letter, with no intervening spaces or underscores.
+
+    Args:
+        snake_str (str): The snake_case string to convert.
+
+    Returns:
+        str: The converted camelCase string.
+
+    Examples:
+        >>> snake_to_camel("this_is_a_test")
+        'thisIsATest'
+    """
     components = snake_str.split("_")
     return components[0] + "".join(x.title() for x in components[1:])
 
 
 class ModelVisitor(ast.NodeVisitor):
+    """Visitor class to traverse through the AST of a Django model file.
+
+    This class extends `ast.NodeVisitor` to process class definitions (models) in
+    a Django models.py file. It extracts field names, types, and optionalities, and
+    stores them in a dictionary for later processing.
+
+    Attributes:
+        models (dict): A dictionary storing the model fields and their corresponding types.
+    """
     def __init__(self):
         self.models = {}
 
     def visit_ClassDef(self, node: ast.ClassDef) -> None:
+        """Visits a ClassDef node and processes the Django model fields.
+
+        Extracts the field names and types from the class definition, handling
+        Django-specific field attributes like `null` and `blank`. Updates the `models`
+        attribute with the processed information.
+
+        Args:
+            node (ast.ClassDef): The ClassDef node representing a Django model.
+        """
         fields = []
 
         for stmt in node.body:
@@ -114,9 +152,28 @@ class ModelVisitor(ast.NodeVisitor):
             self.models[node.name] = fields
 
 
-def parse_model_file(path: str) -> dict[str, list[tuple[str, str]]]:
-    with open(path) as file:
-        tree = ast.parse(file.read())
+def parse_model_file(file_path: Path) -> ModelReturnType:
+    """Parses a Django model file and extracts model definitions as AST.
+
+    Reads a Django model file, parses it into an AST, and then uses `ModelVisitor`
+    to extract model field information.
+
+    Args:
+        file_path (Path): The path to the Django model file.
+
+    Returns:
+        Dict[str, List[Tuple[str, str]]]: A dictionary where keys are model names and
+        values are lists of tuples containing field names and their types.
+
+    Raises:
+        Exception: If there is an error in reading or parsing the file.
+    """
+    try:
+        with file_path.open() as file:
+            tree = ast.parse(file.read())
+    except Exception as e:
+        rich.print(f"[red]Error parsing {file_path}: {e}")
+        return {}
 
     visitor = ModelVisitor()
     visitor.visit(tree)
@@ -124,42 +181,77 @@ def parse_model_file(path: str) -> dict[str, list[tuple[str, str]]]:
 
 
 def write_ts_interface(
-    models: dict[str, list[tuple[str, str]]], output_path: str
+    models: ModelReturnType, output_file: Path
 ) -> None:
-    with open(output_path, "w") as file:
-        for model_name, fields in models.items():
-            file.write(f"export interface {model_name} extends BaseModel {{\n")
-            for field_name, ts_type in fields:
-                file.write(f"  {field_name}: {ts_type};\n")
-            file.write("}\n\n")
+    """Writes TypeScript interfaces for Django models to a file.
+
+    Takes the extracted model information and generates TypeScript interfaces, writing
+    them to the specified output file.
+
+    Args:
+        models (Dict[str, List[Tuple[str, str]]]): The dictionary containing model data.
+        output_file (Path): The path where the TypeScript file will be written.
+        
+    Returns:
+        None: This function does not return anything.
+
+    Raises:
+        Exception: If there is an error in writing to the file.
+    """
+    try:
+        with output_file.open("w") as file:
+            for model_name, fields in models.items():
+                file.write(f"export type {model_name} = BaseModel & {{\n")
+                for field_name, ts_type in fields:
+                    file.write(f"  {field_name}: {ts_type};\n")
+                file.write("}\n\n")
+    except Exception as e:
+        rich.print(f"[red]Error writing to {output_file}: {e}")
 
 
-def main(ignore_dirs: list[str]) -> None:
+def process_directory(
+    root: Path, ignore_dirs: list[str], progress: Progress, task
+) -> None:
+    """Processes each Django model file in a directory recursively.
+
+    Walks through the given directory and its subdirectories, ignores specified directories,
+    and processes each 'models.py' file found. For each model file, it generates TypeScript
+    interfaces and writes them to an output file.
+
+    Args:
+        root (Path): The root directory to start processing from.
+        ignore_dirs (List[str]): A list of directory names to ignore.
+        progress (Progress): Rich library's progress display object.
+        task: The current progress task.
+        
+    Returns:
+        None: This function does not return anything.
+    """
+    for path in root.glob("**/models.py"):
+        if any(ignored_dir in path.parts for ignored_dir in ignore_dirs):
+            continue
+
+        if models := parse_model_file(path):
+            output_file = Path("ts_models") / f"{path.parent.name}-models.ts"
+            write_ts_interface(models, output_file)
+
+            for model_name in models.keys():
+                progress.update(
+                    task,
+                    advance=10,
+                    description=f"[cyan]Generating interface for {model_name}...",
+                )
+
+
+def main(ignore_dirs: list[str] | None) -> None:
+    ignore_dirs = ignore_dirs or []
     os.makedirs("ts_models", exist_ok=True)
 
     with Progress() as progress:
-        task = progress.add_task("[cyan]Starting...", total=100)
+        task = progress.add_task("[cyan]Scanning for Django models...", total=100)
+        process_directory(Path(".."), ignore_dirs, progress, task)
 
-        for root, dirs, files in os.walk(".."):
-            dirs[:] = [d for d in dirs if d not in ignore_dirs]
-
-            for name in files:
-                if name == "models.py":
-                    file_path = os.path.join(root, name)
-                    models = parse_model_file(file_path)
-                    dir_name = os.path.basename(root)
-                    output_file = os.path.join("ts_models", f"{dir_name}-models.ts")
-                    write_ts_interface(models, output_file)
-
-                    for model_name in models.keys():
-                        progress.update(
-                            task,
-                            advance=10,
-                            description=f"[cyan]Generating interface for {model_name}...",
-                        )
-
-        progress.stop()
-        rich.print("[green]Done!")
+    rich.print("[green]Conversion complete!")
 
 
 if __name__ == "__main__":
@@ -168,4 +260,4 @@ if __name__ == "__main__":
     )
     parser.add_argument("--ignore_dirs", nargs="+", help="directories to ignore")
     args = parser.parse_args()
-    main(args.ignore_dirs or [])
+    main(args.ignore_dirs)
