5e75d057234e99669e69330f8bf8d1a46e60fd2a
Eric Moss <yeswolfprod@gmail.com>
2023-12-02 23:17:02 -0500

    chore: split select input into two files

--- client/src/components/common/fields/select-input.tsx
+++ client/src/components/common/fields/select-input.tsx
@@ -15,284 +15,23 @@
  * Grant, and not modifying the license in any other way.
  */
 
-import {
-  Command,
-  CommandGroup,
-  CommandItem,
-  CommandList,
-} from "@/components/ui/command";
 import { cn } from "@/lib/utils";
-import { CaretSortIcon, CheckIcon, Cross2Icon } from "@radix-ui/react-icons";
-import { AlertTriangle } from "lucide-react";
-import React, { memo, useCallback, useEffect, useRef, useState } from "react";
-import {
-  Path,
-  PathValue,
-  UseControllerProps,
-  useController,
-} from "react-hook-form";
-import Select, {
-  ClearIndicatorProps,
-  DropdownIndicatorProps,
-  GroupBase,
-  IndicatorSeparatorProps,
-  MenuListProps,
-  OptionProps,
-  OptionsOrGroups,
-  Props,
-  ValueContainerProps,
-  components,
-} from "react-select";
+import { UseControllerProps, useController } from "react-hook-form";
+import Select, { GroupBase, OptionsOrGroups, Props } from "react-select";
 import CreatableSelect, { CreatableProps } from "react-select/creatable";
 import { Label } from "./label";
-
-/**
- * Option type for the SelectInput component.
- */
-type SelectOption = {
-  readonly label: string;
-  readonly value: string | boolean;
-};
-
-/**
- * Option component for the SelectInput component.
- * @param props {OptionProps}
- * @constructor Option
- */
-function Option({ ...props }: OptionProps) {
-  return (
-    <components.Option
-      className="relative flex cursor-default select-none rounded-sm px-3 py-1.5 text-xs outline-none my-1 hover:bg-accent hover:cursor-pointer hover:rounded-sm"
-      {...props}
-    >
-      {props.label}
-      {props.isSelected && (
-        <CheckIcon className="absolute top-1/2 right-3 transform -translate-y-1/2 h-4 w-4" />
-      )}
-    </components.Option>
-  );
-}
-
-/**
- * DropdownIndicator component for the SelectInput component.
- * @param props {DropdownIndicatorProps}
- * @constructor DropdownIndicator
- */
-function DropdownIndicator(props: DropdownIndicatorProps) {
-  return (
-    <components.DropdownIndicator {...props}>
-      {props.selectProps["aria-invalid"] ? (
-        <AlertTriangle size={15} className="text-red-500" />
-      ) : (
-        <CaretSortIcon className="h-4 w-4 shrink-0" />
-      )}
-    </components.DropdownIndicator>
-  );
-}
-
-/**
- * IndicatorSeparator component for the SelectInput component.
- * @param props {IndicatorSeparatorProps}
- * @constructor IndicatorSeparator
- */
-function IndicatorSeparator(props: IndicatorSeparatorProps) {
-  return (
-    <span
-      className={cn(
-        "bg-foreground/30 h-6 w-px",
-        props.selectProps["aria-invalid"] && "bg-red-500",
-      )}
-    />
-  );
-}
-
-/**
- * ClearIndicator component for the SelectInput component.
- * @param props {ClearIndicatorProps}
- * @constructor ClearIndicator
- */
-function ClearIndicator(props: ClearIndicatorProps) {
-  return (
-    <components.ClearIndicator {...props}>
-      <Cross2Icon className="h-4 w-4 shrink-0" />
-    </components.ClearIndicator>
-  );
-}
-
-/**
- * ValueContainer component for the SelectInput component.
- * @param children {React.ReactNode}
- * @param rest {ValueContainerProps}
- * @constructor ValueContainer
- */
-function ValueContainer({ children, ...rest }: ValueContainerProps) {
-  const selectedCount = rest.getValue().length;
-  const conditional = selectedCount < 3;
-  const { ValueContainer } = components;
-
-  let firstChild: React.ReactNode[] | null = [];
-
-  if (!conditional && Array.isArray(children)) {
-    firstChild = [children[0].shift(), children[1]];
-  }
-
-  return (
-    <ValueContainer {...rest}>
-      {conditional ? children : firstChild}
-      {!conditional && ` and ${selectedCount - 1} others`}
-    </ValueContainer>
-  );
-}
-
-/**
- * Description component for the SelectInput component.
- * @param description {string}
- * @constructor SelectDescription
- */
-function SelectDescription({ description }: { description: string }) {
-  return <p className="text-xs text-foreground/70">{description}</p>;
-}
-
-/**
- * MenuList component for the SelectInput component.
- * @param children {React.ReactNode}
- * @param props {MenuListProps}
- * @constructor MenuList
- */
-function MenuList({
-  children,
-  ...props
-}: MenuListProps & {
-  selectProps?: {
-    maxOptions?: number;
-    formError?: string;
-  };
-}) {
-  return (
-    <components.MenuList {...props}>
-      {Array.isArray(children)
-        ? children.slice(0, props.selectProps?.maxOptions)
-        : children}
-    </components.MenuList>
-  );
-}
-
-/**
- * Gets the label of the option by its value.
- * @param value {PathValue<T, Path<T>>}
- * @param options {OptionsOrGroups<SelectOption, GroupBase<SelectOption>>}
- */
-function getLabelByValue<T extends Record<string, unknown>>(
-  value: PathValue<T, Path<T>>,
-  options: OptionsOrGroups<SelectOption, GroupBase<SelectOption>>,
-) {
-  const option = options.find((opt) => opt.value === value);
-  return option?.label || value;
-}
-
-/**
- * Processes the value of the SelectInput component.
- * @param value {PathValue<T, Path<T>>}
- * @param options {OptionsOrGroups<SelectOption, GroupBase<SelectOption>>}
- * @param isMulti {boolean}
- * @constructor ValueProcessor
- */
-function ValueProcessor<T extends Record<string, unknown>>(
-  value: PathValue<T, Path<T>>,
-  options: OptionsOrGroups<SelectOption, GroupBase<SelectOption>>,
-  isMulti?: boolean,
-) {
-  if (isMulti) {
-    const valuesArray = Array.isArray(value) ? value : [];
-    return valuesArray.map((val) => ({
-      value: val,
-      label: getLabelByValue(val, options),
-    }));
-  }
-
-  if (!value) return undefined;
-
-  return {
-    value: value,
-    label: getLabelByValue(value, options),
-  };
-}
-
-/**
- * Error message component for the SelectInput component.
- * @param isFetchError {boolean}
- * @param formError {string}
- * @constructor ErrorMessage
- */
-function ErrorMessage({
-  isFetchError,
-  formError,
-}: {
-  isFetchError?: boolean;
-  formError?: string;
-}) {
-  return (
-    <p className="text-xs text-red-500">
-      {isFetchError
-        ? "An error has occurred! Please try again later."
-        : formError}
-    </p>
-  );
-}
-
-/**
- * Props for the ContextMenu component.
- * @param key {string}
- * @param icon {React.ReactNode}
- * @param title {string}
- * @param onClick {() => void}
- * @constructor ContextMenuProps
- */
-type ContextMenuProps = {
-  key: string;
-  icon?: React.ReactNode;
-  title: string;
-  textClass?: string;
-  onSelect: () => void;
-};
-
-const ContextMenu = memo(
-  ({
-    menuItems,
-    onClose,
-  }: {
-    menuItems: ContextMenuProps[];
-    onClose: () => void;
-  }) => {
-    const handleSelect = useCallback(
-      (item: ContextMenuProps) => () => {
-        item.onSelect();
-        onClose();
-      },
-      [onClose],
-    );
-
-    return (
-      <Command className="rounded-lg border shadow-md max-h-[200px]">
-        <CommandList>
-          <CommandGroup heading="Actions">
-            {menuItems.map((item) => (
-              <CommandItem
-                key={item.key}
-                value={item.key}
-                onSelect={handleSelect(item)}
-                className="text-xs"
-              >
-                {item.icon}
-                <span className={item.textClass}>{item.title}</span>
-              </CommandItem>
-            ))}
-          </CommandGroup>
-        </CommandList>
-      </Command>
-    );
-  },
-);
+import {
+  ClearIndicator,
+  DropdownIndicator,
+  ErrorMessage,
+  IndicatorSeparator,
+  MenuList,
+  Option,
+  SelectDescription,
+  SelectOption,
+  ValueContainer,
+  ValueProcessor,
+} from "./select-components";
 
 /**
  * Props for the SelectInput component.
@@ -309,7 +48,6 @@ interface SelectInputProps<T extends Record<string, unknown>>
   description?: string;
   options: OptionsOrGroups<SelectOption, GroupBase<SelectOption>>;
   hasContextMenu?: boolean;
-  contextMenuItems?: ContextMenuProps[];
 }
 
 /**
@@ -321,8 +59,6 @@ export function SelectInput<T extends Record<string, unknown>>(
   props: SelectInputProps<T>,
 ) {
   const { field, fieldState } = useController(props);
-  const ref = useRef<HTMLDivElement>(null);
-  const contextMenuRef = useRef<HTMLDivElement>(null); // Ref for the context menu
 
   const {
     label,
@@ -337,55 +73,12 @@ export function SelectInput<T extends Record<string, unknown>>(
     menuPlacement = "auto",
     menuPosition = "absolute",
     hideSelectedOptions = false,
-    hasContextMenu = false,
-    contextMenuItems = [],
     ...controllerProps
   } = props;
 
   const dataLoading = props.isLoading || props.isDisabled;
   const errorOccurred = props.isFetchError || !!fieldState.error?.message;
   const processedValue = ValueProcessor(field.value, options, isMulti);
-
-  const [visible, setVisible] = useState(false);
-  const [contextMenuVisible, setContextMenuVisible] = useState(false);
-
-  const handleClick = () => {
-    setVisible(true);
-    setContextMenuVisible(false); // Close context menu when select menu is opened
-  };
-
-  const handleClickOutside = useCallback((event: MouseEvent) => {
-    if (
-      (!ref.current || !ref.current.contains(event.target as Node)) &&
-      (!contextMenuRef.current ||
-        !contextMenuRef.current.contains(event.target as Node))
-    ) {
-      setVisible(false); // Close the select dropdown
-      setContextMenuVisible(false); // Close the context menu
-    }
-  }, []);
-
-  const closeContextMenu = () => {
-    setContextMenuVisible(false);
-  };
-
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [handleClickOutside]);
-
-  const handleContextMenu = useCallback(
-    (e: React.MouseEvent<HTMLDivElement>) => {
-      if (hasContextMenu) {
-        e.preventDefault();
-        setContextMenuVisible(true);
-        setVisible(false); // Close select menu when context menu is opened
-      }
-    },
-    [hasContextMenu],
-  );
 
   return (
     <>
@@ -400,12 +93,7 @@ export function SelectInput<T extends Record<string, unknown>>(
           {label}
         </Label>
       )}
-      <div
-        className="relative"
-        ref={ref}
-        onClick={handleClick}
-        onContextMenu={handleContextMenu}
-      >
+      <div className="relative">
         <Select
           aria-invalid={fieldState.invalid || isFetchError}
           closeMenuOnSelect={!isMulti}
@@ -416,13 +104,10 @@ export function SelectInput<T extends Record<string, unknown>>(
           isLoading={isLoading}
           isDisabled={dataLoading || isFetchError}
           isClearable={isClearable}
-          onMenuOpen={() => setVisible(true)}
-          onMenuClose={() => setVisible(false)}
           maxOptions={maxOptions}
           placeholder={placeholder}
           isFetchError={isFetchError}
           formError={fieldState.error?.message}
-          menuIsOpen={visible}
           maxMenuHeight={200}
           menuPlacement={menuPlacement}
           menuPosition={menuPosition}
@@ -501,20 +186,6 @@ export function SelectInput<T extends Record<string, unknown>>(
           <SelectDescription description={description!} />
         )}
       </div>
-      {hasContextMenu && contextMenuVisible && (
-        <div
-          ref={contextMenuRef}
-          style={{
-            width: ref.current ? `${ref.current.offsetWidth}px` : "auto",
-          }}
-          className="absolute z-100 max-w-full h-fit pt-2 top-[330px]"
-        >
-          <ContextMenu
-            menuItems={contextMenuItems}
-            onClose={closeContextMenu}
-          />
-        </div>
-      )}
     </>
   );
 }
