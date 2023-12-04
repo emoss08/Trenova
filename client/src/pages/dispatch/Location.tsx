999dbe48744e6799cea860131a0145ce4b6c495e
Eric Moss <yeswolfprod@gmail.com>
2023-12-04 10:28:29 -0500

    change: location table component

--- client/src/pages/dispatch/Location.tsx
+++ client/src/pages/dispatch/Location.tsx
@@ -21,6 +21,12 @@ import { DataTableColumnExpand } from "@/components/common/table/data-table-expa
 import { LocationTableSheet } from "@/components/location/location-table-dialog";
 import { LocationTableEditSheet } from "@/components/location/location-table-edit-dialog";
 import { LocationChart } from "@/components/location/table-chart";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { tableStatusChoices } from "@/lib/constants";
 import { truncateText, upperFirst } from "@/lib/utils";
 import { Location } from "@/types/location";
@@ -34,18 +40,29 @@ const renderSubComponent = ({ row }: { row: Row<Location> }) => {
 function LocationColor({
   color,
   locationName,
+  locationCategoryName,
 }: {
   color: string;
   locationName: string;
+  locationCategoryName?: string;
 }) {
   return (
-    <div className="flex items-center space-x-2 text-sm font-mediumtext-gray-900 dark:text-gray-100">
-      <div
-        className={"h-5 w-5 rounded-xl mx-2"}
-        style={{ backgroundColor: color }}
-      />
-      {locationName}
-    </div>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <div className="flex items-center space-x-2 text-sm font-mediumtext-gray-900 dark:text-gray-100">
+            <div
+              className={"h-2 w-2 rounded-xl mx-2"}
+              style={{ backgroundColor: color }}
+            />
+            {locationName}
+          </div>
+        </TooltipTrigger>
+        <TooltipContent align="start">
+          {locationCategoryName && <p>{locationCategoryName}</p>}
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 }
 
@@ -81,12 +98,22 @@ const columns: ColumnDef<Location>[] = [
           <LocationColor
             color={row.original.locationColor}
             locationName={row.original.name}
+            locationCategoryName={row.original.locationCategoryName as string}
           />
         );
       } else {
         return row.original.name;
       }
     },
+    filterFn: (row, id, value) => {
+      return value.includes(row.getValue(id));
+    },
+  },
+  {
+    accessorKey: "locationCategoryName",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Location Category" />
+    ),
     filterFn: (row, id, value) => {
       return value.includes(row.getValue(id));
     },
@@ -99,6 +126,7 @@ const columns: ColumnDef<Location>[] = [
     cell: ({ row }) =>
       `${upperFirst(row.original.city)}, ${row.original.state}`,
   },
+
   {
     accessorKey: "pickupCount",
     header: "Total Pickups",
