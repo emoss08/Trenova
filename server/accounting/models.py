d5e3bc89de1e678998f620eccc3e924a4792a71b
Eric Moss <yeswolfprod@gmail.com>
2023-12-07 18:08:28 -0500

    fix: isort error

--- server/accounting/models.py
+++ server/accounting/models.py
@@ -28,6 +28,7 @@ from django.db.models.functions import Lower
 from django.urls import reverse
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+
 from utils.models import ChoiceField, GenericModel, PrimaryStatusChoices
 
 
