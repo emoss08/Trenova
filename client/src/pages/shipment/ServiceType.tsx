1c028492f71252680e698bef4d40ce5d4b9abf18
Eric Moss <yeswolfprod@gmail.com>
2023-12-10 00:50:17 -0500

    add: add permission name to table

--- client/src/pages/shipment/ServiceType.tsx
+++ client/src/pages/shipment/ServiceType.tsx
@@ -93,6 +93,7 @@ export default function ServiceTypes() {
       tableFacetedFilters={filters}
       TableSheet={ServiceTypeDialog}
       TableEditSheet={ServiceTypeEditDialog}
+      addPermissionName="add_servicetype"
     />
   );
 }
