062171a153dda70e1cadc807483078b88335097e
Eric Moss <yeswolfprod@gmail.com>
2023-12-13 20:13:12 -0500

    add: success color to tailwind config

--- client/src/components/ui/toast.tsx
+++ client/src/components/ui/toast.tsx
@@ -47,7 +47,7 @@ const toastVariants = cva(
         default: "border bg-background text-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
-        success: "group border-green-500 bg-green-500 text-white",
+        success: "group border-bg-success bg-success text-white",
       },
     },
     defaultVariants: {
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -65,6 +65,7 @@ module.exports = {
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+        success: "hsl(var(--success))",
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
