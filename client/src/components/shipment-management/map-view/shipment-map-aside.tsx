ccc4485ccbd78b6be008e5f4d72fc588c2b49474
Eric Moss <yeswolfprod@gmail.com>
2023-12-27 20:38:02 -0500

    wip: shipment worker aside

--- client/src/components/shipment-management/map-view/shipment-map-aside.tsx
+++ client/src/components/shipment-management/map-view/shipment-map-aside.tsx
@@ -15,111 +15,75 @@
  * Grant, and not modifying the license in any other way.
  */
 import { InputField } from "@/components/common/fields/input";
-import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { useForm } from "react-hook-form";
+import { ErrorLoadingData } from "@/components/common/table/data-table-components";
 import { WorkerSortOptions } from "@/components/shipment-management/map-view/shipment-map-filter";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useDebounce } from "@/hooks/useDebounce";
 import { useFleetCodes, useUsers } from "@/hooks/useQueries";
+import { DEBOUNCE_DELAY } from "@/lib/constants";
+import { getDispatchControl } from "@/services/OrganizationRequestService";
+import { getWorkers } from "@/services/WorkerRequestService";
+import { QueryKeys } from "@/types";
+import { DispatchControl } from "@/types/dispatch";
+import { Worker } from "@/types/worker";
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { WorkerList } from "./worker-list";
 
-const workersItems = [
-  {
-    id: 1,
-    fullName: "John Doe",
-    onDutyClock: "14:00",
-    driveTime: "11:00",
-    seventyHourClock: "70:00",
-    currentStatus: "Driving",
-  },
-  {
-    id: 2,
-    fullName: "Jane Doe",
-    onDutyClock: "14:00",
-    driveTime: "11:00",
-    seventyHourClock: "70:00",
-    currentStatus: "Off-Duty",
-  },
-  {
-    id: 3,
-    fullName: "John Smith",
-    onDutyClock: "14:00",
-    driveTime: "11:00",
-    seventyHourClock: "70:00",
-    currentStatus: "On-Duty (Not Driving)",
-  },
-  {
-    id: 4,
-    fullName: "Jane Smith",
-    onDutyClock: "14:00",
-    driveTime: "11:00",
-    seventyHourClock: "70:00",
-    currentStatus: "Driving",
-  },
-  {
-    id: 5,
-    fullName: "John Doe",
-    onDutyClock: "14:00",
-    driveTime: "11:00",
-    seventyHourClock: "70:00",
-    currentStatus: "Sleeper Berth",
-  },
-  {
-    id: 6,
-    fullName: "Jane Doe",
-    onDutyClock: "14:00",
-    driveTime: "11:00",
-    seventyHourClock: "70:00",
-    currentStatus: "Off-Duty",
-  },
-  {
-    id: 7,
-    fullName: "John Smith",
-    onDutyClock: "14:00",
-    driveTime: "11:00",
-    seventyHourClock: "70:00",
-    currentStatus: "Driving",
-  },
-  {
-    id: 8,
-    fullName: "Jane Smith",
-    onDutyClock: "14:00",
-    driveTime: "11:00",
-    seventyHourClock: "70:00",
-    currentStatus: "Driving",
-  },
-];
-
-const randomWorkerBackground = () => {
-  const colors = [
-    "bg-red-500",
-    "bg-green-500",
-    "bg-blue-500",
-    "bg-yellow-500",
-    "bg-indigo-500",
-    "bg-pink-500",
-    "bg-purple-500",
-  ];
-  return colors[Math.floor(Math.random() * colors.length)];
-};
-
-const currentStatusColor = (status: string) => {
-  switch (status) {
-    case "Driving":
-      return "text-red-500";
-    case "Off-Duty":
-      return "text-green-500";
-    case "On-Duty (Not Driving)":
-      return "text-blue-500";
-    case "Sleeper Berth":
-      return "text-yellow-500";
-    default:
-      return "text-muted-foreground";
-  }
+type WorkerSearchForm = {
+  searchQuery: string;
+  fleetFilter: string;
 };
 
 export function ShipmentMapAside() {
-  const { control } = useForm();
-  const { selectFleetCodes, isLoading: isFleetCodesLoading } = useFleetCodes();
-  const { selectUsersData, isLoading: isUsersLoading } = useUsers();
+  const queryClient = useQueryClient();
+  const { control, watch } = useForm<WorkerSearchForm>({
+    defaultValues: {
+      searchQuery: "",
+      fleetFilter: "",
+    },
+  });
+
+  const searchQuery = watch("searchQuery");
+
+  const debouncedSearchQuery = useDebounce(searchQuery, DEBOUNCE_DELAY);
+
+  const {
+    selectFleetCodes,
+    isLoading: isFleetCodesLoading,
+    isError: isFleetCodeError,
+  } = useFleetCodes();
+  const {
+    selectUsersData,
+    isLoading: isUsersLoading,
+    isError: isUserError,
+  } = useUsers();
+  const {
+    data: dispatchControlData,
+    isLoading: isDispatchControlDataLoading,
+    isError: isDispatchControlError,
+  } = useQuery({
+    queryKey: ["dispatchControl"] as QueryKeys[],
+    queryFn: async () => getDispatchControl(),
+    initialData: (): DispatchControl[] | undefined =>
+      queryClient.getQueryData(["dispatchControl"]),
+    staleTime: Infinity,
+  });
+  const {
+    data: workersData,
+    isLoading: isWorkersLoading,
+    isError: isWorkerError,
+  } = useQuery({
+    queryKey: ["workersList", debouncedSearchQuery],
+    queryFn: async () => getWorkers(100, debouncedSearchQuery),
+    initialData: (): Worker[] | undefined =>
+      queryClient.getQueryData(["workersList", debouncedSearchQuery]),
+    staleTime: Infinity,
+  });
+
+  // Store first element of dispatchControlData in variable
+  const dispatchControlDataArray = dispatchControlData?.[0];
 
   const sortOptions = [
     {
@@ -136,7 +100,6 @@ export function ShipmentMapAside() {
         },
       ],
     },
-
     {
       id: "fleetCode",
       title: "Fleet",
@@ -174,61 +137,49 @@ export function ShipmentMapAside() {
     },
   ];
 
+  const isLoading =
+    isFleetCodesLoading ||
+    isUsersLoading ||
+    isDispatchControlDataLoading ||
+    isWorkersLoading;
+
+  const isError =
+    isFleetCodeError || isUserError || isDispatchControlError || isWorkerError;
+
+  if (isError) {
+    return (
+      <aside className="items-center w-96 p-4 border ring-accent-foreground/20 rounded-md">
+        <div className="mt-52">
+          <ErrorLoadingData />
+        </div>
+      </aside>
+    );
+  }
+
   return (
     <aside className="w-96 p-4 border ring-accent-foreground/20 rounded-md">
-      {/* Fixed search field at the top */}
-      <InputField
-        name="searchQuery"
-        control={control}
-        placeholder="Search Workers..."
-        icon={<MagnifyingGlassIcon className="h-4 w-4 text-muted-foreground" />}
-      />
-
-      {/* Worker Sort Options */}
-      <WorkerSortOptions sortOptions={sortOptions} />
-
-      {/* Scrollable list of workers */}
-      <ScrollArea className="mt-2">
-        <ul className="space-y-3 p-3 h-[610px]">
-          {workersItems.map((item) => (
-            <li
-              key={item.id}
-              className="group relative flex items-center space-x-3 rounded-lg border ring-accent-foreground/20 bg-background px-6 py-3 shadow-sm hover:bg-foreground"
-            >
-              <div className="flex-shrink-0">
-                <div
-                  className={`h-10 w-10 rounded-full ${randomWorkerBackground()} flex items-center justify-center text-white font-bold`}
-                >
-                  {item.fullName[0]}
-                </div>
-              </div>
-              <div className="min-w-0 flex-1">
-                <a href="#" className="focus:outline-none">
-                  <span className="absolute inset-0" aria-hidden="true" />
-                  <p className="text-sm font-bold text-foreground group-hover:text-background">
-                    {item.fullName}
-                  </p>
-                  <p
-                    className={`text-xs ${currentStatusColor(
-                      item.currentStatus,
-                    )} group-hover:text-background truncate`}
-                  >
-                    Current Status: {item.currentStatus}
-                  </p>
-                  <div className="flex">
-                    <p className="text-xs text-muted-foreground group-hover:text-background truncate">
-                      On Duty Clock: {item.onDutyClock}
-                    </p>
-                    <p className="text-xs text-muted-foreground group-hover:text-background truncate ml-3">
-                      Drive Time: {item.driveTime}
-                    </p>
-                  </div>
-                </a>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </ScrollArea>
+      {isLoading ? (
+        <Skeleton className="h-full" />
+      ) : (
+        <>
+          {/* Fixed search field at the top */}
+          <InputField
+            name="searchQuery"
+            control={control}
+            placeholder="Search Workers..."
+            icon={
+              <MagnifyingGlassIcon className="h-4 w-4 text-muted-foreground" />
+            }
+          />
+          {/* Worker Sort Options */}
+          <WorkerSortOptions sortOptions={sortOptions} />
+          {/* Worker List */}
+          <WorkerList
+            dispatchControlData={dispatchControlDataArray as DispatchControl}
+            workersData={workersData as Worker[]}
+          />
+        </>
+      )}
     </aside>
   );
 }
--- client/src/components/shipment-management/map-view/shipment-map-view.tsx
+++ client/src/components/shipment-management/map-view/shipment-map-view.tsx
@@ -14,17 +14,74 @@
  * Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use
  * Grant, and not modifying the license in any other way.
  */
-import { GOOGLE_API_KEY } from "@/lib/constants";
-import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
-import GoogleMapReact from "google-map-react";
-import { useForm } from "react-hook-form";
 import { InputField } from "@/components/common/fields/input";
-import { ShipmentMapOptions } from "@/components/shipment-management/map-view/shipment-map-options";
-import { useShipmentMapStore } from "@/stores/ShipmentStore";
+import { SendMessageDialog } from "@/components/common/send-message-dialog";
 import { ShipmentMapAside } from "@/components/shipment-management/map-view/shipment-map-aside";
+import { ShipmentMapOptions } from "@/components/shipment-management/map-view/shipment-map-options";
 import { ShipmentMapZoom } from "@/components/shipment-management/map-view/shipment-map-zoom";
-import { useCallback } from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { GOOGLE_API_KEY } from "@/lib/constants";
+import { useShipmentMapStore, useShipmentStore } from "@/stores/ShipmentStore";
 import { GoogleMap } from "@google";
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import { TooltipProvider } from "@radix-ui/react-tooltip";
+import GoogleMapReact from "google-map-react";
+import { useCallback } from "react";
+import { useForm } from "react-hook-form";
+
+const markers = [
+  { id: 1, lat: 37.78, lng: -122.41, label: "San Francisco" },
+  { id: 2, lat: 34.05, lng: -118.24, label: "Los Angeles" },
+  { id: 3, lat: 39.28, lng: -76.61, label: "Baltimore" },
+  { id: 4, lat: 41.87, lng: -87.62, label: "Chicago" },
+  { id: 5, lat: 40.71, lng: -74.01, label: "New York" },
+  { id: 6, lat: 29.76, lng: -95.36, label: "Houston" },
+  { id: 7, lat: 33.75, lng: -84.39, label: "Atlanta" },
+  { id: 8, lat: 32.78, lng: -96.8, label: "Dallas" },
+  { id: 9, lat: 39.95, lng: -75.16, label: "Philadelphia" },
+  { id: 10, lat: 32.71, lng: -117.16, label: "San Diego" },
+  { id: 11, lat: 32.79, lng: -79.94, label: "Charleston" },
+  { id: 12, lat: 39.29, lng: -76.61, label: "Baltimore" },
+  { id: 13, lat: 39.95, lng: -75.16, label: "Philadelphia" },
+  { id: 14, lat: 37.78, lng: -122.41, label: "San Francisco" },
+  { id: 15, lat: 34.05, lng: -118.24, label: "Los Angeles" },
+  { id: 16, lat: 39.28, lng: -76.61, label: "Baltimore" },
+  { id: 17, lat: 41.87, lng: -87.62, label: "Chicago" },
+  { id: 18, lat: 40.71, lng: -74.01, label: "New York" },
+  { id: 19, lat: 29.76, lng: -95.36, label: "Houston" },
+  { id: 20, lat: 33.75, lng: -84.39, label: "Atlanta" },
+  { id: 21, lat: 32.78, lng: -96.8, label: "Dallas" },
+  { id: 22, lat: 39.95, lng: -75.16, label: "Philadelphia" },
+  { id: 23, lat: 32.71, lng: -117.16, label: "San Diego" },
+  { id: 24, lat: 32.79, lng: -79.94, label: "Charleston" },
+  { id: 25, lat: 39.29, lng: -76.61, label: "Baltimore" },
+  { id: 26, lat: 39.95, lng: -75.16, label: "Philadelphia" },
+  { id: 27, lat: 37.78, lng: -122.41, label: "San Francisco" },
+  { id: 28, lat: 34.05, lng: -118.24, label: "Los Angeles" },
+];
+
+interface MapMarkerProps {
+  lat: number;
+  lng: number;
+  text: string;
+}
+
+function MapMarker({ text }: MapMarkerProps) {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <div className="flex items-center justify-center w-4 h-4 bg-black rounded-full cursor-auto border-2 border-white" />
+        </TooltipTrigger>
+        <TooltipContent>{text}</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
 
 export function ShipmentMapView() {
   const defaultProps = {
@@ -40,6 +97,8 @@ export function ShipmentMapView() {
   const [mapLayers] = useShipmentMapStore.use("mapLayers");
   const [map, setMap] = useShipmentMapStore.use("map");
   const [, setMaps] = useShipmentMapStore.use("maps");
+  const [sendMessageDialogOpen, setSendMessageDialogOpen] =
+    useShipmentStore.use("sendMessageDialogOpen");
 
   const handleApiLoaded = useCallback(
     ({ map, maps }: { map: GoogleMap; maps: GoogleMap }) => {
@@ -50,44 +109,61 @@ export function ShipmentMapView() {
   );
 
   return (
-    <div className="flex h-[700px] w-screen mx-auto space-x-10">
-      <ShipmentMapAside />
-      <div className="flex-grow relative">
-        {/* Absolute positioned map options */}
-        <div className="absolute top-0 right-0 z-10 p-2">
-          <ShipmentMapOptions />
+    <>
+      <div className="flex h-[700px] w-screen mx-auto space-x-10">
+        <ShipmentMapAside />
+        <div className="flex-grow relative">
+          {/* Absolute positioned map options */}
+          <div className="absolute top-0 right-0 z-10 p-2">
+            <ShipmentMapOptions />
+          </div>
+          {/* Absolute positioned search field */}
+          <div className="absolute top-0 left-0 z-10 p-2">
+            <InputField
+              name="searchMapQuery"
+              control={control}
+              placeholder="Search Shipments..."
+              className="shadow-md pl-10"
+              icon={
+                <MagnifyingGlassIcon className="h-4 w-4 text-muted-foreground" />
+              }
+            />
+          </div>
+          {/* Absolute positioned zoom controls */}
+          <div className="absolute bottom-0 right-0 z-10 p-2 mb-4">
+            <ShipmentMapZoom map={map} />
+          </div>
+          {/* Google Map */}
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: GOOGLE_API_KEY as string }}
+            defaultCenter={defaultProps.center}
+            defaultZoom={defaultProps.zoom}
+            style={{ width: "100%", height: "100%" }}
+            layerTypes={mapLayers}
+            yesIWantToUseGoogleMapApiInternals
+            onGoogleApiLoaded={handleApiLoaded}
+            options={{
+              mapTypeId: mapType,
+              disableDefaultUI: true,
+            }}
+          >
+            {markers.map((marker) => (
+              <MapMarker
+                key={marker.id}
+                lat={marker.lat}
+                lng={marker.lng}
+                text={marker.label}
+              />
+            ))}
+          </GoogleMapReact>
         </div>
-        {/* Absolute positioned search field */}
-        <div className="absolute top-0 left-0 z-10 p-2">
-          <InputField
-            name="searchMapQuery"
-            control={control}
-            placeholder="Search Orders..."
-            className="shadow-md pl-10"
-            icon={
-              <MagnifyingGlassIcon className="h-4 w-4 text-muted-foreground" />
-            }
-          />
-        </div>
-        {/* Absolute positioned zoom controls */}
-        <div className="absolute bottom-0 right-0 z-10 p-2 mb-4">
-          <ShipmentMapZoom map={map} />
-        </div>
-        {/* Google Map */}
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: GOOGLE_API_KEY as string }}
-          defaultCenter={defaultProps.center}
-          defaultZoom={defaultProps.zoom}
-          style={{ width: "100%", height: "100%" }} // Ensure the map fills the container
-          layerTypes={mapLayers}
-          yesIWantToUseGoogleMapApiInternals
-          onGoogleApiLoaded={handleApiLoaded}
-          options={{
-            mapTypeId: mapType,
-            disableDefaultUI: true,
-          }}
-        />
       </div>
-    </div>
+      {sendMessageDialogOpen && (
+        <SendMessageDialog
+          open={sendMessageDialogOpen}
+          onOpenChange={() => setSendMessageDialogOpen(false)}
+        />
+      )}
+    </>
   );
 }
