623389ff9e3e48cd29565c989db2497239794e36
Eric Moss <yeswolfprod@gmail.com>
2024-01-01 00:21:35 -0500

    remove waffle middleware

--- server/backend/settings.py
+++ server/backend/settings.py
@@ -99,7 +99,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "auditlog.middleware.AuditlogMiddleware",
-    "waffle.middleware.WaffleMiddleware",
     # "core.middleware.logging_middleware.RocketLoggingMiddleware",
 ]
 
