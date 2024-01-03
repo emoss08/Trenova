ab5d5fddcd009f2e57c8242bf9ff411e56b296a7
Eric Moss <yeswolfprod@gmail.com>
2024-01-02 22:35:10 -0500

    change: add remaining endpoints to dispatch schema

--- server/dispatch/schema.py
+++ server/dispatch/schema.py
@@ -17,16 +17,56 @@
 
 import typing
 
+from dispatch import models
+from django.core.exceptions import PermissionDenied
 from django_filters import FilterSet
-from graphene import ObjectType, Schema, relay
+from graphene import Field, ObjectType, Schema, relay
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
-
-from dispatch import models
 
 if typing.TYPE_CHECKING:
     from django.db.models import QuerySet
     from graphql import GraphQLResolveInfo
+
+
+class DispatchControlNode(DjangoObjectType):
+    """Dispatch Control Node for GraphQL.
+
+    Notes:
+        - This is only available to users with the `dispatch.view_dispatchcontrol` permission.
+        - Available by default to anyone that is an admin.
+    """
+
+    class Meta:
+        model = models.DispatchControl
+        interfaces = (relay.Node,)
+        fields = "__all__"
+
+    @staticmethod
+    def has_read_permission(*, info: "GraphQLResolveInfo") -> bool:
+        user = info.context.user
+        return user.is_superuser or user.has_perm("dispatch.view_dispatchcontrol")
+
+
+class FeasibilityToolControlNode(DjangoObjectType):
+    """Dispatch Control Node for GraphQL.
+
+    Notes:
+        - This is only available to users with the `dispatch.view_feasibilitytoolcontrol` permission.
+        - Available by default to anyone that is an admin.
+    """
+
+    class Meta:
+        model = models.FeasibilityToolControl
+        interfaces = (relay.Node,)
+        fields = "__all__"
+
+    @staticmethod
+    def has_read_permission(*, info: "GraphQLResolveInfo") -> bool:
+        user = info.context.user
+        return user.is_superuser or user.has_perm(
+            "dispatch.view_feasibilitytoolcontrol"
+        )
 
 
 class DelayCodeFilterSet(FilterSet):
@@ -61,13 +101,109 @@ class DelayCodeNode(DjangoObjectType):
         return queryset.filter(organization_id=info.context.user.organization_id)
 
 
+class CommentTypeFilterSet(FilterSet):
+    """
+    FilterSet for Comment Type
+    """
+
+    class Meta:
+        model = models.CommentType
+        fields = ["status"]
+
+
+class CommentTypeNode(DjangoObjectType):
+    """
+    DjangoObjectType for CommentType
+    """
+
+    class Meta:
+        """
+        Meta class defining the model and filter set for CommentTypeNode
+        """
+
+        model = models.CommentType
+        filterset_class = CommentTypeFilterSet
+        interfaces = (relay.Node,)
+        fields = "__all__"
+
+    @classmethod
+    def get_queryset(
+        cls, queryset: "QuerySet[models.CommentType]", info: "GraphQLResolveInfo"
+    ) -> "QuerySet[models.CommentType]":
+        return queryset.filter(organization_id=info.context.user.organization_id)
+
+
+class FleetCodeFilterSet(FilterSet):
+    """
+    FilterSet for Comment Type
+    """
+
+    class Meta:
+        model = models.FleetCode
+        fields = ["status"]
+
+
+class FleetCodeNode(DjangoObjectType):
+    """
+    DjangoObjectType for FleetCode
+    """
+
+    class Meta:
+        """
+        Meta class defining the model and filter set for FleetCodeNode
+        """
+
+        model = models.FleetCode
+        filterset_class = FleetCodeFilterSet
+        interfaces = (relay.Node,)
+        fields = "__all__"
+
+    @classmethod
+    def get_queryset(
+        cls, queryset: "QuerySet[models.FleetCode]", info: "GraphQLResolveInfo"
+    ) -> "QuerySet[models.FleetCode]":
+        return queryset.filter(
+            organization_id=info.context.user.organization_id
+        ).select_related("manager")
+
+
 class Query(ObjectType):
     """
     The Query class defines the GraphQL queries that can be made to the server
     """
 
+    dispatch_control = Field(DispatchControlNode)
+    feasibility_tool_control = Field(FeasibilityToolControlNode)
     delay_code = relay.Node.Field(DelayCodeNode)
     delay_codes = DjangoFilterConnectionField(DelayCodeNode)
+    comment_type = relay.Node.Field(CommentTypeNode)
+    comment_types = DjangoFilterConnectionField(CommentTypeNode)
+    fleet_code = relay.Node.Field(FleetCodeNode)
+    fleet_codes = DjangoFilterConnectionField(FleetCodeNode)
+
+    def resolve_dispatch_control(
+        self, info: "GraphQLResolveInfo", **kwargs: typing.Any
+    ) -> models.DispatchControl:
+        if not DispatchControlNode.has_read_permission(info=info):
+            raise PermissionDenied(
+                "You do not have permission to view dispatch control"
+            )
+
+        return models.DispatchControl.objects.get(
+            organization_id=info.context.user.organization_id
+        )
+
+    def resolve_feasibility_tool_control(
+        self, info: "GraphQLResolveInfo", **kwargs: typing.Any
+    ) -> models.FeasibilityToolControl:
+        if not FeasibilityToolControlNode.has_read_permission(info=info):
+            raise PermissionDenied(
+                "You do not have permission to view feasibility tool control"
+            )
+
+        return models.FeasibilityToolControl.objects.get(
+            organization_id=info.context.user.organization_id
+        )
 
 
 schema = Schema(query=Query)
