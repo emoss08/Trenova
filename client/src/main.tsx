826c7bac37cfbd6255dbcbf43c0d213d47293879
Eric Moss <yeswolfprod@gmail.com>
2024-01-10 22:42:26 -0500

    remove: strict mode
    unfortunately, this has to go. It was breaking stuff that I can't fix because they are third party packages like the markers in the shipment map view.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -24,32 +24,30 @@ import { Button } from "./components/ui/button";
 import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <QueryErrorResetBoundary>
-      {({ reset }) => (
-        <ErrorBoundary
-          onReset={reset}
-          fallbackRender={({ resetErrorBoundary }) => (
-            <div className="flex h-screen items-center justify-center bg-gray-100">
-              <div className="text-center">
-                <ExclamationTriangleIcon className="mx-auto h-12 w-12 text-red-500" />
-                <h1 className="mt-4 text-2xl font-bold text-gray-800">
-                  There was an error!
-                </h1>
-                <p className="text-gray-600">Please try again.</p>
-                <Button
-                  onClick={resetErrorBoundary}
-                  className="mt-6 rounded-md px-4 py-2 shadow"
-                >
-                  Try again
-                </Button>
-              </div>
+  <QueryErrorResetBoundary>
+    {({ reset }) => (
+      <ErrorBoundary
+        onReset={reset}
+        fallbackRender={({ resetErrorBoundary }) => (
+          <div className="flex h-screen items-center justify-center bg-gray-100">
+            <div className="text-center">
+              <ExclamationTriangleIcon className="mx-auto h-12 w-12 text-red-500" />
+              <h1 className="mt-4 text-2xl font-bold text-gray-800">
+                There was an error!
+              </h1>
+              <p className="text-gray-600">Please try again.</p>
+              <Button
+                onClick={resetErrorBoundary}
+                className="mt-6 rounded-md px-4 py-2 shadow"
+              >
+                Try again
+              </Button>
             </div>
-          )}
-        >
-          <App />
-        </ErrorBoundary>
-      )}
-    </QueryErrorResetBoundary>
-  </React.StrictMode>,
+          </div>
+        )}
+      >
+        <App />
+      </ErrorBoundary>
+    )}
+  </QueryErrorResetBoundary>,
 );
