8350af6bff0b1b2f357e190acb4d656b8987134e
Eric Moss <yeswolfprod@gmail.com>
2024-01-14 00:45:47 -0500

    replace react vite plugin

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -15,8 +15,8 @@
  * Grant, and not modifying the license in any other way.
  */
 
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
