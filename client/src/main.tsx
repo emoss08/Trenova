90ea26efe2faaa7077e225bb472b3c6af609c785
Eric Moss <yeswolfprod@gmail.com>
2024-01-19 21:19:31 -0500

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -21,7 +21,6 @@ import ReactDOM from "react-dom/client";
 import { ErrorBoundary } from "react-error-boundary";
 import App from "./App";
 import { Button } from "./components/ui/button";
-import React from "react";
 import "./i18n";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
