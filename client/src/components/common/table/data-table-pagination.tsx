a91cd5cf18aa97160ccd89438b3cf2e22e2b24d3
Eric Moss <yeswolfprod@gmail.com>
2024-01-25 19:24:19 -0500

    change: data table pagination to no longer show
    if there are no pages or records in the table do not show the pagination

--- client/src/components/common/table/data-table-pagination.tsx
+++ client/src/components/common/table/data-table-pagination.tsx
@@ -47,7 +47,9 @@ export function DataTablePagination<TData>({
   );
   const totalCount = table.getPageCount() * pagination.pageSize;
 
-  return (
+  console.info("Page count", table.getPageCount());
+
+  return table.getPageCount() > 0 ? (
     <div className="flex items-center justify-between px-2">
       <div className="hidden sm:block">
         <p className="text-sm text-muted-foreground">
@@ -84,42 +86,42 @@ export function DataTablePagination<TData>({
         <div className="flex items-center space-x-2">
           <Button
             variant="outline"
-            className="hidden h-8 w-8 p-0 lg:flex"
+            className="hidden size-8 p-0 lg:flex"
             onClick={() => table.setPageIndex(0)}
             disabled={!table.getCanPreviousPage()}
           >
             <span className="sr-only">Go to first page</span>
-            <ChevronsLeft className="h-4 w-4" />
+            <ChevronsLeft className="size-4" />
           </Button>
           <Button
             variant="outline"
-            className="h-8 w-8 p-0"
+            className="size-8 p-0"
             onClick={() => table.previousPage()}
             disabled={!table.getCanPreviousPage()}
           >
             <span className="sr-only">Go to previous page</span>
-            <ChevronLeftIcon className="h-4 w-4" />
+            <ChevronLeftIcon className="size-4" />
           </Button>
           <Button
             variant="outline"
-            className="h-8 w-8 p-0"
+            className="size-8 p-0"
             onClick={() => table.nextPage()}
             disabled={!table.getCanNextPage()}
           >
             <span className="sr-only">Go to next page</span>
-            <ChevronRightIcon className="h-4 w-4" />
+            <ChevronRightIcon className="size-4" />
           </Button>
           <Button
             variant="outline"
-            className="hidden h-8 w-8 p-0 lg:flex"
+            className="hidden size-8 p-0 lg:flex"
             onClick={() => table.setPageIndex(table.getPageCount() - 1)} // Directly set to the last page
             disabled={!table.getCanNextPage()}
           >
             <span className="sr-only">Go to last page</span>
-            <ChevronsRight className="h-4 w-4" />
+            <ChevronsRight className="size-4" />
           </Button>
         </div>
       </div>
     </div>
-  );
+  ) : null;
 }
