798f95f6eec17cb4131a6b2d05ecdb02b83390ab
Eric Moss <yeswolfprod@gmail.com>
2024-01-25 19:30:59 -0500

    change: sheet header style

--- client/src/components/ui/sheet.tsx
+++ client/src/components/ui/sheet.tsx
@@ -83,7 +83,7 @@ const SheetContent = React.forwardRef<
     >
       {children}
       <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
-        <X className="h-4 w-4" />
+        <X className="size-4" />
         <span className="sr-only">Close</span>
       </SheetPrimitive.Close>
     </SheetPrimitive.Content>
@@ -125,7 +125,10 @@ const SheetTitle = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SheetPrimitive.Title
     ref={ref}
-    className={cn("text-lg font-semibold text-foreground", className)}
+    className={cn(
+      "text-lg font-semibold text-foreground underline decoration-orange-600",
+      className,
+    )}
     {...props}
   />
 ));
