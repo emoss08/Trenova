54f34d4fe1ed7448364866e6eddb888395428568
Eric Moss <yeswolfprod@gmail.com>
2024-02-10 18:22:24 -0500

    Update input.tsx

--- client/src/components/common/fields/input.tsx
+++ client/src/components/common/fields/input.tsx
@@ -105,7 +105,7 @@ export function InputField<T extends FieldValues>({
         />
         {fieldState.invalid && (
           <>
-            <div className="pointer-events-none absolute inset-y-0 right-0 mr-3">
+            <div className="pointer-events-none absolute inset-y-0 right-0 mr-2.5 mt-2.5">
               <AlertTriangle size={15} className="text-red-500" />
             </div>
             <ErrorMessage formError={fieldState.error?.message} />
