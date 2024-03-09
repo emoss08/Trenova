152325602a909b71cb9f96c1207c756f4781fb1c
Eric Moss <yeswolfprod@gmail.com>
2024-03-09 16:10:41 -0500

    bump

--- server/organization/templatetags/site_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from django.conf import settings
-
-register = template.Library()
-
-
-@register.simple_tag
-def base_url():
-    return getattr(settings, "BASE_URL", "http://localhost:8000/")
