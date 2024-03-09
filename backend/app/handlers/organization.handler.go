56a6161d31d33cfc8c29aa24d8c298eb47cdda6b
Eric Moss <yeswolfprod@gmail.com>
2024-03-09 17:19:45 -0500

    change: handler to return proper response.

--- backend/app/handlers/organization.handler.go
+++ backend/app/handlers/organization.handler.go
@@ -9,26 +9,29 @@ import (
 	"gorm.io/gorm"
 )
 
+// Returns the organization of the user.
 func GetOrganization(db *gorm.DB, store *gormstore.Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		orgId, ok := utils.GetUserOrgFromSession(r, store)
 
 		if !ok {
-			utils.ResponseWithError(w, http.StatusUnauthorized, utils.ValidationErrorDetail{
-				Code:   "unauthorized",
-				Detail: "Unauthorized",
-				Attr:   "organizationId",
+			utils.ResponseWithError(w, http.StatusUnauthorized, utils.ValidationErrorResponse{
+				Type: "unauthorized",
+				Errors: []utils.ValidationErrorDetail{
+					{
+						Code:   "invalid",
+						Detail: "You are not authorized to access this resource.",
+						Attr:   "all",
+					},
+				},
 			})
 			return
 		}
 
 		var org models.Organization
 		if err := db.Model(&models.Organization{}).Where("id = ?", orgId).First(&org).Error; err != nil {
-			utils.ResponseWithError(w, http.StatusInternalServerError, utils.ValidationErrorDetail{
-				Code:   "notFound",
-				Detail: err.Error(),
-				Attr:   "id",
-			})
+			errorResponse := utils.FormatDatabaseError(err)
+			utils.ResponseWithError(w, http.StatusInternalServerError, errorResponse)
 			return
 		}
 
