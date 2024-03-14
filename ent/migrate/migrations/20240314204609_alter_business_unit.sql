6efe6174f782933728abc3ed802fc38561a5de11
Eric Moss <yeswolfprod@gmail.com>
2024-03-14 17:00:11 -0400

    enfore atlas sum change

--- ent/migrate/migrations/20240314204609_alter_business_unit.sql
+++ ent/migrate/migrations/20240314204609_alter_business_unit.sql
@@ -1,2 +1,3 @@
 -- Modify "billing_controls" table
-ALTER TABLE "billing_controls" ADD COLUMN "enforce_customer_billing" boolean NOT NULL DEFAULT false;
+ALTER TABLE "billing_controls"
+ADD COLUMN "enforce_customer_billing" boolean NOT NULL DEFAULT false;
