32ca220c1319fe60ec917584fc636b874864abbb
Eric Moss <yeswolfprod@gmail.com>
2024-04-06 01:58:45 -0400

    remove: copyright notice

--- client/src/components/location/location-table-dialog.tsx
+++ client/src/components/location/location-table-dialog.tsx
@@ -1,20 +1,3 @@
-/*
- * COPYRIGHT(c) 2024 Trenova
- *
- * This file is part of Trenova.
- *
- * The Trenova software is licensed under the Business Source License 1.1. You are granted the right
- * to copy, modify, and redistribute the software, but only for non-production use or with a total
- * of less than three server instances. Starting from the Change Date (November 16, 2026), the
- * software will be made available under version 2 or later of the GNU General Public License.
- * If you use the software in violation of this license, your rights under the license will be
- * terminated automatically. The software is provided "as is," and the Licensor disclaims all
- * warranties and conditions. If you use this license's text or the "Business Source License" name
- * and trademark, you must comply with the Licensor's covenants, which include specifying the
- * Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use
- * Grant, and not modifying the license in any other way.
- */
-
 import { LocationContactForm } from "@/components/location/location-contacts-form";
 import { LocationInfoForm } from "@/components/location/location-info-form";
 import { Button } from "@/components/ui/button";
@@ -40,7 +23,7 @@ import { LocationCommentForm } from "./location-comments-form";
 export function LocationTableSheet({ onOpenChange, open }: TableSheetProps) {
   const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
 
-  const { control, reset, handleSubmit, formState } = useForm<FormValues>({
+  const { control, reset, handleSubmit } = useForm<FormValues>({
     resolver: yupResolver(LocationSchema),
     defaultValues: {
       status: "A",
@@ -55,8 +38,6 @@ export function LocationTableSheet({ onOpenChange, open }: TableSheetProps) {
       description: "",
     },
   });
-
-  console.log("Erros", formState.errors);
 
   const mutation = useCustomMutation<FormValues>(
     control,
