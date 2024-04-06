24d5b80e89af2fba9239bcc228034eb4b2be0c2d
Eric Moss <yeswolfprod@gmail.com>
2024-04-06 01:58:45 -0400

    remove: copyright notice

--- client/src/pages/dispatch/Locations.tsx
+++ client/src/pages/dispatch/Locations.tsx
@@ -1,53 +1,34 @@
-/*
- * COPYRIGHT(c) 2024 Trenova
- *
- * This file is part of Trenova.
- *
- * The Trenova software is licensed under the Business Source License 1.1. You are granted the right
- * to copy, modify, and redistribute the software, but only for non-production use or with a total
- * of less than three server instances. Starting from the Change Date (November 16, 2026), the
- * software will be made available under version 2 or later of the GNU General Public License.
- * If you use the software in violation of this license, your rights under the license will be
- * terminated automatically. The software is provided "as is," and the Licensor disclaims all
- * warranties and conditions. If you use this license's text or the "Business Source License" name
- * and trademark, you must comply with the Licensor's covenants, which include specifying the
- * Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use
- * Grant, and not modifying the license in any other way.
- */
-
+import { Checkbox } from "@/components/common/fields/checkbox";
 import { DataTable } from "@/components/common/table/data-table";
 import { DataTableColumnHeader } from "@/components/common/table/data-table-column-header";
 import { StatusBadge } from "@/components/common/table/data-table-components";
-import { DataTableColumnExpand } from "@/components/common/table/data-table-expand";
 import { LocationTableSheet } from "@/components/location/location-table-dialog";
 import { LocationTableEditSheet } from "@/components/location/location-table-edit-dialog";
 import { tableStatusChoices } from "@/lib/choices";
 import { upperFirst } from "@/lib/utils";
 import { type Location } from "@/types/location";
 import { type FilterConfig } from "@/types/tables";
-import type { ColumnDef, Row } from "@tanstack/react-table";
-import { Suspense, lazy } from "react";
-
-const LocationChart = lazy(
-  () => import("../../components/location/chart/table-chart"),
-);
-
-const renderSubComponent = ({ row }: { row: Row<Location> }) => {
-  return (
-    <Suspense fallback={null}>
-      <LocationChart row={row} />
-    </Suspense>
-  );
-};
+import type { ColumnDef } from "@tanstack/react-table";
 
 const columns: ColumnDef<Location>[] = [
   {
-    id: "expander",
-    footer: (props) => props.column.id,
-    header: () => null,
-    cell: ({ row }) => {
-      return <DataTableColumnExpand row={row} />;
-    },
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={table.getIsAllPageRowsSelected()}
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+        className="translate-y-[2px]"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+        className="translate-y-[2px]"
+      />
+    ),
     enableSorting: false,
     enableHiding: false,
   },
@@ -131,7 +112,6 @@ export default function Locations() {
       tableFacetedFilters={filters}
       TableSheet={LocationTableSheet}
       TableEditSheet={LocationTableEditSheet}
-      renderSubComponent={renderSubComponent}
       getRowCanExpand={() => true}
       addPermissionName="add_location"
     />
