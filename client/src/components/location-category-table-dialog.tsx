2e1f9ba7ca8cb9b6a1f2428b52ac1aa9382093d3
Eric Moss <yeswolfprod@gmail.com>
2024-04-07 02:02:12 -0400

    change: dialog to credenza

--- client/src/components/location-category-table-dialog.tsx
+++ client/src/components/location-category-table-dialog.tsx
@@ -1,32 +1,7 @@
-/*
- * COPYRIGHT(c) 2024 Trenova
- *
- * This file is part of Trenova.
- *
- * The Trenova software is licensed under the Business Source License 1.1. You are granted the right
- * to copy, modify, and redistribute the software, but only for non-production use or with a total
- * of less than three server instances. Starting from the Change Date (November 16, 2026), the
- * software will be made available under version 2 or later of the GNU General Public License.
- * If you use the software in violation of this license, your rights under the license will be
- * terminated automatically. The software is provided "as is," and the Licensor disclaims all
- * warranties and conditions. If you use this license's text or the "Business Source License" name
- * and trademark, you must comply with the Licensor's covenants, which include specifying the
- * Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use
- * Grant, and not modifying the license in any other way.
- */
-
 import { GradientPicker } from "@/components/common/fields/color-field";
 import { InputField } from "@/components/common/fields/input";
 import { TextareaField } from "@/components/common/fields/textarea";
 import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-} from "@/components/ui/dialog";
 import { useCustomMutation } from "@/hooks/useCustomMutation";
 import { LocationCategorySchema as formSchema } from "@/lib/validations/LocationSchema";
 import { type LocationCategoryFormValues as FormValues } from "@/types/location";
@@ -34,6 +9,16 @@ import { type TableSheetProps } from "@/types/tables";
 import { yupResolver } from "@hookform/resolvers/yup";
 import React from "react";
 import { Control, useForm } from "react-hook-form";
+import {
+  Credenza,
+  CredenzaBody,
+  CredenzaClose,
+  CredenzaContent,
+  CredenzaDescription,
+  CredenzaFooter,
+  CredenzaHeader,
+  CredenzaTitle,
+} from "./ui/credenza";
 
 export function LCForm({ control }: { control: Control<FormValues> }) {
   return (
@@ -74,7 +59,7 @@ export function LCForm({ control }: { control: Control<FormValues> }) {
   );
 }
 
-export function LocationCategoryTableSheet({
+export function LocationCategoryDialog({
   onOpenChange,
   open,
 }: TableSheetProps) {
@@ -109,23 +94,30 @@ export function LocationCategoryTableSheet({
   };
 
   return (
-    <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Create New Location Category</DialogTitle>
-        </DialogHeader>
-        <DialogDescription>
+    <Credenza open={open} onOpenChange={onOpenChange}>
+      <CredenzaContent>
+        <CredenzaHeader>
+          <CredenzaTitle>Create New Location Category</CredenzaTitle>
+        </CredenzaHeader>
+        <CredenzaDescription>
           Please fill out the form below to create a new Location Category.
-        </DialogDescription>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <LCForm control={control} />
-          <DialogFooter className="mt-6">
-            <Button type="submit" isLoading={isSubmitting}>
-              Save
-            </Button>
-          </DialogFooter>
-        </form>
-      </DialogContent>
-    </Dialog>
+        </CredenzaDescription>
+        <CredenzaBody>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <LCForm control={control} />
+            <CredenzaFooter>
+              <CredenzaClose asChild>
+                <Button variant="outline" type="button">
+                  Cancel
+                </Button>
+              </CredenzaClose>
+              <Button type="submit" isLoading={isSubmitting}>
+                Save Changes
+              </Button>
+            </CredenzaFooter>
+          </form>
+        </CredenzaBody>
+      </CredenzaContent>
+    </Credenza>
   );
 }
--- client/src/pages/accounting/DivisionCodes.tsx
+++ client/src/pages/accounting/DivisionCodes.tsx
@@ -1,26 +1,9 @@
-/*
- * COPYRIGHT(c) 2024 Trenova
- *
- * This file is part of Trenova.
- *
- * The Trenova software is licensed under the Business Source License 1.1. You are granted the right
- * to copy, modify, and redistribute the software, but only for non-production use or with a total
- * of less than three server instances. Starting from the Change Date (November 16, 2026), the
- * software will be made available under version 2 or later of the GNU General Public License.
- * If you use the software in violation of this license, your rights under the license will be
- * terminated automatically. The software is provided "as is," and the Licensor disclaims all
- * warranties and conditions. If you use this license's text or the "Business Source License" name
- * and trademark, you must comply with the Licensor's covenants, which include specifying the
- * Change License as the GPL Version 2.0 or a compatible license, specifying an Additional Use
- * Grant, and not modifying the license in any other way.
- */
-
 import { Checkbox } from "@/components/common/fields/checkbox";
 import { DataTable } from "@/components/common/table/data-table";
 import { DataTableColumnHeader } from "@/components/common/table/data-table-column-header";
 import { StatusBadge } from "@/components/common/table/data-table-components";
 import { DivisionCodeDialog } from "@/components/division-code-table-dialog";
-import { DivisionCodeTableEditDialog } from "@/components/division-code-table-edit-dialog";
+import { DivisionCodeEditDialog } from "@/components/division-code-table-edit-dialog";
 import { tableStatusChoices } from "@/lib/choices";
 import { type DivisionCode } from "@/types/accounting";
 import { type FilterConfig } from "@/types/tables";
@@ -93,7 +76,7 @@ export default function DivisionCodes() {
       filterColumn="code"
       tableFacetedFilters={filters}
       TableSheet={DivisionCodeDialog}
-      TableEditSheet={DivisionCodeTableEditDialog}
+      TableEditSheet={DivisionCodeEditDialog}
     />
   );
 }
