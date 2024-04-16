006a4ec186bc547d53e735382ee8151dfdcd50e8
Eric Moss <yeswolfprod@gmail.com>
2024-04-16 00:16:48 -0400

    ✅(change): improve client impls

--- internal/util/minio/client.go
+++ internal/util/minio/client.go
@@ -1,77 +1,69 @@
+// Package minio provides a client for managing media storage using Minio.
 package minio
 
 import (
+	"context"
+
 	"github.com/minio/minio-go/v7"
-	"github.com/minio/minio-go/v7/pkg/credentials"
+	"github.com/pkg/errors"
 )
 
+// Client represents a client to interact with the Minio server.
 type Client struct {
-	Client *minio.Client
-	Config *minio.Options
+	client   *minio.Client  // client is the Minio client.
+	options  *minio.Options // options are the configurations for the Minio client.
+	endpoint string         // endpoint is the Minio server URL.
 }
 
-func NewMinioClient(endpoint, accessKey, secretKey string, useSSL bool) (*Client, error) {
-	minioClient, err := minio.New(endpoint, &minio.Options{
-		Creds:  credentials.NewStaticV4(accessKey, secretKey, ""),
-		Secure: useSSL,
-	})
-	if err != nil {
-		return nil, err
+// NewClient initializes a new Minio client.
+// It panics if the Minio client cannot be created.
+func NewClient(endpoint string, opts *minio.Options) *Client {
+	mClient := &Client{
+		options:  opts,
+		endpoint: endpoint,
 	}
-
-	return &Client{
-		Client: minioClient,
-		Config: &minio.Options{
-			Creds:  credentials.NewStaticV4(accessKey, secretKey, ""),
-			Secure: useSSL,
-		},
-	}, nil
+	mClient.initialize()
+	return mClient
 }
 
-// // CreateMediaBucket creates a new bucket for media.
-// func (m *Client) CreateMediaBucket(bucketName, location string) error {
-// 	ctx := context.Background()
+// initialize sets up the Minio client. It panics if the client cannot be created.
+func (c *Client) initialize() {
+	client, err := minio.New(c.endpoint, c.options)
+	if err != nil {
+		panic(errors.Wrap(err, "failed to create Minio client"))
+	}
+	c.client = client
+}
 
-// 	err := client.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{Region: location})
-// 	if err != nil {
-// 		// Check to see if we already own this bucket (which happens if the server restarts)
-// 		exists, errBucketExists := client.BucketExists(ctx, bucketName)
-// 		if errBucketExists == nil && exists {
-// 			log.Printf("We already own %s\n", bucketName)
-// 		} else {
-// 			log.Fatalln(err)
-// 		}
-// 	} else {
-// 		log.Printf("Successfully created %s\n", bucketName)
-// 	}
+func (c *Client) Ping(ctx context.Context) error {
+	_, err := c.client.ListBuckets(ctx)
+	return err
+}
 
-// 	return nil
-// }
+// CreateBucket ensures a bucket is available for storing media.
+//
+// It creates the bucket if it does not already exist.
+func (c *Client) CreateBucket(ctx context.Context, bucketName, location string) error {
+	exists, err := c.client.BucketExists(ctx, bucketName)
+	if err != nil {
+		return errors.Wrap(err, "checking if bucket exists failed")
+	}
+	if !exists {
+		if err = c.client.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{Region: location}); err != nil {
+			return errors.Wrap(err, "creating bucket failed")
+		}
+	}
+	return nil
+}
 
-// // UploadFile uploads a file to the minio server
-// // By default, the file will be uploaded to the media bucket.
-// func (m *Client) UploadFile(objectName, filePath, bucketName string) error {
-// 	contenttype := "application/octet-stream"
-// 	ctx := context.Background()
-
-// 	_, err := client.FPutObject(ctx, bucketName, objectName, filePath, minio.PutObjectOptions{ContentType: contenttype})
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	return nil
-// }
-
-// // UploadImage uploads an image to the minio server
-// // By default, the image will be uploaded to the media bucket.
-// func (m *Client) UploadImage(objectName, filePath, bucketName string) error {
-// 	contenttype := "image/webp"
-// 	ctx := context.Background()
-
-// 	_, err := client.FPutObject(ctx, bucketName, objectName, filePath, minio.PutObjectOptions{ContentType: contenttype})
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	return nil
-// }
+// UploadMedia uploads a file to the specified bucket and object name.
+//
+// It returns UploadInfo or an error if the upload fails.
+func (c *Client) UploadMedia(ctx context.Context, bucketName, filePath, objectName string) (minio.UploadInfo, error) {
+	contentType := "application/octet-stream"
+	ui, err := c.client.FPutObject(ctx, bucketName, objectName, filePath, minio.PutObjectOptions{ContentType: contentType})
+	if err != nil {
+		return minio.UploadInfo{}, errors.Wrap(err, "upload failed")
+	}
+	return ui, nil
+}
