9dbd35cbdbcc162b1c55e5c84af27b2622ec2b08
Eric Moss <yeswolfprod@gmail.com>
2024-07-06 20:30:20 -0400

    ✅(add): customer fixture

---
+++ fixtures/load_customer.go
@@ -0,0 +1,45 @@
+package fixtures
+
+import (
+	"context"
+
+	"github.com/emoss08/trenova/pkg/gen"
+	"github.com/emoss08/trenova/pkg/models"
+	"github.com/emoss08/trenova/pkg/models/property"
+	"github.com/google/uuid"
+	"github.com/uptrace/bun"
+)
+
+func loadCustomers(ctx context.Context, db *bun.DB, gen *gen.CodeGenerator, orgID, buID uuid.UUID) error {
+	count, err := db.NewSelect().Model((*models.Customer)(nil)).Count(ctx)
+	if err != nil {
+		return err
+	}
+
+	if count < 20 {
+		for i := 0; i < 20; i++ {
+			customer := models.Customer{
+				BusinessUnitID: buID,
+				OrganizationID: orgID,
+				Status:         property.StatusActive,
+				Name:           "TEST",
+			}
+
+			err = db.RunInTx(ctx, nil, func(ctx context.Context, tx bun.Tx) error {
+				mkg, mErr := models.QueryCustomerMasterKeyGenerationByOrgID(ctx, db, orgID)
+				if mErr != nil {
+					return mErr
+				}
+
+				return customer.InsertCustomer(ctx, tx, gen, mkg.Pattern)
+			})
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
+	}
+
+	return nil
+}
