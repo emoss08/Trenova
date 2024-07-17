a9323bfd0f0197e314ea88026ac2c7646398c7d2
Eric Moss <yeswolfprod@gmail.com>
2024-07-17 12:40:23 -0400

    🍪(chore): license text

--- internal/api/handlers/test.handler.go
+++ internal/api/handlers/test.handler.go
@@ -1,3 +1,22 @@
+// Copyright (c) 2024 Trenova Technologies, LLC
+//
+// Licensed under the Business Source License 1.1 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://trenova.app/pricing/
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//
+// Key Terms:
+// - Non-production use only
+// - Change Date: 2026-11-16
+// - Change License: GNU General Public License v2 or later
+//
+// For full license text, see the LICENSE file in the root directory.
+
 // handlers to test functionality. ONLY DEV.
 package handlers
 
