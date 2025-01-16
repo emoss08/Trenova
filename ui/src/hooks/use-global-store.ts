1ee2b472124f34c9272395e4f0287985611cf737
Eric Moss <yeswolfprod@gmail.com>
2025-01-15 23:53:56 -0500

    🪛(chore): remove old license

--- ui/src/hooks/use-global-store.ts
+++ ui/src/hooks/use-global-store.ts
@@ -1,15 +1,4 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/**
- * Trenova - (c) 2024 Eric Moss
- * Licensed under the Business Source License 1.1 (BSL 1.1)
- *
- * You may use this software for non-production purposes only.
- * For full license text, see the LICENSE file in the project root.
- *
- * This software will be licensed under GPLv2 or later on 2026-11-16.
- * For alternative licensing options, email: eric@trenova.app
- */
 
 /**
  * Credits: Acorn1010 - https://gist.github.com/acorn1010/9f4621d3dfc33052ffd84f6c2a06d4d6.
