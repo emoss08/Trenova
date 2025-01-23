13ca17a75084a6dd1c7e5009562610a14dc6e35a
Eric Moss <yeswolfprod@gmail.com>
2025-01-23 17:37:55 -0500

    bump

--- cmd/parse/main.go
+++ cmd/parse/main.go
@@ -1,25 +1,17 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"path/filepath"
+// func main() {
+// 	dir, err := rptmeta.GetCurrentWorkingDirectory()
+// 	if err != nil {
+// 		log.Fatalf("Failed to get current working directory: %v", err)
+// 	}
 
-	"github.com/emoss08/trenova/internal/pkg/rptmeta"
-)
+// 	// Go back two directories
+// 	dir = filepath.Join(dir, "..", "..")
 
-func main() {
-	dir, err := rptmeta.GetCurrentWorkingDirectory()
-	if err != nil {
-		log.Fatalf("Failed to get current working directory: %v", err)
-	}
-
-	// Go back two directories
-	dir = filepath.Join(dir, "..", "..")
-
-	parsed, err := rptmeta.Parse(filepath.Join(dir, "internal/pkg/rptmeta/canned", "workers.rptmeta.yaml"))
-	if err != nil {
-		log.Fatalf("Failed to parse report: %v", err)
-	}
-	fmt.Printf("Report: %+v\n\n", parsed)
-}
+// 	parsed, err := rptmeta.Parse(filepath.Join(dir, "internal/pkg/rptmeta/canned", "workers.rptmeta.yaml"))
+// 	if err != nil {
+// 		log.Fatalf("Failed to parse report: %v", err)
+// 	}
+// 	fmt.Printf("Report: %+v\n\n", parsed)
+// }
