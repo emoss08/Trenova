8c519f1f971fe85615f84d6ebb2a6127164083c7
Eric Moss <yeswolfprod@gmail.com>
2025-01-23 17:38:58 -0500

    bump

--- cmd/parse/main.go
+++ cmd/parse/main.go
@@ -1,17 +1,4 @@
 package main
 
-// func main() {
-// 	dir, err := rptmeta.GetCurrentWorkingDirectory()
-// 	if err != nil {
-// 		log.Fatalf("Failed to get current working directory: %v", err)
-// 	}
-
-// 	// Go back two directories
-// 	dir = filepath.Join(dir, "..", "..")
-
-// 	parsed, err := rptmeta.Parse(filepath.Join(dir, "internal/pkg/rptmeta/canned", "workers.rptmeta.yaml"))
-// 	if err != nil {
-// 		log.Fatalf("Failed to parse report: %v", err)
-// 	}
-// 	fmt.Printf("Report: %+v\n\n", parsed)
-// }
+func main() {
+}
