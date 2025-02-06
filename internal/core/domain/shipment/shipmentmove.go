7986a75fbea527de2e621cd265b5eaaee1bbe3d9
Eric Moss <yeswolfprod@gmail.com>
2025-02-06 17:54:49 -0500

    🟡(change): remove equipment & worker fields

--- internal/core/domain/shipment/shipmentmove.go
+++ internal/core/domain/shipment/shipmentmove.go
@@ -5,9 +5,6 @@ import (
 
 	"github.com/emoss08/trenova/internal/core/domain/businessunit"
 	"github.com/emoss08/trenova/internal/core/domain/organization"
-	"github.com/emoss08/trenova/internal/core/domain/tractor"
-	"github.com/emoss08/trenova/internal/core/domain/trailer"
-	"github.com/emoss08/trenova/internal/core/domain/worker"
 	"github.com/emoss08/trenova/internal/pkg/errors"
 	"github.com/emoss08/trenova/internal/pkg/utils/timeutils"
 	"github.com/emoss08/trenova/pkg/types/pulid"
@@ -27,11 +24,7 @@ type ShipmentMove struct {
 	OrganizationID pulid.ID `bun:"organization_id,pk,notnull,type:VARCHAR(100)" json:"organizationId"`
 
 	// Relationship identifiers (Non-Primary-Keys)
-	ShipmentID        pulid.ID  `bun:"shipment_id,type:VARCHAR(100),notnull" json:"shipmentId"`
-	PrimaryWorkerID   *pulid.ID `bun:"primary_worker_id,type:VARCHAR(100),nullzero" json:"primaryWorkerId"`
-	SecondaryWorkerID *pulid.ID `bun:"secondary_worker_id,type:VARCHAR(100),nullzero" json:"secondaryWorkerId"`
-	TrailerID         *pulid.ID `bun:"trailer_id,type:VARCHAR(100),nullzero" json:"trailerId"`
-	TractorID         *pulid.ID `bun:"tractor_id,type:VARCHAR(100),nullzero" json:"tractorId"`
+	ShipmentID pulid.ID `bun:"shipment_id,type:VARCHAR(100),notnull" json:"shipmentId"`
 
 	// Core Fields
 	Status   MoveStatus `json:"status" bun:"status,type:move_status_enum,notnull,default:'New'"`
@@ -45,14 +38,11 @@ type ShipmentMove struct {
 	UpdatedAt int64 `bun:"updated_at,type:BIGINT,nullzero,notnull,default:extract(epoch from current_timestamp)::bigint" json:"updatedAt"`
 
 	// Relationships
-	BusinessUnit    *businessunit.BusinessUnit `bun:"rel:belongs-to,join:business_unit_id=id" json:"-"`
-	Organization    *organization.Organization `bun:"rel:belongs-to,join:organization_id=id" json:"-"`
-	Shipment        *Shipment                  `bun:"rel:belongs-to,join:shipment_id=id" json:"shipment,omitempty"`
-	Tractor         *tractor.Tractor           `bun:"rel:belongs-to,join:tractor_id=id" json:"tractor,omitempty"`
-	Trailer         *trailer.Trailer           `bun:"rel:belongs-to,join:trailer_id=id" json:"trailer,omitempty"`
-	PrimaryWorker   *worker.Worker             `bun:"rel:belongs-to,join:primary_worker_id=id" json:"primaryWorker,omitempty"`
-	SecondaryWorker *worker.Worker             `bun:"rel:belongs-to,join:secondary_worker_id=id" json:"secondaryWorker,omitempty"`
-	Stops           []*Stop                    `bun:"rel:has-many,join:id=shipment_move_id" json:"stops,omitempty"`
+	BusinessUnit *businessunit.BusinessUnit `bun:"rel:belongs-to,join:business_unit_id=id" json:"-"`
+	Organization *organization.Organization `bun:"rel:belongs-to,join:organization_id=id" json:"-"`
+	Shipment     *Shipment                  `bun:"rel:belongs-to,join:shipment_id=id" json:"shipment,omitempty"`
+	Assignment   *Assignment                `bun:"rel:has-one,join:id=shipment_move_id" json:"assignment,omitempty"`
+	Stops        []*Stop                    `bun:"rel:has-many,join:id=shipment_move_id" json:"stops,omitempty"`
 }
 
 func (sm *ShipmentMove) Validate(ctx context.Context, multiErr *errors.MultiError) {
@@ -67,19 +57,6 @@ func (sm *ShipmentMove) Validate(ctx context.Context, multiErr *errors.MultiErro
 				MoveStatusCompleted,
 				MoveStatusCanceled,
 			).Error("Status must be a valid move status"),
-		),
-
-		// Tractor ID is requird
-		validation.Field(&sm.TractorID,
-			validation.Required.Error("Tractor is required"),
-		),
-		// Trailer ID is requird
-		validation.Field(&sm.TrailerID,
-			validation.Required.Error("Trailer is required"),
-		),
-		// Primary Worker ID is requird
-		validation.Field(&sm.PrimaryWorkerID,
-			validation.Required.Error("Primary Worker is required"),
 		),
 	)
 	if err != nil {
