4f25b3ec1fba6ba5e236289733680861834b31be
emoss08 <66630775+emoss08@users.noreply.github.com>
2025-03-22 16:38:32 -0400

    🟡(change): login screen background

--- ui/src/app/auth/login-page.tsx
+++ ui/src/app/auth/login-page.tsx
@@ -1,26 +1,30 @@
 import logo from "@/assets/logo.webp";
 import { MetaTags } from "@/components/meta-tags";
 import { LazyImage } from "@/components/ui/image";
+import { SplashCursor } from "@/components/ui/splash-cursor";
 import { AuthForm } from "./_components/auth-form";
 
 export function LoginPage() {
   return (
     <>
       <MetaTags title="Login" description="Login to your account" />
-      <div className="fixed inset-0 flex flex-col items-center justify-center px-4">
-        <div className="absolute top-8 left-8">
-          <LazyImage
-            src={logo}
-            alt="Trenova Logo"
-            width={40}
-            height={40}
-            layout="constrained"
-          />
+      <div className="relative w-full h-screen overflow-hidden">
+        <SplashCursor />
+        <div className="fixed inset-0 flex flex-col items-center justify-center px-4">
+          <div className="absolute top-8 left-8">
+            <LazyImage
+              src={logo}
+              alt="Trenova Logo"
+              width={40}
+              height={40}
+              layout="constrained"
+            />
+          </div>
+          <AuthForm />
+          <footer className="mt-8 text-center text-sm text-muted-foreground">
+            &copy; {new Date().getFullYear()} Trenova. All rights reserved.
+          </footer>
         </div>
-        <AuthForm />
-        <footer className="mt-8 text-center text-sm text-muted-foreground">
-          &copy; {new Date().getFullYear()} Trenova. All rights reserved.
-        </footer>
       </div>
     </>
   );
