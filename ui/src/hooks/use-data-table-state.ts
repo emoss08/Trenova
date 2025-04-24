45c126dce12723c9fe64beee3ae40630b4cf8e5d
Eric Moss <66630775+emoss08@users.noreply.github.com>
2025-04-24 17:39:01 -0400

    🟡(change): remove zustand data-table state
    We will be using nuqs to manage the state for the data-table.

--- ui/src/hooks/use-data-table-state.ts
+++ ui/src/hooks/use-data-table-state.ts
@@ -1,37 +1,6 @@
-import { useTableStore as store } from "@/stores/table-store";
-import { DataTableState } from "@/types/data-table";
+import { parseAsInteger } from "nuqs";
 
-export function useDataTableState<
-  TData extends Record<string, any>,
->(): DataTableState<TData> {
-  const [{ pageIndex, pageSize }, setPagination] = store.use("pagination");
-  const [rowSelection, setRowSelection] = store.use("rowSelection");
-  const [currentRecord, setCurrentRecord] = store.use("currentRecord");
-  const [columnVisibility, setColumnVisibility] = store.use("columnVisibility");
-  const [columnFilters, setColumnFilters] = store.use("columnFilters");
-  const [sorting, setSorting] = store.use("sorting");
-  const [showFilterDialog, setShowFilterDialog] = store.use("showFilterDialog");
-  const [initialPageSize, setInitialPageSize] = store.use("initialPageSize");
-  const [defaultSort, setDefaultSort] = store.use("defaultSort");
-
-  return {
-    pagination: { pageIndex, pageSize },
-    setPagination,
-    rowSelection,
-    setRowSelection,
-    currentRecord,
-    setCurrentRecord,
-    columnVisibility,
-    setColumnVisibility,
-    columnFilters,
-    setColumnFilters,
-    sorting,
-    setSorting,
-    showFilterDialog,
-    setShowFilterDialog,
-    initialPageSize,
-    setInitialPageSize,
-    defaultSort,
-    setDefaultSort,
-  };
-}
+export const DataTableSearchParams = {
+  page: parseAsInteger.withDefault(1),
+  pageSize: parseAsInteger.withDefault(10),
+};
