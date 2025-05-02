963f2e710714325e8eb96646925d78b5ca9653d4
Eric Moss <yeswolfprod@gmail.com>
2025-05-01 22:57:06 -0400

    🟡(change): add new indexes

--- internal/infrastructure/database/postgres/migrations/20241211015840_shipment.tx.up.sql
+++ internal/infrastructure/database/postgres/migrations/20241211015840_shipment.tx.up.sql
@@ -1,5 +1,5 @@
 -- Enums with documentation
-CREATE TYPE "shipment_status_enum" AS ENUM (
+CREATE TYPE "shipment_status_enum" AS ENUM(
     'New',
     -- Initial state when shipment is created
     'PartiallyAssigned',
@@ -19,7 +19,7 @@ CREATE TYPE "shipment_status_enum" AS ENUM (
     'Canceled' -- Shipment has been Canceled
 );
 
-CREATE TYPE "rating_method_enum" AS ENUM (
+CREATE TYPE "rating_method_enum" AS ENUM(
     'FlatRate',
     -- Fixed rate for entire shipment
     'PerMile',
@@ -36,7 +36,7 @@ CREATE TYPE "rating_method_enum" AS ENUM (
 );
 
 --bun:split
-CREATE TABLE IF NOT EXISTS "shipments" (
+CREATE TABLE IF NOT EXISTS "shipments"(
     "id" varchar(100) NOT NULL,
     "pro_number" varchar(100) NOT NULL,
     "organization_id" varchar(100) NOT NULL,
@@ -65,19 +65,19 @@ CREATE TABLE IF NOT EXISTS "shipments" (
     "updated_at" bigint NOT NULL DEFAULT EXTRACT(EPOCH FROM CURRENT_TIMESTAMP) ::bigint,
     -- Constraints
     CONSTRAINT "pk_shipments" PRIMARY KEY ("id", "organization_id", "business_unit_id"),
-    CONSTRAINT "fk_shipments_business_unit" FOREIGN KEY ("business_unit_id") REFERENCES "business_units" ("id") ON UPDATE NO ACTION ON DELETE CASCADE,
-    CONSTRAINT "fk_shipments_organization" FOREIGN KEY ("organization_id") REFERENCES "organizations" ("id") ON UPDATE NO ACTION ON DELETE CASCADE,
-    CONSTRAINT "fk_shipments_canceled_by" FOREIGN KEY ("canceled_by_id") REFERENCES "users" ("id") ON UPDATE NO ACTION ON DELETE SET NULL
+    CONSTRAINT "fk_shipments_business_unit" FOREIGN KEY ("business_unit_id") REFERENCES "business_units"("id") ON UPDATE NO ACTION ON DELETE CASCADE,
+    CONSTRAINT "fk_shipments_organization" FOREIGN KEY ("organization_id") REFERENCES "organizations"("id") ON UPDATE NO ACTION ON DELETE CASCADE,
+    CONSTRAINT "fk_shipments_canceled_by" FOREIGN KEY ("canceled_by_id") REFERENCES "users"("id") ON UPDATE NO ACTION ON DELETE SET NULL
 );
 
 --bun:split
-CREATE INDEX IF NOT EXISTS "idx_shipments_bol" ON "shipments" ("bol");
+CREATE INDEX IF NOT EXISTS "idx_shipments_bol" ON "shipments"("bol");
 
-CREATE INDEX IF NOT EXISTS "idx_shipments_created_at" ON "shipments" ("created_at", "updated_at");
+CREATE INDEX IF NOT EXISTS "idx_shipments_created_at" ON "shipments"("created_at", "updated_at");
 
-CREATE INDEX IF NOT EXISTS "idx_shipments_status" ON "shipments" ("status");
+CREATE INDEX IF NOT EXISTS "idx_shipments_status" ON "shipments"("status");
 
-CREATE INDEX IF NOT EXISTS "idx_shipments_business_unit" ON "shipments" ("business_unit_id", "organization_id");
+CREATE INDEX IF NOT EXISTS "idx_shipments_business_unit" ON "shipments"("business_unit_id", "organization_id");
 
 COMMENT ON TABLE shipments IS 'Stores information about shipments and their billing status';
 
@@ -86,13 +86,13 @@ ALTER TABLE "shipments"
     ADD COLUMN IF NOT EXISTS search_vector tsvector;
 
 --bun:split
-CREATE INDEX IF NOT EXISTS idx_shipments_search ON shipments USING GIN (search_vector);
+CREATE INDEX IF NOT EXISTS idx_shipments_search ON shipments USING GIN(search_vector);
 
 --bun:split
-CREATE INDEX IF NOT EXISTS idx_shipments_dates_brin ON shipments USING BRIN (actual_ship_date, actual_delivery_date, created_at) WITH (pages_per_range = 128);
+CREATE INDEX IF NOT EXISTS idx_shipments_dates_brin ON shipments USING BRIN(actual_ship_date, actual_delivery_date, created_at) WITH (pages_per_range = 128);
 
 --bun:split
-CREATE OR REPLACE FUNCTION shipments_search_vector_update ()
+CREATE OR REPLACE FUNCTION shipments_search_vector_update()
     RETURNS TRIGGER
     AS $$
 BEGIN
@@ -113,10 +113,10 @@ DROP TRIGGER IF EXISTS shipments_search_vector_trigger ON shipments;
 CREATE TRIGGER shipments_search_vector_trigger
     BEFORE INSERT OR UPDATE ON shipments
     FOR EACH ROW
-    EXECUTE FUNCTION shipments_search_vector_update ();
+    EXECUTE FUNCTION shipments_search_vector_update();
 
 --bun:split
-CREATE INDEX IF NOT EXISTS idx_shipments_active ON shipments (created_at DESC)
+CREATE INDEX IF NOT EXISTS idx_shipments_active ON shipments(created_at DESC)
 WHERE
     status NOT IN ('Completed', 'Canceled', 'Billed');
 
@@ -132,5 +132,10 @@ ALTER TABLE shipments
     ALTER COLUMN business_unit_id SET STATISTICS 1000;
 
 --bun:split
-CREATE INDEX IF NOT EXISTS idx_shipments_trgm_pro_bol ON shipments USING gin ((pro_number || ' ' || bol) gin_trgm_ops);
+CREATE INDEX IF NOT EXISTS idx_shipments_trgm_pro_bol ON shipments USING gin((pro_number || ' ' || bol) gin_trgm_ops);
+
+--bun:split
+CREATE INDEX idx_shipments_bu_org_status_created_at ON shipments(business_unit_id, organization_id, status, created_at DESC);
+
+CREATE INDEX idx_shipments_bu_org_include ON shipments(business_unit_id, organization_id) INCLUDE (status, created_at, pro_number, bol);
 
