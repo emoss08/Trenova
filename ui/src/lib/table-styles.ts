ebac41b67b2374a0a43244b7bc519ab95ef15da5
Eric Moss <yeswolfprod@gmail.com>
2025-06-29 21:01:52 -0400

    🟡(change): shipment status table styles

--- ui/src/lib/table-styles.ts
+++ ui/src/lib/table-styles.ts
@@ -7,11 +7,13 @@ export function getShipmentStatusRowClassName(status: ShipmentStatus): string {
       return cn(
         "bg-orange-600/10 hover:bg-orange-600/20 data-[state=selected]:bg-orange-600/30 focus-visible:bg-orange-600/20",
         "dark:bg-orange-600/20 dark:hover:bg-orange-600/30 dark:data-[state=selected]:bg-orange-600/30 dark:focus-visible:bg-orange-600/20",
+        "outline-orange-600 border-orange-600 [&_td]:md:border-orange-600",
       );
     case ShipmentStatus.Canceled:
       return cn(
         "bg-red-600/10 hover:bg-red-600/20 data-[state=selected]:bg-red-600/30 focus-visible:bg-red-600/20",
         "dark:bg-red-600/20 dark:hover:bg-red-600/30 dark:data-[state=selected]:bg-red-600/30 dark:focus-visible:bg-red-600/20",
+        "outline-red-600 border-red-600 [&_td]:md:border-red-600",
       );
     default:
       return "";
