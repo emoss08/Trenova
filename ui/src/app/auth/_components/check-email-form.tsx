422ecb4ef0932ca33b3e8b6e9923d657ec3735e2
Eric Moss <yeswolfprod@gmail.com>
2025-07-07 16:37:04 -0400

    🔴(delete): cloudflare turnstile

--- ui/src/app/auth/_components/check-email-form.tsx
+++ ui/src/app/auth/_components/check-email-form.tsx
@@ -2,15 +2,12 @@ import { InputField } from "@/components/fields/input-field";
 import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormGroup } from "@/components/ui/form";
 import { Icon } from "@/components/ui/icons";
-import { TURNSTILE_SITE_KEY } from "@/constants/env";
 import { checkEmailSchema, CheckEmailSchema } from "@/lib/schemas/auth-schema";
 import { api } from "@/services/api";
 import { APIError } from "@/types/errors";
 import { faEnvelope } from "@fortawesome/pro-regular-svg-icons";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Turnstile } from "@marsidev/react-turnstile";
 import { useMutation } from "@tanstack/react-query";
-import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 
@@ -18,16 +15,11 @@ type CheckEmailFormProps = {
   onEmailVerified: (email: string) => void;
 };
 
-type TurnstileStatus = "error" | "expired" | "solved";
-
 export function CheckEmailForm({ onEmailVerified }: CheckEmailFormProps) {
   const mutation = useMutation({
     mutationFn: async (values: CheckEmailSchema) =>
       await api.auth.checkEmail(values.emailAddress),
   });
-  const [status, setStatus] = useState<TurnstileStatus | null>(null);
-
-  console.info("Turnstile status", status);
 
   const {
     control,
@@ -80,25 +72,11 @@ export function CheckEmailForm({ onEmailVerified }: CheckEmailFormProps) {
         size="lg"
         type="submit"
         className="w-full"
-        isLoading={isSubmitting || status !== "solved"}
+        isLoading={isSubmitting}
         loadingText="Verifying..."
-        disabled={status !== "solved"}
       >
         Continue
       </Button>
-      <div className="flex w-full justify-center mt-4">
-        <Turnstile
-          siteKey={TURNSTILE_SITE_KEY}
-          options={{
-            action: "email-verification",
-            appearance: "execute",
-            size: "flexible",
-          }}
-          onSuccess={() => setStatus("solved")}
-          onError={() => setStatus("error")}
-          onExpire={() => setStatus("expired")}
-        />
-      </div>
     </Form>
   );
 }
