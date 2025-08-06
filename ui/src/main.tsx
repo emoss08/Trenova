371f4e749bd45c5d12e1d9bcc4146bd7e092a708
Eric Moss <yeswolfprod@gmail.com>
2025-08-06 19:59:20 -0400

    ✅(add): sentry support to ui

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -3,15 +3,35 @@
  * Licensed under FSL-1.1-ALv2 (Functional Source License 1.1, Apache 2.0 Future)
  * Full license: https://github.com/emoss08/Trenova/blob/master/LICENSE.md */
 
+import * as Sentry from "@sentry/react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { Providers } from "./components/providers.tsx";
+import { APP_ENV, ENABLE_SENTRY, SENTRY_DSN } from "./constants/env.ts";
 import "./styles/app.css";
 
+if (ENABLE_SENTRY) {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    sendDefaultPii: true,
+    environment: APP_ENV,
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration(),
+    ],
+    tracesSampleRate: 1.0,
+    tracePropagationTargets: ["localhost", "trenova.app"],
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+    enableLogs: true,
+  });
+}
+
 createRoot(document.getElementById("root")!).render(
-  // <StrictMode>
-  <Providers>
-    <App />
-  </Providers>,
-  // </StrictMode>,
+  <StrictMode>
+    <Providers>
+      <App />
+    </Providers>
+  </StrictMode>,
 );
