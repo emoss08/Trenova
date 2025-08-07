52c428f871e2373b435cf9e2a3bc09e2bfb0eddd
Eric Moss <yeswolfprod@gmail.com>
2025-08-06 22:08:45 -0400

    🍪(chore): run formatters

--- services/ai-document-quality/src/models/model.py
+++ services/ai-document-quality/src/models/model.py
@@ -1,11 +1,10 @@
-#
 # Copyright 2023-2025 Eric Moss
 # Licensed under FSL-1.1-ALv2 (Functional Source License 1.1, Apache 2.0 Future)
 # Full license: https://github.com/emoss08/Trenova/blob/master/LICENSE.md#
 import logging
 import os
 from dataclasses import dataclass
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, List, Optional
 
 import numpy as np
 import pandas as pd
