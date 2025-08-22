de9251bc7e3fc63fdc4c1abb617ab794df3f9001
Eric Moss <yeswolfprod@gmail.com>
2025-08-22 14:52:32 -0400

    ✅(add): meta tag

--- ui/src/app/docker/page.tsx
+++ ui/src/app/docker/page.tsx
@@ -4,12 +4,14 @@
  * Full license: https://github.com/emoss08/Trenova/blob/master/LICENSE.md
  */
 
+import { MetaTags } from "@/components/meta-tags";
 import { DockerOverview } from "./_components/docker-overview";
 import { DockerTabs } from "./_components/docker-tabs";
 
 export function DockerManagement() {
   return (
     <div className="flex flex-col gap-4 p-4">
+      <MetaTags title="Docker Management" description="Docker Management" />
       <div className="flex items-center justify-between">
         <div>
           <h1 className="text-2xl font-semibold">Docker Management</h1>
