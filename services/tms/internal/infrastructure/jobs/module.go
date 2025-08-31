375fbed5d7300b9bbb48f0b44f436feab0e16f3d
Eric Moss <yeswolfprod@gmail.com>
2025-08-31 03:11:57 -0400

    🟡(change): remove asynq

--- services/tms/internal/infrastructure/jobs/module.go
+++ services/tms/internal/infrastructure/jobs/module.go
@@ -1,127 +1,53 @@
-/*
- * Copyright 2023-2025 Eric Moss
- * Licensed under FSL-1.1-ALv2 (Functional Source License 1.1, Apache 2.0 Future)
- * Full license: https://github.com/emoss08/Trenova/blob/master/LICENSE.md */
-
 package jobs
 
 import (
-	"context"
-
-	"github.com/emoss08/trenova/internal/core/ports/services"
-	"github.com/emoss08/trenova/internal/pkg/config"
-	"github.com/emoss08/trenova/internal/pkg/jobs"
-	"github.com/emoss08/trenova/internal/pkg/jobs/handlers"
-	"github.com/emoss08/trenova/internal/pkg/jobs/scheduler"
-	"github.com/emoss08/trenova/internal/pkg/jobs/triggers"
-	"github.com/hibiken/asynq"
-	"github.com/rs/zerolog/log"
+	"github.com/emoss08/trenova/internal/infrastructure/jobs/temporaljobs"
+	"github.com/emoss08/trenova/internal/infrastructure/jobs/temporaljobs/auditjobs"
+	"github.com/emoss08/trenova/internal/infrastructure/jobs/temporaljobs/jobscheduler"
+	"github.com/emoss08/trenova/internal/infrastructure/jobs/temporaljobs/notificationjobs"
+	"github.com/emoss08/trenova/internal/infrastructure/jobs/temporaljobs/shipmentjobs"
+	"github.com/emoss08/trenova/internal/infrastructure/jobs/temporaljobs/systemjobs"
 	"go.uber.org/fx"
 )
 
-// Module provides job service infrastructure and handlers
 var Module = fx.Module(
-	"jobs",
-	fx.Provide(
-		// Infrastructure
-		NewRedisConnOpt,
-		jobs.NewJobService,
-		scheduler.NewCronScheduler,
-		triggers.NewShipmentTrigger,
+	"temporal-jobs",
+	ActivitiesModule,
+	ClientModule,
+	WorkerModule,
+	SchedulerModule,
+)
 
-		// Job Handlers
-		fx.Annotate(
-			handlers.NewPatternAnalysisHandler,
-			fx.As(new(services.JobHandler)),
-			fx.ResultTags(`group:"job_handlers"`),
-		),
-		fx.Annotate(
-			handlers.NewExpireSuggestionsHandler,
-			fx.As(new(services.JobHandler)),
-			fx.ResultTags(`group:"job_handlers"`),
-		),
-		fx.Annotate(
-			handlers.NewDuplicateShipmentHandler,
-			fx.As(new(services.JobHandler)),
-			fx.ResultTags(`group:"job_handlers"`),
-		),
-		fx.Annotate(
-			handlers.NewDelayShipmentHandler,
-			fx.As(new(services.JobHandler)),
-			fx.ResultTags(`group:"job_handlers"`),
-		),
-		fx.Annotate(
-			handlers.NewEmailHandler,
-			fx.As(new(services.JobHandler)),
-			fx.ResultTags(`group:"job_handlers"`),
-		),
-		fx.Annotate(
-			handlers.NewEmailQueueHandler,
-			fx.As(new(services.JobHandler)),
-			fx.ResultTags(`group:"job_handlers"`),
-		),
-	),
-	fx.Invoke(
-		RegisterLifecycleHooks,
+var ActivitiesModule = fx.Module(
+	"temporal-activities",
+	fx.Provide(
+		shipmentjobs.NewActivities,
+		notificationjobs.NewActivities,
+		systemjobs.NewActivities,
+		auditjobs.NewActivities,
 	),
 )
 
-// NewRedisConnOpt creates Redis connection options for Asynq
-func NewRedisConnOpt(cfg *config.Manager) asynq.RedisClientOpt {
-	redisConfig := cfg.Redis()
+var ClientModule = fx.Module(
+	"temporal-client",
+	fx.Provide(
+		temporaljobs.NewTemporalClient,
+	),
+)
 
-	return asynq.RedisClientOpt{
-		Addr:     redisConfig.Addr,
-		Password: redisConfig.Password,
-		DB:       redisConfig.DB,
-	}
-}
+var WorkerModule = fx.Module(
+	"temporal-worker",
+	fx.Invoke(
+		shipmentjobs.NewWorker,
+		notificationjobs.NewWorker,
+		systemjobs.NewWorker,
+		auditjobs.NewWorker,
+	),
+)
 
-// LifecycleParams defines dependencies for lifecycle management
-type LifecycleParams struct {
-	fx.In
-
-	Lifecycle     fx.Lifecycle
-	JobService    services.JobService
-	CronScheduler scheduler.CronSchedulerInterface
-}
-
-// RegisterLifecycleHooks registers startup and shutdown hooks for the job service and scheduler
-func RegisterLifecycleHooks(p LifecycleParams) {
-	p.Lifecycle.Append(fx.Hook{
-		OnStart: func(context.Context) error {
-			// Start job service first
-			if err := p.JobService.Start(); err != nil {
-				log.Error().Err(err).Msg("failed to start job service")
-				return err
-			}
-
-			// Then start cron scheduler
-			if err := p.CronScheduler.Start(); err != nil {
-				log.Error().Err(err).Msg("failed to start cron scheduler")
-				return err
-			}
-
-			log.Info().Msg("job service and cron scheduler started")
-
-			return nil
-		},
-		OnStop: func(context.Context) error {
-			// Stop scheduler first
-			if err := p.CronScheduler.Stop(); err != nil {
-				log.Error().Err(err).Msg("failed to stop cron scheduler")
-				return err
-			}
-
-			// Then stop job service
-			if err := p.JobService.Shutdown(); err != nil {
-				log.Error().Err(err).Msg("failed to stop job service")
-				return err
-			}
-
-			log.Info().Msg("job service and cron scheduler stopped")
-
-			return nil
-		},
-	})
-}
+var SchedulerModule = fx.Module(
+	"temporal-scheduler",
+	fx.Invoke(
+		jobscheduler.NewScheduler,
+	),
+)
--- shared/payload/go.mod
+++ shared/payload/go.mod
@@ -7,6 +7,7 @@ require github.com/emoss08/trenova/shared/pulid v0.0.0-00010101000000-0000000000
 require (
 	github.com/oklog/ulid/v2 v2.1.1 // indirect
 	github.com/rotisserie/eris v0.5.4 // indirect
+	github.com/stretchr/testify v1.11.0 // indirect
 )
 
 replace github.com/emoss08/trenova/shared/pulid => ../pulid
