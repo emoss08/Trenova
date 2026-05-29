b92fe39821285a2370193860ecb957bf61a576af
Eric Moss <yeswolfprod@gmail.com>
2026-05-29 10:13:03 -0400

    change: react-query staleTime
    users were receiving cached information that would sometimes cause a
    hard refresh of the browser window

--- client/src/providers.tsx
+++ client/src/providers.tsx
@@ -18,7 +18,7 @@ const queryClient = new QueryClient({
       // },
       retry: false,
       refetchOnWindowFocus: false,
-      staleTime: 5 * 60 * 1000, // 5 minutes
+      staleTime: 0,
       gcTime: 10 * 60 * 1000, // 10 minutes
     },
   },
